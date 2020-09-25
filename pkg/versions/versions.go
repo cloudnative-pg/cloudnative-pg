18fd8577bd9d9a49559470c8ec10d76205c1ef84
Leonardo Cecchi <leonardo.cecchi@2ndquadrant.it>
2020-09-25 14:40:57 +0200

    Add PostgreSQL 13 to E2E tests
    Co-authored-by: Leonardo Cecchi <leonardo.cecchi@2ndquadrant.it>

--- pkg/versions/versions.go
+++ pkg/versions/versions.go
@@ -16,7 +16,7 @@ const (
 
 	// DefaultImageName is the image used by default by the operator to create
 	// pods.
-	DefaultImageName = "quay.io/2ndquadrant/postgres:12.3"
+	DefaultImageName = "quay.io/2ndquadrant/postgres:13.0"
 
 	// DefaultOperatorImageName used to bootstrap the controller in the Pods running
 	// PostgreSQL
