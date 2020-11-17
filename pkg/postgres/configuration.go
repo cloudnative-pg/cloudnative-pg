3ff029e4d384b2ee6f31b7adf9bc5e6249ce20b5
Leonardo Cecchi <leonardo.cecchi@2ndquadrant.it>
2020-11-17 11:52:13 +0100

    Retain compatibility with PostgreSQL <12
    Avoid setting the `ssl_min_protocol_version`, which is available only
    from PostgreSQL 12 and beyond.
    
    Co-authored-by: Leonardo Cecchi <leonardo.cecchi@2ndquadrant.it>
    Co-authored-by: Marco Nenciarini <marco.nenciarini@2ndquadrant.it>

--- pkg/postgres/configuration.go
+++ pkg/postgres/configuration.go
@@ -199,15 +199,6 @@ var (
 			"ssl_cert_file":    ServerCertificateLocation,
 			"ssl_key_file":     ServerKeyLocation,
 			"ssl_ca_file":      CACertificateLocation,
-			"ssl_ciphers": "ECDHE-ECDSA-AES128-GCM-SHA256:" +
-				"ECDHE-RSA-AES128-GCM-SHA256:" +
-				"ECDHE-ECDSA-AES256-GCM-SHA384:" +
-				"ECDHE-RSA-AES256-GCM-SHA384:" +
-				"ECDHE-ECDSA-CHACHA20-POLY1305:" +
-				"ECDHE-RSA-CHACHA20-POLY1305:" +
-				"DHE-RSA-AES128-GCM-SHA256:" +
-				"DHE-RSA-AES256-GCM-SHA384",
-			"ssl_min_protocol_version": "TLSv1.2",
 		},
 	}
 )
