5b19b62d65b00617766c763ff7677c9539a991d2
Marco Nenciarini <marco.nenciarini@2ndquadrant.it>
2020-11-20 10:43:58 +0100

    Versions update
    Co-authored-by: Marco Nenciarini <marco.nenciarini@2ndquadrant.it>

--- pkg/versions/versions.go
+++ pkg/versions/versions.go
@@ -16,7 +16,7 @@ const (
 
 	// DefaultImageName is the image used by default by the operator to create
 	// pods.
-	DefaultImageName = "quay.io/enterprisedb/postgresql:13"
+	DefaultImageName = "quay.io/enterprisedb/postgresql:13.1"
 
 	// DefaultOperatorImageName used to bootstrap the controller in the Pods running
 	// PostgreSQL
