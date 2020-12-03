4d96c930877c1b3243ef6fcefe063cbf3cbf95d8
Marco Nenciarini <marco.nenciarini@2ndquadrant.it>
2020-12-03 11:08:00 +0100

    Fix missing events while a Job is running
    Co-authored-by: Marco Nenciarini <marco.nenciarini@2ndquadrant.it>
    Co-authored-by: Leonardo Cecchi <leonardo.cecchi@2ndquadrant.it>

--- controllers/replicas.go
+++ controllers/replicas.go
@@ -31,6 +31,7 @@ func (r *ClusterReconciler) updateTargetPrimaryFromPods(
 	status postgres.PostgresqlStatusList,
 ) error {
 	log := r.Log.WithValues("namespace", cluster.Namespace, "name", cluster.Name)
+	// TODO: what if I delete the master with only 2 instances
 	if len(status.Items) <= 1 && cluster.Status.Instances <= 1 {
 		// Can't make a switchover of failover if we have
 		// less than two instances
