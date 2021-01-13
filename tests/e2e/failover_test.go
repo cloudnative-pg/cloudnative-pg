8610ec7f6eb4bf8ac19a59d315b65a59a528a680
Francesco Canovai <francesco.canovai@enterprisedb.com>
2021-01-13 18:37:47 +0100

    Fix failover E2E test
    The test was pausing replication on an instance, expecting the other one
    to be promoted. However, the walreceiver was still receiving WALs, so
    the paused instance could still be promoted, making the test fail. We
    now send a SIGSTOP to the walreceiver, so we know for sure that nothing
    gets on that standby and we can expect the other one to be promoted.
    
    Terminate the pausedReplica walsender on the primary after pausing the
    PausedReplica walreceiver. This way, we don't wait for the replication
    timeout to occur.
    
    Co-authored-by: Niccolò Fei <niccolo.fei@enterprisedb.com>
    Co-authored-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>

--- tests/e2e/failover_test.go
+++ tests/e2e/failover_test.go
@@ -30,25 +30,35 @@ var _ = Describe("Failover", func() {
 		err := env.DeleteNamespace(namespace)
 		Expect(err).ToNot(HaveOccurred())
 	})
+
+	// This tests only checks that after the failure of a primary the instance
+	// that has received/applied more WALs is promoted.
+	// To make sure that we know which instance is promoted, we pause the
+	// second instance walreceiver via a SIGSTOP signal, create WALs and then
+	// delete the primary pod. We need to make sure to SIGCONT the walreceiver,
+	// otherwise the operator will wait forever for the walreceiver to die
+	// before deciding which instance to promote (which should be the third).
 	It("reacts to primary failure", func() {
 		// Create a cluster in a namespace we'll delete after the test
 		err := env.CreateNamespace(namespace)
 		Expect(err).ToNot(HaveOccurred())
 
 		var pods []string
-		var currentPrimary, targetPrimary, pausedReplica string
+		var currentPrimary, targetPrimary, pausedReplica, pid string
 
 		AssertCreateCluster(namespace, clusterName, sampleFile, env)
 
-		// First we check that the starting situation is the expected one
-		By("checking that CurrentPrimary and TargetPrimary are the same", func() {
+		// We check that the currentPrimary is the -1 instance as expected,
+		// and we define the targetPrimary (-3) and pausedReplica (-2).
+		By("checking that CurrentPrimary and TargetPrimary are equal", func() {
 			namespacedName := types.NamespacedName{
 				Namespace: namespace,
 				Name:      clusterName,
 			}
 			cluster := &clusterv1alpha1.Cluster{}
 			err := env.Client.Get(env.Ctx, namespacedName, cluster)
-			Expect(cluster.Status.CurrentPrimary, err).To(BeEquivalentTo(cluster.Status.TargetPrimary))
+			Expect(cluster.Status.CurrentPrimary, err).To(
+				BeEquivalentTo(cluster.Status.TargetPrimary))
 			currentPrimary = cluster.Status.CurrentPrimary
 
 			// Gather pod names
@@ -62,9 +72,10 @@ var _ = Describe("Failover", func() {
 			pausedReplica = pods[1]
 			targetPrimary = pods[2]
 		})
-		// We pause the replication on a standby. In this way we know that
-		// this standby will be behind the other when we do some work.
-		By("pausing the replication on the 2nd node of the Cluster", func() {
+		// We pause the walreceiver on the pausedReplica standby with a SIGSTOP.
+		// In this way we know that this standby will lag behind when
+		// we do some work on the primary.
+		By("pausing the walreceiver on the 2nd node of the Cluster", func() {
 			namespacedName := types.NamespacedName{
 				Namespace: namespace,
 				Name:      pausedReplica,
@@ -72,13 +83,57 @@ var _ = Describe("Failover", func() {
 			pausedPod := corev1.Pod{}
 			err := env.Client.Get(env.Ctx, namespacedName, &pausedPod)
 			Expect(err).ToNot(HaveOccurred())
+
+			// Get the walreceiver pid
 			timeout := time.Second * 2
-			_, _, err = env.ExecCommand(env.Ctx, pausedPod, "postgres", &timeout,
-				"psql", "-U", "postgres", "-c", "SELECT pg_wal_replay_pause()")
+			query := "SELECT pid FROM pg_stat_activity WHERE backend_type = 'walreceiver'"
+			out, _, err := env.ExecCommand(
+				env.Ctx, pausedPod, "postgres", &timeout,
+				"psql", "-U", "postgres", "-tAc", query)
 			Expect(err).ToNot(HaveOccurred())
+			pid = strings.Trim(out, "\n")
+
+			// Send the SIGSTOP
+			_, _, err = env.ExecCommand(env.Ctx, pausedPod, "postgres", &timeout,
+				"kill", "-STOP", pid)
+			Expect(err).ToNot(HaveOccurred())
+
+			// Terminate the pausedReplica walsender on the primary.
+			// We don't wont to wait for the replication timeout.
+			namespacedName = types.NamespacedName{
+				Namespace: namespace,
+				Name:      currentPrimary,
+			}
+			primaryPod := corev1.Pod{}
+			err = env.Client.Get(env.Ctx, namespacedName, &primaryPod)
+			Expect(err).ToNot(HaveOccurred())
+			query = fmt.Sprintf("SELECT pg_terminate_backend(pid) FROM pg_stat_replication "+
+				"WHERE application_name = '%v'", pausedReplica)
+			_, _, err = env.ExecCommand(
+				env.Ctx, primaryPod, "postgres", &timeout,
+				"psql", "-U", "postgres", "-tAc", query)
+			Expect(err).ToNot(HaveOccurred())
+
+			// Expect the primary to have lost connection with the stopped standby
+			timeout = time.Second * 60
+			Eventually(func() (string, error) {
+				namespacedName := types.NamespacedName{
+					Namespace: namespace,
+					Name:      currentPrimary,
+				}
+				primaryPod := corev1.Pod{}
+				err := env.Client.Get(env.Ctx, namespacedName, &primaryPod)
+				Expect(err).ToNot(HaveOccurred())
+				query := "SELECT count(*) FROM pg_stat_replication"
+				out, _, err := env.ExecCommand(
+					env.Ctx, primaryPod, "postgres", &timeout,
+					"psql", "-U", "postgres", "-tAc", query)
+				return strings.Trim(out, "\n"), err
+			}, timeout).Should(BeEquivalentTo("1"))
 		})
-		// And now we do a checkpoint and a switch wal, so we're sure
-		// the paused standby is behind
+
+		// Perform a CHECKPOINT on the primary and wait for the working standby
+		// to replicate at it
 		By("generating some WAL traffic in the Cluster", func() {
 			namespacedName := types.NamespacedName{
 				Namespace: namespace,
@@ -87,27 +142,36 @@ var _ = Describe("Failover", func() {
 			primaryPod := corev1.Pod{}
 			err := env.Client.Get(env.Ctx, namespacedName, &primaryPod)
 			Expect(err).ToNot(HaveOccurred())
+
+			// Get the current lsn
 			timeout := time.Second * 2
-			_, _, err = env.ExecCommand(env.Ctx, primaryPod, "postgres", &timeout,
-				"psql", "-U", "postgres", "-c", "CHECKPOINT; SELECT pg_switch_wal()")
+			initialLSN, _, err := env.ExecCommand(
+				env.Ctx, primaryPod, "postgres", &timeout,
+				"psql", "-U", "postgres", "-tAc", "SELECT pg_current_wal_lsn()")
 			Expect(err).ToNot(HaveOccurred())
-			commandTimeout := 60
+
+			_, _, err = env.ExecCommand(
+				env.Ctx, primaryPod, "postgres", &timeout,
+				"psql", "-U", "postgres", "-c", "CHECKPOINT")
+			Expect(err).ToNot(HaveOccurred())
+
 			// The replay_lsn of the targetPrimary should be ahead
-			// compared to the pausedReplica one
+			// of the one before the checkpoint
+			timeout = time.Second * 60
 			Eventually(func() (string, error) {
 				primaryPod := corev1.Pod{}
 				err := env.Client.Get(env.Ctx, namespacedName, &primaryPod)
 				Expect(err).ToNot(HaveOccurred())
 				query := fmt.Sprintf("SELECT true FROM pg_stat_replication "+
-					"WHERE application_name = '%v' "+
-					"AND replay_lsn > (SELECT replay_lsn "+
-					"FROM pg_stat_replication WHERE "+
-					"application_name = '%v')", targetPrimary, pausedReplica)
-				out, _, err := env.ExecCommand(env.Ctx, primaryPod, "postgres", &timeout,
+					"WHERE application_name = '%v' AND replay_lsn > '%v'",
+					targetPrimary, strings.Trim(initialLSN, "\n"))
+				out, _, err := env.ExecCommand(
+					env.Ctx, primaryPod, "postgres", &timeout,
 					"psql", "-U", "postgres", "-tAc", query)
 				return strings.TrimSpace(out), err
-			}, commandTimeout).Should(BeEquivalentTo("t"))
+			}, timeout).Should(BeEquivalentTo("t"))
 		})
+
 		// Force-delete the primary. Eventually the cluster should elect a
 		// new target primary (and we check that it's the expected one)
 		By("deleting the CurrentPrimary node to trigger a failover", func() {
@@ -122,7 +186,31 @@ var _ = Describe("Failover", func() {
 			err := env.DeletePod(namespace, currentPrimary, forceDelete)
 			Expect(err).ToNot(HaveOccurred())
 
-			timeout := 120
+			// We wait until the operator knows that the primary is dead.
+			// At this point the promotion is waiting for all the walreceivers
+			// to be disconnected. We can send the SIGCONT now.
+			timeout := 60
+			Eventually(func() (int32, error) {
+				cluster := &clusterv1alpha1.Cluster{}
+				err := env.Client.Get(env.Ctx, namespacedName, cluster)
+				return cluster.Status.ReadyInstances, err
+			}, timeout).Should(BeEquivalentTo(2))
+
+			namespacedPausedPodName := types.NamespacedName{
+				Namespace: namespace,
+				Name:      pausedReplica,
+			}
+			pausedPod := corev1.Pod{}
+			err = env.Client.Get(env.Ctx, namespacedPausedPodName, &pausedPod)
+			Expect(err).ToNot(HaveOccurred())
+			commandTimeout := time.Second * 2
+			_, _, err = env.ExecCommand(env.Ctx, pausedPod, "postgres",
+				&commandTimeout, "kill", "-CONT", pid)
+			Expect(err).ToNot(HaveOccurred())
+
+			// The operator should eventually set the cluster target primary to
+			// the instance we expect to take that role (-3).
+			timeout = 120
 			Eventually(func() (string, error) {
 				cluster := &clusterv1alpha1.Cluster{}
 				err := env.Client.Get(env.Ctx, namespacedName, cluster)
@@ -130,8 +218,12 @@ var _ = Describe("Failover", func() {
 			}, timeout).ShouldNot(BeEquivalentTo(currentPrimary))
 			cluster := &clusterv1alpha1.Cluster{}
 			err = env.Client.Get(env.Ctx, namespacedName, cluster)
-			Expect(cluster.Status.TargetPrimary, err).To(BeEquivalentTo(targetPrimary))
+			Expect(cluster.Status.TargetPrimary, err).To(
+				BeEquivalentTo(targetPrimary))
 		})
+
+		// Finally, the cluster current primary should be changed by the
+		// operator to the target primary
 		By("waiting that the TargetPrimary become also CurrentPrimary", func() {
 			namespacedName := types.NamespacedName{
 				Namespace: namespace,
