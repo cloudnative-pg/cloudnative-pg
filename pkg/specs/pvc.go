78ac56db1ac4fd078ed04e7e508c3116f78bba1f
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2021-03-29 19:47:22 +0200

    Avoid classifying new PVCs as dangling
    Before this patch, when a PVC initialization Job completes, the PVC is
    classified as dangling and the operator generates a new Pod to use it.
    
    This behavior conflicts with the maintenance window management
    when the reuse of PVC is disabled. This patch introduces
    a new "initializing" PVC status to avoid classifying the initialized PVC
    as dangling and to continue the setup of the new instance.
    
    Co-authored-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>

--- pkg/specs/pvc.go
+++ pkg/specs/pvc.go
@@ -7,17 +7,28 @@ Copyright (C) 2019-2021 EnterpriseDB Corporation.
 package specs
 
 import (
+	"strings"
+
+	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 )
 
-// DetectDanglingPVCs fill the list of the PVCs which are dangling, given that
-// PVC are usually named after Pods
-func DetectDanglingPVCs(
-	podList []corev1.Pod,
-	pvcList []corev1.PersistentVolumeClaim,
-) []string {
-	var result []string
+// PVCUsageStatus is the status of the PVC we generated
+type PVCUsageStatus struct {
+	// List of PVC that are being initialized (they have a corresponding Job but not a corresponding Pod)
+	Initializing []string
 
+	// List of PVC that are dangling (they don't have a corresponding Job nor a corresponding Pod)
+	Dangling []string
+}
+
+// DetectPVCs fill the list of the PVCs which are dangling, given that
+// PVC are usually named after Pods
+func DetectPVCs(
+	podList []corev1.Pod,
+	jobList []batchv1.Job,
+	pvcList []corev1.PersistentVolumeClaim,
+) (result PVCUsageStatus) {
 	for _, pvc := range pvcList {
 		if pvc.Status.Phase != corev1.ClaimPending &&
 			pvc.Status.Phase != corev1.ClaimBound {
@@ -28,17 +39,37 @@ func DetectDanglingPVCs(
 		if pvc.ObjectMeta.DeletionTimestamp != nil {
 			continue
 		}
-		found := false
 
+		// Find a Pod corresponding to this PVC
+		podFound := false
 		for idx := range podList {
 			if podList[idx].Name == pvc.Name {
-				found = true
+				podFound = true
 				break
 			}
 		}
 
-		if !found {
-			result = append(result, pvc.Name)
+		if podFound {
+			// We found a Pod using this PVC so this
+			// PVC is not dangling
+			continue
+		}
+
+		jobFound := false
+		for idx := range jobList {
+			if strings.HasPrefix(jobList[idx].Name, pvc.Name+"-") {
+				jobFound = true
+				break
+			}
+		}
+
+		if jobFound {
+			// We have found a Job corresponding to this PVC, so we
+			// are initializing it or the initialization is just completed
+			result.Initializing = append(result.Initializing, pvc.Name)
+		} else {
+			// This PVC has not a Job nor a Pod using it, it's dangling
+			result.Dangling = append(result.Dangling, pvc.Name)
 		}
 	}
 
