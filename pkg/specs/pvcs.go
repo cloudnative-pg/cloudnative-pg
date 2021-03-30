8edfb24a889ea72e06ab691e27d860c95120ab86
Gabriele Quaresima <72663103+gabriele-wolfox@users.noreply.github.com>
2021-03-30 17:40:59 +0200

    Merge "pkg/specs/pvc.go" and "pkg/specs/pvcs.go"
    Co-authored-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>

--- pkg/specs/pvcs.go
+++ pkg/specs/pvcs.go
@@ -9,7 +9,9 @@ package specs
 import (
 	"fmt"
 	"strconv"
+	"strings"
 
+	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -22,6 +24,15 @@ var (
 	// user is not valid and can't be specified in a PVC declaration
 	ErrorInvalidSize = fmt.Errorf("invalid storage size")
 )
+
+// PVCUsageStatus is the status of the PVC we generated
+type PVCUsageStatus struct {
+	// List of PVC that are being initialized (they have a corresponding Job but not a corresponding Pod)
+	Initializing []string
+
+	// List of PVC that are dangling (they don't have a corresponding Job nor a corresponding Pod)
+	Dangling []string
+}
 
 // CreatePVC create spec of a PVC, given its name and the storage configuration
 func CreatePVC(
@@ -71,4 +82,58 @@ func CreatePVC(
 	}
 
 	return result, nil
+}
+
+// DetectPVCs fill the list of the PVCs which are dangling, given that
+// PVC are usually named after Pods
+func DetectPVCs(
+	podList []corev1.Pod,
+	jobList []batchv1.Job,
+	pvcList []corev1.PersistentVolumeClaim,
+) (result PVCUsageStatus) {
+	for _, pvc := range pvcList {
+		if pvc.Status.Phase != corev1.ClaimPending &&
+			pvc.Status.Phase != corev1.ClaimBound {
+			continue
+		}
+
+		// There's no point in reattaching deleted PVCs
+		if pvc.ObjectMeta.DeletionTimestamp != nil {
+			continue
+		}
+
+		// Find a Pod corresponding to this PVC
+		podFound := false
+		for idx := range podList {
+			if podList[idx].Name == pvc.Name {
+				podFound = true
+				break
+			}
+		}
+
+		if podFound {
+			// We found a Pod using this PVC so this
+			// PVC is not dangling
+			continue
+		}
+
+		jobFound := false
+		for idx := range jobList {
+			if strings.HasPrefix(jobList[idx].Name, pvc.Name+"-") {
+				jobFound = true
+				break
+			}
+		}
+
+		if jobFound {
+			// We have found a Job corresponding to this PVC, so we
+			// are initializing it or the initialization is just completed
+			result.Initializing = append(result.Initializing, pvc.Name)
+		} else {
+			// This PVC has not a Job nor a Pod using it, it's dangling
+			result.Dangling = append(result.Dangling, pvc.Name)
+		}
+	}
+
+	return result
 }
