ca54812e4423266f0f8afbc381374c8c57fedc2e
jbattiato <jonathan.battiato@enterprisedb.com>
2021-03-31 16:05:27 +0200

    E2E tests for user-defined monitoring queries
    Co-authored-by: Niccolò Fei <niccolo.fei@enterprisedb.com>
    Co-authored-by: Francesco Canovai <francesco.canovai@enterprisedb.com>

--- tests/e2e/metrics_test.go
+++ tests/e2e/metrics_test.go
@@ -8,8 +8,10 @@ package e2e
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
+	"regexp"
+
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/EnterpriseDB/cloud-native-postgresql/tests"
 
@@ -20,8 +22,8 @@ import (
 var _ = Describe("Metrics", func() {
 	// Cluster identifiers
 	const namespace = "cluster-metrics-e2e"
-	const sampleFile = fixturesDir + "/base/cluster-storage-class.yaml"
-	const clusterName = "postgresql-storage-class"
+	const sampleFile = fixturesDir + "/metrics/cluster-metrics.yaml"
+	const clusterName = "postgresql-metrics"
 	JustAfterEach(func() {
 		if CurrentGinkgoTestDescription().Failed {
 			env.DumpClusterEnv(namespace, clusterName,
@@ -33,9 +35,39 @@ var _ = Describe("Metrics", func() {
 		Expect(err).ToNot(HaveOccurred())
 	})
 	It("can gather metrics", func() {
-		// Create a cluster in a namespace we'll delete after the test
+		// Create the cluster namespace
 		err := env.CreateNamespace(namespace)
 		Expect(err).ToNot(HaveOccurred())
+
+		By("verifying the custom metrics ConfigMaps and Secrets exist", func() {
+			// Create the ConfigMaps and a Secret
+			customQueries := fixturesDir + "/metrics/custom-queries.yaml"
+			_, _, err = tests.Run("kubectl apply -n " + namespace + " -f " + customQueries)
+			Expect(err).ToNot(HaveOccurred())
+
+			// Check configmaps exist
+			timeout := 20
+			Eventually(func() ([]corev1.ConfigMap, error) {
+				cmList := &corev1.ConfigMapList{}
+				err := env.Client.List(
+					env.Ctx, cmList, client.InNamespace(namespace),
+					client.MatchingLabels{"e2e": "metrics"},
+				)
+				return cmList.Items, err
+			}, timeout).Should(HaveLen(2))
+
+			// Check secret exists
+			Eventually(func() ([]corev1.Secret, error) {
+				secretList := &corev1.SecretList{}
+				err := env.Client.List(
+					env.Ctx, secretList, client.InNamespace(namespace),
+					client.MatchingLabels{"e2e": "metrics"},
+				)
+				return secretList.Items, err
+			}, timeout).Should(HaveLen(1))
+		})
+
+		// Create the cluster
 		AssertCreateCluster(namespace, clusterName, sampleFile, env)
 
 		By("collecting metrics on each pod", func() {
@@ -44,17 +76,27 @@ var _ = Describe("Metrics", func() {
 			// Gather metrics in each pod
 			for _, pod := range podList.Items {
 				pod := pod // pin the variable
-				// cnp_collector_last_collection_error returns 0 if no metric collection failed
-				metricsCmd := "sh -c 'curl -s 127.0.0.1:8000/metrics | grep ^cnp_collector_last_collection_error'"
-
+				// We define a few metrics in the tests. We check that all of them exist and
+				// there are no errors during the collection.
+				re := regexp.MustCompile(
+					`(?m:^(` +
+						`cnp_pg_postmaster_start_time_seconds \d+\.\d+|` + // wokeignore:rule=master
+						`cnp_pg_wal_files_total \d+|` +
+						`cnp_pg_database_size_bytes{datname="app"} [0-9e\+\.]+|` +
+						`cnp_pg_replication_slots_inactive 0|` +
+						`cnp_pg_stat_archiver_archived \d+|` +
+						`cnp_pg_stat_archiver_failed \d+|` +
+						`cnp_pg_locks_blocked_queries 0|` +
+						`cnp_collector_last_collection_error 0)` +
+						`$)`)
+				metricsCmd := "sh -c 'curl -s 127.0.0.1:8000/metrics'"
 				out, _, err := tests.Run(fmt.Sprintf(
 					"kubectl exec -n %v %v -- %v",
 					namespace,
 					pod.GetName(),
 					metricsCmd))
-				Expect(err).ToNot(HaveOccurred())
-				collectionError, err := strconv.Atoi(strings.Split(strings.TrimSpace(out), " ")[1])
-				Expect(collectionError, err).To(BeEquivalentTo(0))
+				matches := re.FindAllString(out, -1)
+				Expect(matches, err).To(HaveLen(8), "Metric collection issues on %v.\nCollected metrics:\n%v", pod.Name, out)
 			}
 		})
 	})
