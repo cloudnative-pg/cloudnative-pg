c724e5ea7ac0e158913a583f06595f0504803667
Philippe scorsolini <philippe.scorsolini@enterprisedb.com>
2021-05-24 16:34:32 +0200

    Removing postmaster.pid file if already present
    Co-authored-by: Gabriele Quaresima <gabriele.quaresima@enterprisedb.com>
    Co-authored-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>

---
+++ pkg/management/postgres/pidfile.go
@@ -0,0 +1,44 @@
+/*
+This file is part of Cloud Native PostgreSQL.
+
+Copyright (C) 2019-2021 EnterpriseDB Corporation.
+*/
+
+package postgres
+
+import (
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/EnterpriseDB/cloud-native-postgresql/pkg/management/log"
+)
+
+// PostgresqlPidFile is the name of the file which contains
+// the PostgreSQL PID file
+const PostgresqlPidFile = "postmaster.pid" //wokeignore:rule=master
+
+// CleanupStalePidFile deletes any stale PostgreSQL PID file
+//
+// The file is created by any instances and left in the PGDATA volume
+// in case of unclean termination.
+//
+// The presence of this file will prevent the starting instance from running.
+//
+// We assumed we don't need to check whether an actual instance is running
+// if the file is present, as the volume is mounted RWO.
+// Moreover, a running instance would die when you remove its PID file.
+func (instance *Instance) CleanupStalePidFile() error {
+	pidFile := path.Join(instance.PgData, PostgresqlPidFile)
+	err := os.Remove(pidFile)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+		return fmt.Errorf("deleting file %s: %w", pidFile, err)
+	}
+
+	log.Log.Info("Deleted stale PostgreSQL pid file from PGDATA directory")
+
+	return nil
+}
---
+++ pkg/management/postgres/pidfile_test.go
@@ -0,0 +1,43 @@
+/*
+This file is part of Cloud Native PostgreSQL.
+
+Copyright (C) 2019-2021 EnterpriseDB Corporation.
+*/
+
+package postgres
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("the cleanup of a stale PostgreSQL pid file", func() {
+	var tmpDir string
+
+	var _ = BeforeEach(func() {
+		var err error
+		tmpDir, err = ioutil.TempDir("", "cleanup-stale-pid-file-")
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	var _ = AfterEach(func() {
+		Expect(os.RemoveAll(tmpDir)).To(Succeed())
+	})
+
+	It("works if the file is not present", func() {
+		instance := &Instance{PgData: tmpDir}
+		err := instance.CleanupStalePidFile()
+		Expect(err).ShouldNot(HaveOccurred())
+	})
+	It("works if the file is present", func() {
+		instance := &Instance{PgData: tmpDir}
+		err := ioutil.WriteFile(filepath.Join(tmpDir, PostgresqlPidFile), []byte("1234"), 0o400)
+		Expect(err).ShouldNot(HaveOccurred())
+		err = instance.CleanupStalePidFile()
+		Expect(err).ShouldNot(HaveOccurred())
+	})
+})
