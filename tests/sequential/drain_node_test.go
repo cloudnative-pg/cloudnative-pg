7bb8521ab6f48c88fc3d735df3dffaabee94fb8f
Jitendra Wadle <jitendra.wadle@enterprisedb.com>
2021-08-10 17:20:59 +0530

    test(E2E): improve drain node
    Test a scenario where all the pods reside on a single node,
    a second node is available and PVCs can be used by all the nodes.
    
    Co-authored-by: Danish Khan <danish.khan@enterprisedb.com>
    Co-authored-by: Francesco Canovai <francesco.canovai@enterprisedb.com>

--- tests/sequential/drain_node_test.go
+++ tests/sequential/drain_node_test.go
@@ -21,14 +21,16 @@ import (
 	"github.com/EnterpriseDB/cloud-native-postgresql/tests"
 )
 
-// Set of tests in which we check that operator is able to failover primary and bring back
-// replica's when we drain node
+// Set of tests in which we check that operator is able to fail over a new
+// primary and bring back the replicas when we drain nodes
 var _ = Describe("E2E Drain Node", func() {
 	var nodesWithLabels []string
 
 	BeforeEach(func() {
 		nodes, _ := env.GetNodeList()
-		// We label all the nodes where we could run the workloads
+		// We label three nodes where we could run the workloads, and ignore
+		// the others. The pods of the clusters created in this test run only
+		// where the drain label exists.
 		for _, node := range nodes.Items {
 			if (node.Spec.Unschedulable != true) && (len(node.Spec.Taints) == 0) {
 				nodesWithLabels = append(nodesWithLabels, node.Name)
@@ -45,6 +47,8 @@ var _ = Describe("E2E Drain Node", func() {
 	})
 
 	AfterEach(func() {
+		// Uncordon the cordoned nodes and remove the labels we added in the
+		// BeforeEach section
 		UncordonAllNodes()
 		for _, node := range nodesWithLabels {
 			cmd := fmt.Sprintf("kubectl label node %v drain- ", node)
@@ -56,7 +60,7 @@ var _ = Describe("E2E Drain Node", func() {
 
 	Context("Maintenance on, reuse pvc on", func() {
 		// Initialize empty global namespace variable
-		const namespace = "drain-node-e2e-pvc-on-two-nodes"
+		var namespace string
 		const sampleFile = fixturesDir + "/drain-node/cluster-drain-node.yaml"
 		const clusterName = "cluster-drain-node"
 
@@ -80,12 +84,9 @@ var _ = Describe("E2E Drain Node", func() {
 		// the storage is bound to the node, we have to uncordon the node
 		// first. We uncordon it in all cases and check for the UIDs of the
 		// PVC(s).
-		// TODO: since the podDisruptionBudget is dropped when reusePVC
-		// is on, if case the pods are all on the same node the drain will
-		// fail. However, this shouldn't happen unless one of the two nodes
-		// is overloaded, since the anti-affinity should keep pods away from
-		// each other.
-		It("can drain the primary pod's node", func() {
+
+		It("can drain the primary pod's node with 3 pods on 2 nodes", func() {
+			namespace = "drain-node-e2e-pvc-on-two-nodes"
 			By("leaving only two nodes uncordoned", func() {
 				// mark a node unschedulable so the pods will be distributed only on two nodes
 				for _, cordonNode := range nodesWithLabels[:len(nodesWithLabels)-2] {
@@ -118,6 +119,7 @@ var _ = Describe("E2E Drain Node", func() {
 			// We take advantage of the fact that related PVCs and Pods have
 			// the same name.
 			podList, err := env.GetClusterPodList(namespace, clusterName)
+			Expect(err).ToNot(HaveOccurred())
 			pvcUIDMap := make(map[string]types.UID)
 			for _, pod := range podList.Items {
 				pvcNamespacedName := types.NamespacedName{
@@ -131,18 +133,16 @@ var _ = Describe("E2E Drain Node", func() {
 			}
 
 			// Drain the node containing the primary pod and store the list of running pods
-			podsOnPrimaryNode := tryDrainingPrimaryNode(namespace, clusterName)
+			podsOnPrimaryNode := drainPrimaryNode(namespace, clusterName)
 
-			if len(podsOnPrimaryNode) < len(podList.Items) {
-				By("verifying failover after drain", func() {
-					timeout := 180
-					// Expect a failover to have happened
-					Eventually(func() (string, error) {
-						pod, err := env.GetClusterPrimary(namespace, clusterName)
-						return pod.Name, err
-					}, timeout).ShouldNot(BeEquivalentTo(oldPrimary))
-				})
-			}
+			By("verifying failover after drain", func() {
+				timeout := 180
+				// Expect a failover to have happened
+				Eventually(func() (string, error) {
+					pod, err := env.GetClusterPrimary(namespace, clusterName)
+					return pod.Name, err
+				}, timeout).ShouldNot(BeEquivalentTo(oldPrimary))
+			})
 
 			By("uncordon nodes and check new pods use old pvcs", func() {
 				UncordonAllNodes()
@@ -172,6 +172,124 @@ var _ = Describe("E2E Drain Node", func() {
 			// Expect the (previously created) test data to be available
 			AssertTestDataExistence(namespace, clusterName)
 			assertClusterStandbysAreStreaming(namespace, clusterName)
+		})
+
+		// Scenario: all the pods of a cluster are on a single node and another schedulable node exists.
+		// We perform the drain the node hosting the primary.
+		// If PVCs can be moved: all the replicas will be killed and rescheduled to a different node,
+		// then a switchover will be triggered, and the old primary will be killed and moved too.
+		// The drain will succeed.
+		// We have skipped this scenario on the Local executors, Openshift, EKS, RKE
+		// because here PVCs can not be moved, so this all replicas should be killed and can not be rescheduled on a
+		// new node as there are none, the primary node can not be killed, therefore the drain fails.
+
+		When("the cluster allows moving PVCs between nodes", func() {
+			BeforeEach(func() {
+				// AKS using rook and the standard GKE StorageClass allow moving PVCs between nodes
+				isAKS, err := env.IsAKS()
+				Expect(err).ToNot(HaveOccurred())
+				isGKE, err := env.IsGKE()
+				Expect(err).ToNot(HaveOccurred())
+				if !(isAKS || isGKE) {
+					Skip("This test case is only applicable on clusters where PVC can be moved")
+				}
+			})
+			It("can drain the primary pod's node with 3 pods on 1 nodes", func() {
+				namespace = "drain-node-e2e-pvc-on-one-nodes"
+				var cordonNodes []string
+				By("leaving only one node uncordoned", func() {
+					// cordon all nodes but one
+					for _, cordonNode := range nodesWithLabels[:len(nodesWithLabels)-1] {
+						cordonNodes = append(cordonNodes, cordonNode)
+						cmd := fmt.Sprintf("kubectl cordon %v", cordonNode)
+						_, _, err := tests.Run(cmd)
+						Expect(err).ToNot(HaveOccurred())
+					}
+				})
+
+				// Create a cluster in a namespace we'll delete after the test
+				err := env.CreateNamespace(namespace)
+				Expect(err).ToNot(HaveOccurred())
+				AssertCreateCluster(namespace, clusterName, sampleFile, env)
+
+				By("waiting for the jobs to be removed", func() {
+					// Wait for jobs to be removed
+					timeout := 180
+					Eventually(func() (int, error) {
+						podList, err := env.GetPodList(namespace)
+						return len(podList.Items), err
+					}, timeout).Should(BeEquivalentTo(3))
+				})
+
+				// Load test data
+				oldPrimary := clusterName + "-1"
+				AssertTestDataCreation(namespace, clusterName)
+
+				// We create a mapping between the pod names and the UIDs of
+				// their volumes. We do not expect the UIDs to change.
+				// We take advantage of the fact that related PVCs and Pods have
+				// the same name.
+				podList, err := env.GetClusterPodList(namespace, clusterName)
+				pvcUIDMap := make(map[string]types.UID)
+				for _, pod := range podList.Items {
+					pvcNamespacedName := types.NamespacedName{
+						Namespace: namespace,
+						Name:      pod.Name,
+					}
+					pvc := corev1.PersistentVolumeClaim{}
+					err = env.Client.Get(env.Ctx, pvcNamespacedName, &pvc)
+					Expect(err).ToNot(HaveOccurred())
+					pvcUIDMap[pod.Name] = pvc.GetUID()
+				}
+
+				// We uncordon a cordoned node, so there will be a node for the PVCs
+				// to move to.
+				By(fmt.Sprintf("uncordon one more node '%v'", cordonNodes[0]), func() {
+					cmd := fmt.Sprintf("kubectl uncordon %v", cordonNodes[0])
+					_, _, err = tests.Run(cmd)
+					Expect(err).ToNot(HaveOccurred())
+				})
+
+				// Drain the node containing the primary pod and store the list of running pods
+				podsOnPrimaryNode := drainPrimaryNode(namespace, clusterName)
+
+				By("verifying failover after drain", func() {
+					timeout := 180
+					// Expect a failover to have happened
+					Eventually(func() (string, error) {
+						pod, err := env.GetClusterPrimary(namespace, clusterName)
+						return pod.Name, err
+					}, timeout).ShouldNot(BeEquivalentTo(oldPrimary))
+				})
+
+				By("check new pods use old pvcs", func() {
+					// Ensure evicted pods have restarted and are running.
+					// one of them could have become the new primary.
+					timeout := 300
+					for _, podName := range podsOnPrimaryNode {
+						namespacedName := types.NamespacedName{
+							Namespace: namespace,
+							Name:      podName,
+						}
+						Eventually(func() (bool, error) {
+							pod := corev1.Pod{}
+							err := env.Client.Get(env.Ctx, namespacedName, &pod)
+							return utils.IsPodActive(pod) && utils.IsPodReady(pod), err
+						}, timeout).Should(BeTrue())
+
+						pod := corev1.Pod{}
+						err = env.Client.Get(env.Ctx, namespacedName, &pod)
+						// Check that the PVC UID hasn't changed
+						pvc := corev1.PersistentVolumeClaim{}
+						err = env.Client.Get(env.Ctx, namespacedName, &pvc)
+						Expect(pvc.GetUID(), err).To(BeEquivalentTo(pvcUIDMap[podName]))
+					}
+				})
+
+				// Expect the (previously created) test data to be available
+				AssertTestDataExistence(namespace, clusterName)
+				assertClusterStandbysAreStreaming(namespace, clusterName)
+			})
 		})
 	})
 
@@ -318,35 +436,6 @@ func drainPrimaryNode(namespace string, clusterName string) []string {
 			}
 			return usedNodes, err
 		}, timeout).ShouldNot(ContainElement(primaryNode))
-	})
-
-	return podNames
-}
-
-// tryDrainingPrimaryNode drains the node containing the primary pod.
-// It returns the names of the pods that were running on that node
-func tryDrainingPrimaryNode(namespace string, clusterName string) []string {
-	var primaryNode string
-	var podNames []string
-	By("identifying primary node and try draining", func() {
-		pod, err := env.GetClusterPrimary(namespace, clusterName)
-		Expect(err).ToNot(HaveOccurred())
-		primaryNode = pod.Spec.NodeName
-
-		// Gather the pods running on this node
-		podList, err := env.GetClusterPodList(namespace, clusterName)
-		Expect(err).ToNot(HaveOccurred())
-		for _, pod := range podList.Items {
-			if pod.Spec.NodeName == primaryNode {
-				podNames = append(podNames, pod.Name)
-			}
-		}
-
-		timeout := 300
-		// should set a timeout otherwise will hang forever
-		cmd := fmt.Sprintf("kubectl drain %v --ignore-daemonsets --delete-local-data --force --timeout=%ds",
-			primaryNode, timeout)
-		_, _, _ = tests.RunUnchecked(cmd)
 	})
 
 	return podNames
