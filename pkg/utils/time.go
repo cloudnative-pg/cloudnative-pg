8288f903867a6953081ec068339b9e22b891be96
Gabriele Quaresima <gabriele.quaresima@enterprisedb.com>
2021-09-02 19:02:29 +0200

    chore: add support for RFC3339 format in target time
    Co-authored-by: Philippe Scorsolini <philippe.scorsolini@enterprisedb.com>

---
+++ pkg/utils/time.go
@@ -0,0 +1,22 @@
+/*
+This file is part of Cloud Native PostgreSQL.
+
+Copyright (C) 2019-2021 EnterpriseDB Corporation.
+*/
+
+package utils
+
+import (
+	"time"
+)
+
+// ConvertToPostgresFormat converts timestamps to PostgreSQL time format, if needed.
+// e.g. "2006-01-02T15:04:05Z07:00" --> "2006-01-02 15:04:05.000000Z07:00"
+// If the conversion fails, the input timestamp is returned as it is.
+func ConvertToPostgresFormat(timestamp string) string {
+	parsedTimestamp, err := time.Parse(time.RFC3339, timestamp)
+	if err != nil {
+		return timestamp
+	}
+	return parsedTimestamp.Format("2006-01-02 15:04:05.000000Z07:00")
+}
