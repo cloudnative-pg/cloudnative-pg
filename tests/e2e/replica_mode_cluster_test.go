f26a26ce788840670956930566be00b0ea84851e
Jitendra Wadle <jitendra.wadle@enterprisedb.com>
2021-10-05 06:18:28 +0530

    test(E2E): add test for "archive_mode" set to "always" for designated primary
    Co-authored-by: Gabriele Quaresima <gabriele.quaresima@enterprisedb.com>
    Co-authored-by: Francesco Canovai <francesco.canovai@enterprisedb.com>

--- tests/e2e/replica_mode_cluster_test.go
+++ tests/e2e/replica_mode_cluster_test.go
@@ -19,80 +19,62 @@ import (
 )
 
 var _ = Describe("Replica Mode", func() {
-	commandTimeout := time.Second * 2
+	const (
+		replicaModeClusterDir = "/replica_mode_cluster/"
+		srcClusterName        = "cluster-replica-src"
+		srcClusterSample      = fixturesDir + replicaModeClusterDir + srcClusterName + ".yaml"
+		checkQuery            = "SELECT count(*) FROM test_replica"
+	)
+
+	// Setting variables
+	var replicaClusterName, replicaNamespace string
+	replicaCommandTimeout := time.Second * 2
+
+	JustAfterEach(func() {
+		if CurrentSpecReport().Failed() {
+			env.DumpClusterEnv(replicaNamespace, srcClusterName,
+				"out/"+CurrentSpecReport().LeafNodeText+"-source-cluster.log")
+			env.DumpClusterEnv(replicaNamespace, replicaClusterName,
+				"out/"+CurrentSpecReport().LeafNodeText+"-replica-cluster.log")
+		}
+	})
+	AfterEach(func() {
+		err := env.DeleteNamespace(replicaNamespace)
+		Expect(err).ToNot(HaveOccurred())
+	})
 
 	Context("can bootstrap a replica cluster using TLS auth", func() {
-		const (
-			replicaModeClusterDir   = "/replica_mode_cluster/"
-			srcClusterName          = "cluster-replica-src"
-			srcClusterSample        = fixturesDir + replicaModeClusterDir + srcClusterName + ".yaml"
-			replicaClusterSampleTLS = fixturesDir + replicaModeClusterDir + "cluster-replica-tls.yaml"
-			checkQuery              = "SELECT count(*) FROM test_replica"
-		)
-
-		// Setting context variables
-		namespace := "replica-mode-tls-auth"
-		replicaClusterName := "cluster-replica-tls"
-
-		JustAfterEach(func() {
-			if CurrentSpecReport().Failed() {
-				env.DumpClusterEnv(namespace, srcClusterName,
-					"out/"+CurrentSpecReport().LeafNodeText+"-source-cluster.log")
-				env.DumpClusterEnv(namespace, replicaClusterName,
-					"out/"+CurrentSpecReport().LeafNodeText+"-replica-cluster.log")
-			}
-		})
-		AfterEach(func() {
-			err := env.DeleteNamespace(namespace)
-			Expect(err).ToNot(HaveOccurred())
-		})
+		const replicaClusterSampleTLS = fixturesDir + replicaModeClusterDir + "cluster-replica-tls.yaml"
 
 		It("should work", func() {
-			AssertReplicaModeCluster(namespace, srcClusterName, srcClusterSample, replicaClusterName,
+			replicaNamespace = "replica-mode-tls-auth"
+			replicaClusterName = "cluster-replica-tls"
+			err := env.CreateNamespace(replicaNamespace)
+			Expect(err).ToNot(HaveOccurred())
+			AssertReplicaModeCluster(replicaNamespace, srcClusterName, srcClusterSample, replicaClusterName,
 				replicaClusterSampleTLS, checkQuery)
 		})
 	})
 
 	Context("can bootstrap a replica cluster using basic auth", func() {
-		const (
-			replicaModeClusterDir         = "/replica_mode_cluster/"
-			srcClusterName                = "cluster-replica-src"
-			srcClusterSample              = fixturesDir + replicaModeClusterDir + srcClusterName + ".yaml"
-			replicaClusterSampleBasicAuth = fixturesDir + replicaModeClusterDir + "cluster-replica-basicauth.yaml"
-			checkQuery                    = "SELECT count(*) FROM test_replica"
-		)
+		const replicaClusterSampleBasicAuth = fixturesDir + replicaModeClusterDir + "cluster-replica-basicauth.yaml"
 
-		// Setting context variables
-		namespace := "replica-mode-basic-auth"
-		replicaClusterName := "cluster-replica-basicauth"
-
-		JustAfterEach(func() {
-			if CurrentSpecReport().Failed() {
-				env.DumpClusterEnv(namespace, srcClusterName,
-					"out/"+CurrentSpecReport().LeafNodeText+"-source-cluster.log")
-				env.DumpClusterEnv(namespace, replicaClusterName,
-					"out/"+CurrentSpecReport().LeafNodeText+"-replica-cluster.log")
-			}
-		})
-		AfterEach(func() {
-			err := env.DeleteNamespace(namespace)
-			Expect(err).ToNot(HaveOccurred())
-		})
-
-		var primarySrcCluster, primaryReplicaCluster *corev1.Pod
+		var primaryReplicaCluster *corev1.Pod
 		var err error
 
 		It("still works detached from remote server promoting the designated primary", func() {
+			replicaNamespace = "replica-mode-basic-auth"
+			replicaClusterName = "cluster-replica-basicauth"
+
+			err = env.CreateNamespace(replicaNamespace)
+			Expect(err).ToNot(HaveOccurred())
 			// Create a cluster first
 			By("creating a replica cluster", func() {
-				AssertReplicaModeCluster(namespace, srcClusterName, srcClusterSample,
+				AssertReplicaModeCluster(replicaNamespace, srcClusterName, srcClusterSample,
 					replicaClusterName, replicaClusterSampleBasicAuth, checkQuery)
-				// Get primary from source cluster
-				primarySrcCluster, err = env.GetClusterPrimary(namespace, srcClusterName)
-				Expect(err).ToNot(HaveOccurred())
 
 				// Get primary from replica cluster
-				primaryReplicaCluster, err = env.GetClusterPrimary(namespace, replicaClusterName)
+				primaryReplicaCluster, err = env.GetClusterPrimary(replicaNamespace, replicaClusterName)
 				Expect(err).ToNot(HaveOccurred())
 			})
 
@@ -100,7 +82,7 @@ var _ = Describe("Replica Mode", func() {
 				_, _, err = tests.Run(fmt.Sprintf(
 					"kubectl patch cluster %v -n %v  -p '{\"spec\":{\"replica\":{\"enabled\":false}}}'"+
 						" --type='merge'",
-					replicaClusterName, namespace))
+					replicaClusterName, replicaNamespace))
 				Expect(err).ToNot(HaveOccurred())
 			})
 
@@ -108,7 +90,7 @@ var _ = Describe("Replica Mode", func() {
 				query := "select pg_is_in_recovery();"
 				Eventually(func() (string, error) {
 					stdOut, _, err := env.ExecCommand(env.Ctx, *primaryReplicaCluster, "postgres",
-						&commandTimeout, "psql", "-U", "postgres", "app", "-tAc", query)
+						&replicaCommandTimeout, "psql", "-U", "postgres", "app", "-tAc", query)
 					return strings.Trim(stdOut, "\n"), err
 				}, 300, 15).Should(BeEquivalentTo("f"))
 			})
@@ -117,96 +99,61 @@ var _ = Describe("Replica Mode", func() {
 				query := "CREATE TABLE replica_cluster_primary AS VALUES (1), (2);"
 				// Expect write operation to succeed
 				_, _, err := env.ExecCommand(env.Ctx, *primaryReplicaCluster, "postgres",
-					&commandTimeout, "psql", "-U", "postgres", "app", "-tAc", query)
+					&replicaCommandTimeout, "psql", "-U", "postgres", "app", "-tAc", query)
 				Expect(err).ToNot(HaveOccurred())
 			})
 
 			By("writing some new data to the source cluster", func() {
-				query := "INSERT INTO test_replica VALUES (4);"
-				_, _, err := env.ExecCommand(env.Ctx, *primarySrcCluster, "postgres",
-					&commandTimeout, "psql", "-U", "postgres", "app", "-tAc", query)
+				err := insertRecordIntoTable(replicaNamespace, srcClusterName, "test_replica", 4)
 				Expect(err).ToNot(HaveOccurred())
 			})
 
 			By("verifying that replica cluster was not modified", func() {
-				stdOut, _, err := env.ExecCommand(env.Ctx, *primaryReplicaCluster, "postgres",
-					&commandTimeout, "psql", "-U", "postgres", "app", "-tAc", checkQuery)
-				Expect(err).ToNot(HaveOccurred())
-				Expect(strings.Trim(stdOut, "\n")).To(BeEquivalentTo("3"))
+				AssertDataExpectedCount(replicaNamespace, primaryReplicaCluster.Name, "test_replica", 3)
+			})
+		})
+	})
+
+	Context("archive mode set to 'always' on designated primary", Focus, func() {
+		It("verify replica cluster can archive WALs from the designated primary", Focus, func() {
+			const replicaClusterSample = fixturesDir + replicaModeClusterDir + "cluster-replica-archive-mode-always.yaml"
+
+			replicaNamespace = "replica-mode-archive"
+			err := env.CreateNamespace(replicaNamespace)
+			Expect(err).ToNot(HaveOccurred())
+
+			replicaClusterName, err := env.GetResourceNameFromYAML(replicaClusterSample)
+			Expect(err).ToNot(HaveOccurred())
+			By("creating the credentials for minio", func() {
+				AssertStorageCredentialsAreCreated(replicaNamespace, "backup-storage-creds", "minio", "minio123")
+			})
+			By("setting up minio", func() {
+				InstallMinio(replicaNamespace)
+			})
+			// Create the minio client pod and wait for it to be ready.
+			// We'll use it to check if everything is archived correctly
+			By("setting up minio client pod", func() {
+				InstallMinioClient(replicaNamespace)
+			})
+
+			AssertReplicaModeCluster(replicaNamespace, srcClusterName, srcClusterSample, replicaClusterName,
+				replicaClusterSample, checkQuery)
+
+			// Get primary from replica cluster
+			primaryReplicaCluster, err := env.GetClusterPrimary(replicaNamespace, replicaClusterName)
+			Expect(err).ToNot(HaveOccurred())
+
+			By("verify archive mode is set to 'always on' designated primary", func() {
+				query := "show archive_mode;"
+				Eventually(func() (string, error) {
+					stdOut, _, err := env.ExecCommand(env.Ctx, *primaryReplicaCluster, "postgres",
+						&commandTimeout, "psql", "-U", "postgres", "app", "-tAc", query)
+					return strings.Trim(stdOut, "\n"), err
+				}, 30).Should(BeEquivalentTo("always"))
+			})
+			By("verify the WALs are archived from the designated primary", func() {
+				AssertArchiveWalOnMinio(replicaNamespace, srcClusterName)
 			})
 		})
 	})
 })
-
-func AssertReplicaModeCluster(
-	namespace,
-	srcClusterName,
-	srcClusterSample,
-	replicaClusterName,
-	replicaClusterSample,
-	checkQuery string) {
-	commandTimeout := time.Second * 2
-	var primarySrcCluster, primaryReplicaCluster *corev1.Pod
-	var err error
-
-	err = env.CreateNamespace(namespace)
-	Expect(err).ToNot(HaveOccurred())
-
-	By("creating source cluster", func() {
-		// Create replica source cluster
-		AssertCreateCluster(namespace, srcClusterName, srcClusterSample, env)
-		// Get primary from source cluster
-		Eventually(func() error {
-			primarySrcCluster, err = env.GetClusterPrimary(namespace, srcClusterName)
-			return err
-		}, 5).Should(BeNil())
-	})
-
-	By("creating test data in source cluster", func() {
-		cmd := "CREATE TABLE test_replica AS VALUES (1), (2);"
-		_, _, err := env.ExecCommand(env.Ctx, *primarySrcCluster, "postgres",
-			&commandTimeout, "psql", "-U", "postgres", "app", "-tAc", cmd)
-		Expect(err).ToNot(HaveOccurred())
-	})
-
-	By("creating replica cluster", func() {
-		AssertCreateCluster(namespace, replicaClusterName, replicaClusterSample, env)
-		// Get primary from replica cluster
-		Eventually(func() error {
-			primaryReplicaCluster, err = env.GetClusterPrimary(namespace, replicaClusterName)
-			return err
-		}, 5).Should(BeNil())
-	})
-
-	By("verifying that replica cluster primary is in recovery mode", func() {
-		query := "select pg_is_in_recovery();"
-		Eventually(func() (string, error) {
-			stdOut, _, err := env.ExecCommand(env.Ctx, *primaryReplicaCluster, "postgres",
-				&commandTimeout, "psql", "-U", "postgres", "app", "-tAc", query)
-			return strings.Trim(stdOut, "\n"), err
-		}, 300, 15).Should(BeEquivalentTo("t"))
-	})
-
-	By("checking data have been copied correctly in replica cluster", func() {
-		Eventually(func() (string, error) {
-			stdOut, _, err := env.ExecCommand(env.Ctx, *primaryReplicaCluster, "postgres",
-				&commandTimeout, "psql", "-U", "postgres", "app", "-tAc", checkQuery)
-			return strings.Trim(stdOut, "\n"), err
-		}, 180, 10).Should(BeEquivalentTo("2"))
-	})
-
-	By("writing some new data to the source cluster", func() {
-		cmd := "INSERT INTO test_replica VALUES (3);"
-		_, _, err := env.ExecCommand(env.Ctx, *primarySrcCluster, "postgres",
-			&commandTimeout, "psql", "-U", "postgres", "app", "-tAc", cmd)
-		Expect(err).ToNot(HaveOccurred())
-	})
-
-	By("checking new data have been copied correctly in replica cluster", func() {
-		Eventually(func() (string, error) {
-			stdOut, _, err := env.ExecCommand(env.Ctx, *primaryReplicaCluster, "postgres",
-				&commandTimeout, "psql", "-U", "postgres", "app", "-tAc", checkQuery)
-			return strings.Trim(stdOut, "\n"), err
-		}, 180, 15).Should(BeEquivalentTo("3"))
-	})
-}
