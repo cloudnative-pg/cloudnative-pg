07d6a37f2d07151a2840784848ac9e37a174c9ba
Marco Nenciarini <marco.nenciarini@enterprisedb.com>
2021-10-11 13:00:16 +0200

    fix: Use 127.0.0.1 instead of localhost in setup-cluster.sh script

--- hack/setup-cluster.sh
+++ hack/setup-cluster.sh
@@ -312,7 +312,7 @@ deploy_fluentd() {
 load_image_registry() {
   local image=$1
 
-  local image_local_name=${image/${registry_name}/localhost}
+  local image_local_name=${image/${registry_name}/127.0.0.1}
   docker tag "${image}" "${image_local_name}"
   docker push "${image_local_name}"
 }
