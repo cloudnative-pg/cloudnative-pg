75425ab7906b2ee91f83e1c5c48065f76ddf0de5
Marco Nenciarini <marco.nenciarini@enterprisedb.com>
2021-11-12 17:05:17 +0100

    chore: refactor cache usage in manager binary
    Co-authored-by: Jihyuk Bok <jihyuk.bok@enterprisedb.com>

--- internal/management/cache/client/client.go
+++ internal/management/cache/client/client.go
@@ -9,116 +9,41 @@ Copyright (C) 2019-2021 EnterpriseDB Corporation.
 package client
 
 import (
-	"context"
 	"encoding/json"
-	"errors"
-	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
 
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	"k8s.io/client-go/util/retry"
 
 	apiv1 "github.com/EnterpriseDB/cloud-native-postgresql/api/v1"
 	"github.com/EnterpriseDB/cloud-native-postgresql/internal/management/cache"
-	barmanCredentials "github.com/EnterpriseDB/cloud-native-postgresql/pkg/management/barman/credentials"
 	"github.com/EnterpriseDB/cloud-native-postgresql/pkg/management/url"
 )
 
-// GetCluster gets the required cluster from cache or from the APIServer in case of cache miss
-func GetCluster(ctx context.Context,
-	typedClient client.Client,
-	namespace string,
-	name string,
-) (*apiv1.Cluster, error) {
-	var cluster *apiv1.Cluster
-	cached := true
-
-	cluster, err := getClusterFromCacheEndpoint()
-	if errors.Is(err, cache.ErrCacheMiss) {
-		cached = false
-	} else if err != nil {
-		return nil, fmt.Errorf("failed to get cluster: %w", err)
-	}
-
-	if cached {
-		return cluster, nil
-	}
-
-	cluster = &apiv1.Cluster{}
-
-	err = typedClient.Get(ctx, client.ObjectKey{
-		Namespace: namespace,
-		Name:      name,
-	}, cluster)
+// GetCluster gets the required cluster from cache
+func GetCluster() (*apiv1.Cluster, error) {
+	bytes, err := httpCacheGet(cache.ClusterKey)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get cluster: %w", err)
+		return nil, err
+	}
+
+	cluster := &apiv1.Cluster{}
+	err = json.Unmarshal(bytes, cluster)
+	if err != nil {
+		return nil, err
 	}
 
 	return cluster, nil
 }
 
-// GetEnv gets the environment variables from cache or builds them in case of cache
-// miss
-func GetEnv(ctx context.Context,
-	typedClient client.Client,
-	namespace string,
-	config *apiv1.BarmanObjectStoreConfiguration,
-	key string,
-) ([]string, error) {
+// GetEnv gets the environment variables from cache
+func GetEnv(key string) ([]string, error) {
+	bytes, err := httpCacheGet(key)
+	if err != nil {
+		return nil, err
+	}
+
 	var env []string
-	cached := true
-
-	env, err := getEnvFromCacheEndpoint(key)
-	if errors.Is(err, cache.ErrCacheMiss) {
-		cached = false
-	} else if err != nil {
-		return nil, fmt.Errorf("failed to get cluster: %w", err)
-	}
-
-	if cached {
-		return env, nil
-	}
-
-	env, err = barmanCredentials.EnvSetCloudCredentials(
-		ctx,
-		typedClient,
-		namespace,
-		config,
-		os.Environ())
-	if err != nil {
-		return nil, fmt.Errorf("failed to get cloud credentials: %w", err)
-	}
-
-	return env, nil
-}
-
-// getClusterFromCacheEndpoint retrieves the cluster from the cache
-func getClusterFromCacheEndpoint() (*apiv1.Cluster, error) {
-	var cluster apiv1.Cluster
-
-	bytes, err := getFromCache(cache.ClusterKey)
-	if err != nil {
-		return nil, err
-	}
-
-	err = json.Unmarshal(bytes, &cluster)
-	if err != nil {
-		return nil, err
-	}
-
-	return &cluster, nil
-}
-
-// getEnvFromCacheEndpoint retrieves the list of environment variables from the 'cachePath' file
-func getEnvFromCacheEndpoint(c string) ([]string, error) {
-	var env []string
-
-	bytes, err := getFromCache(c)
-	if err != nil {
-		return nil, err
-	}
-
 	err = json.Unmarshal(bytes, &env)
 	if err != nil {
 		return nil, err
@@ -127,7 +52,23 @@ func getEnvFromCacheEndpoint(c string) ([]string, error) {
 	return env, nil
 }
 
-func getFromCache(urlPath string) ([]byte, error) {
+// httpCacheGet retrieves an object from the cache.
+// In case of failures it retries for a while before giving up
+func httpCacheGet(urlPath string) ([]byte, error) {
+	var bytes []byte
+	err := retry.OnError(retry.DefaultBackoff, func(error) bool { return true }, func() error {
+		var err error
+		bytes, err = get(urlPath)
+		return err
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes, nil
+}
+
+func get(urlPath string) ([]byte, error) {
 	resp, err := http.Get(url.Local(url.PathCache+urlPath, url.LocalPort))
 	if err != nil {
 		return nil, err
