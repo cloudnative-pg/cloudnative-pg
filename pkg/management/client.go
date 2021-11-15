7d823e28223ba38640a21bd42e7ac0644407037e
Jihyuk Bok <jihyuk.bok@enterprisedb.com>
2021-11-15 12:18:29 +0100

    fix: Using only limited number of resource for controller-runtime client creation
    This change should avoid receiving warnings like the following in the instance manager logs
    
        Waited for 1.176168644s due to client-side throttling, not priority and fairness
    
    Co-authored-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- pkg/management/client.go
+++ pkg/management/client.go
@@ -10,13 +10,16 @@ package management
 
 import (
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/kubernetes"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/record"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 
 	apiv1 "github.com/EnterpriseDB/cloud-native-postgresql/api/v1"
 )
@@ -30,16 +33,40 @@ func init() {
 }
 
 // NewControllerRuntimeClient creates a new typed K8s client where
-// the PostgreSQL CRD has already been registered
+// the PostgreSQL CRD and some basic k8s resources have been already registered.
+//
+// While using the typed client you may encounter an error like this:
+// no matches for kind "X" in version "Y".
+//
+// This means that the runtime.Object is missing and needs to registered in the client.
 func NewControllerRuntimeClient() (client.WithWatch, error) {
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		return nil, err
 	}
 
+	mapper := meta.NewDefaultRESTMapper([]schema.GroupVersion{apiv1.GroupVersion})
+	// add here any resource that need to be registered.
+	objectsToRegister := []runtime.Object{
+		// custom resources
+		&apiv1.Cluster{}, &apiv1.Backup{}, &apiv1.Pooler{},
+		// k8s resources needed for the typedClient to work properly
+		&v1.ConfigMap{}, &v1.Secret{},
+	}
+
+	// we register the resources
+	for _, obj := range objectsToRegister {
+		gvk, err := apiutil.GVKForObject(obj, Scheme)
+		if err != nil {
+			return nil, err
+		}
+
+		mapper.Add(gvk, meta.RESTScopeNamespace)
+	}
+
 	return client.NewWithWatch(config, client.Options{
 		Scheme: Scheme,
-		Mapper: nil,
+		Mapper: mapper,
 	})
 }
 
