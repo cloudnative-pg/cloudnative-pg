9d0067194e714ed211c5f3d53465c70f7110eac5
armru <armando.ruocco@enterprisedb.com>
2021-11-17 17:53:22 +0100

    feat: add ownership to operator secrets
    Co-authored-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>

--- pkg/utils/ownership.go
+++ pkg/utils/ownership.go
@@ -6,7 +6,12 @@ Copyright (C) 2019-2021 EnterpriseDB Corporation.
 
 package utils
 
-import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (
+	"context"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+)
 
 // SetAsOwnedBy sets the controlled object as owned by a certain other
 // controller object with his type information
@@ -22,4 +27,32 @@ func SetAsOwnedBy(controlled *metav1.ObjectMeta, controller metav1.ObjectMeta, t
 			Controller: &isController,
 		},
 	})
+}
+
+// SetAsOwnedByOperatorDeployment sets the controlled object as owned by the operator deployment.
+//
+// IMPORTANT: The controlled resource must reside in the same namespace as the operator as described by:
+// https://kubernetes.io/docs/concepts/overview/working-with-objects/owners-dependents/
+func SetAsOwnedByOperatorDeployment(ctx context.Context,
+	client kubernetes.Interface,
+	controlled *metav1.ObjectMeta,
+) error {
+	const operatorDepName = "postgresql-operator-controller-manager"
+	// This is the easiest way to get the typeMeta.
+	// If you instantiate an empty v1.Deployment struct it does NOT contain the typeMeta information.
+	typeMeta := metav1.TypeMeta{
+		Kind:       "Deployment",
+		APIVersion: "apps/v1",
+	}
+
+	getOptions := metav1.GetOptions{TypeMeta: typeMeta}
+	dep, err := client.AppsV1().Deployments(controlled.Namespace).Get(ctx, operatorDepName, getOptions)
+	if err != nil {
+		return err
+	}
+
+	// The deployment typeMeta is empty (kubernetes bug), so we pass the one we already populated.
+	SetAsOwnedBy(controlled, dep.ObjectMeta, typeMeta)
+
+	return nil
 }
