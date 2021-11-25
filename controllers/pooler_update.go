41318380eec696049c560980f93728ad6585c949
armru <armando.ruocco@enterprisedb.com>
2021-11-25 17:41:02 +0100

    fix: add `imagePullSecret` to the pooler's service account
    Co-authored-by: Philippe Scorsolini <philippe.scorsolini@enterprisedb.com>
    Co-authored-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>

--- controllers/pooler_update.go
+++ controllers/pooler_update.go
@@ -8,13 +8,17 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"reflect"
 
+	corev1 "k8s.io/api/core/v1"
 	apierrs "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	apiv1 "github.com/EnterpriseDB/cloud-native-postgresql/api/v1"
+	"github.com/EnterpriseDB/cloud-native-postgresql/internal/configuration"
 	"github.com/EnterpriseDB/cloud-native-postgresql/pkg/management/log"
 	"github.com/EnterpriseDB/cloud-native-postgresql/pkg/specs/pgbouncer"
 	"github.com/EnterpriseDB/cloud-native-postgresql/pkg/utils/hash"
@@ -86,7 +90,7 @@ func (r *PoolerReconciler) updateDeployment(
 		}
 		updatedDeployment.Annotations[pgbouncer.PgbouncerPoolerSpecHash] = updatedVersion
 
-		log.Info("Updating deployment")
+		contextLog.Info("Updating deployment")
 		err = r.Patch(ctx, updatedDeployment, client.MergeFrom(resources.Deployment))
 		if err != nil {
 			return err
@@ -132,19 +136,25 @@ func (r *PoolerReconciler) updateRBAC(
 	resources *poolerManagedResources,
 ) error {
 	contextLog := log.FromContext(ctx)
-
+	serviceAccount := pgbouncer.ServiceAccount(pooler)
+	if err := r.ensureServiceAccountPullSecret(ctx, pooler, serviceAccount); err != nil {
+		return err
+	}
 	if resources.ServiceAccount == nil {
-		serviceAccount := pgbouncer.ServiceAccount(pooler)
+		contextLog.Info("Creating service account")
 		if err := ctrl.SetControllerReference(pooler, serviceAccount, r.Scheme); err != nil {
 			return err
 		}
-
-		contextLog.Info("Creating service account")
-		err := r.Create(ctx, serviceAccount)
-		if err != nil && !apierrs.IsAlreadyExists(err) {
+		if err := r.Create(ctx, serviceAccount); err != nil && !apierrs.IsAlreadyExists(err) {
 			return err
 		}
 		resources.ServiceAccount = serviceAccount
+	} else if !reflect.DeepEqual(serviceAccount.ImagePullSecrets, resources.ServiceAccount.ImagePullSecrets) {
+		contextLog.Info("Updating service account")
+		resources.ServiceAccount.ImagePullSecrets = serviceAccount.ImagePullSecrets
+		if err := r.Update(ctx, resources.ServiceAccount); err != nil {
+			return err
+		}
 	}
 
 	role := pgbouncer.Role(pooler)
@@ -180,6 +190,70 @@ func (r *PoolerReconciler) updateRBAC(
 		}
 		resources.RoleBinding = &roleBinding
 	}
+
+	return nil
+}
+
+// ensureServiceAccountPullSecret will create the image pull secret in the pooler namespace and add the reference
+// inside the pooler service account
+func (r *PoolerReconciler) ensureServiceAccountPullSecret(
+	ctx context.Context,
+	pooler *apiv1.Pooler,
+	serviceAccount *corev1.ServiceAccount,
+) error {
+	if serviceAccount == nil {
+		return nil
+	}
+
+	contextLog := log.FromContext(ctx)
+	if configuration.Current.OperatorNamespace == "" {
+		// We are not getting started via a k8s deployment. Perhaps we are running in our development environment
+		return nil
+	}
+
+	// no pull secret name, there is nothing to do
+	if configuration.Current.OperatorPullSecretName == "" {
+		return nil
+	}
+	// Let's find the operator secret
+	var operatorSecret corev1.Secret
+	if err := r.Get(ctx, client.ObjectKey{
+		Name:      configuration.Current.OperatorPullSecretName,
+		Namespace: configuration.Current.OperatorNamespace,
+	}, &operatorSecret); err != nil {
+		if apierrs.IsNotFound(err) {
+			// There is no secret like that, probably because we are running in our development environment
+			return nil
+		}
+		return err
+	}
+
+	poolerSecretName := fmt.Sprintf("%s-pull", pooler.Name)
+
+	// Let's create the secret with the required info
+	secret := corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: pooler.Namespace,
+			// we change name to avoid ownership conflicts with the managed secret from cnp cluster
+			Name: poolerSecretName,
+		},
+		Data: operatorSecret.Data,
+		Type: operatorSecret.Type,
+	}
+
+	if err := ctrl.SetControllerReference(pooler, &secret, r.Scheme); err != nil {
+		return err
+	}
+
+	contextLog.Debug("creating image pull secret for service account")
+	// Another sync loop may have already created the service. Let's check that
+	if err := r.Create(ctx, &secret); err != nil && !apierrs.IsAlreadyExists(err) {
+		return err
+	}
+
+	serviceAccount.ImagePullSecrets = append(serviceAccount.ImagePullSecrets, corev1.LocalObjectReference{
+		Name: poolerSecretName,
+	})
 
 	return nil
 }
