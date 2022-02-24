24136a9caa24f3c7c53026cb8bdf6ddc8b95edbb
Marco Nenciarini <marco.nenciarini@enterprisedb.com>
2022-02-24 18:22:38 +0100

    fix: preserve ServiceAccount content when updated
    Preserve the content of metadata.Annotations and imagePullSecrets
    when the operator updates the ServiceAccount.
    
    Previously they were overwritten on every update.
    
    Co-authored-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- controllers/cluster_create_test.go
+++ controllers/cluster_create_test.go
@@ -13,11 +13,11 @@ import (
 	policyv1beta1 "k8s.io/api/policy/v1beta1"
 	"k8s.io/apimachinery/pkg/types"
 
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
-
 	apiv1 "github.com/EnterpriseDB/cloud-native-postgresql/api/v1"
 	"github.com/EnterpriseDB/cloud-native-postgresql/pkg/specs"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("cluster_create unit tests", func() {
@@ -88,6 +88,70 @@ var _ = Describe("cluster_create unit tests", func() {
 			assertResourceExists(cluster.GetServiceReadOnlyName(), namespace, &corev1.Service{})
 			assertResourceExists(cluster.GetServiceReadWriteName(), namespace, &corev1.Service{})
 			assertResourceExists(cluster.GetServiceReadName(), namespace, &corev1.Service{})
+		})
+	})
+
+	It("should make sure that createOrPatchServiceAccount works correctly", func() {
+		ctx := context.Background()
+		namespace := newFakeNamespace()
+		cluster := newFakeCNPCluster(namespace)
+
+		By("executing createOrPatchServiceAccount (create)", func() {
+			err := clusterReconciler.createOrPatchServiceAccount(ctx, cluster)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		sa := &corev1.ServiceAccount{}
+
+		By("making sure that the serviceaccount has been created", func() {
+			assertResourceExists(cluster.Name, namespace, sa)
+		})
+
+		By("adding an annotation, a label and an image pull secret to the service account", func() {
+			sa.Annotations["test"] = "annotation"
+			sa.Labels["test"] = "label"
+			sa.ImagePullSecrets = append(sa.ImagePullSecrets, corev1.LocalObjectReference{
+				Name: "sa-pullsecret",
+			})
+			err := k8sClient.Update(context.Background(), sa)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		By("executing createOrPatchServiceAccount (no-patch)", func() {
+			err := clusterReconciler.createOrPatchServiceAccount(ctx, cluster)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		By("making sure that the serviceaccount is untouched because there is no change in the cluster", func() {
+			updatedSa := &corev1.ServiceAccount{}
+			assertResourceExists(cluster.Name, namespace, updatedSa)
+			Expect(updatedSa).To(BeEquivalentTo(sa))
+		})
+
+		By("adding an image pull secret to the cluster to trigger a service account update", func() {
+			cluster.Spec.ImagePullSecrets = append(cluster.Spec.ImagePullSecrets, apiv1.LocalObjectReference{
+				Name: "cluster-pullsecret",
+			})
+			err := k8sClient.Update(context.Background(), cluster)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		By("executing createOrPatchServiceAccount (patch)", func() {
+			err := clusterReconciler.createOrPatchServiceAccount(ctx, cluster)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		By("making sure that the serviceaccount is patched correctly", func() {
+			updatedSA := &corev1.ServiceAccount{}
+			assertResourceExists(cluster.Name, namespace, updatedSA)
+			Expect(updatedSA.Annotations["test"]).To(BeEquivalentTo("annotation"))
+			Expect(updatedSA.Labels["test"]).To(BeEquivalentTo("label"))
+			Expect(updatedSA.ImagePullSecrets).To(ContainElements(corev1.LocalObjectReference{
+				Name: "cluster-pullsecret",
+			}))
+			Expect(updatedSA.ImagePullSecrets).To(ContainElements(corev1.LocalObjectReference{
+				Name: "sa-pullsecret",
+			}))
 		})
 	})
 
