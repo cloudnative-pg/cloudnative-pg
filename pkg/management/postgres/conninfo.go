5cd4a218b0fdf24dbf66e7b468da5f9e5e45e348
Philippe Scorsolini <philippe.scorsolini@enterprisedb.com>
2022-03-02 10:22:08 +0100

    fix: reverting to arbitrary order for `primary_conninfo`
    The old code was totally correct and sound. However, changing the order
    of the keys in `primary_conninfo` would require an unneeded restart
    of clusters based on PostgreSQL 12 and older.
    
    Besides that restart being unneeded, this would break the E2E tests
    that expect that, after an online upgrade of the instance manager,
    Pods continue to be up and running.
    
    This doesn't happen from PostgreSQL 13 on, since from this version the
    `primary_conninfo` string can be changed simply by reloading the
    server.

--- pkg/management/postgres/conninfo.go
+++ pkg/management/postgres/conninfo.go
@@ -10,21 +10,21 @@ import (
 	"fmt"
 
 	apiv1 "github.com/EnterpriseDB/cloud-native-postgresql/api/v1"
-	"github.com/EnterpriseDB/cloud-native-postgresql/pkg/configfile"
 	"github.com/EnterpriseDB/cloud-native-postgresql/pkg/postgres"
 )
 
 // buildPrimaryConnInfo builds the connection string to connect to primaryHostname
 func buildPrimaryConnInfo(primaryHostname, applicationName string) string {
-	primaryConnInfoParameters := map[string]string{
-		"host":             primaryHostname,
-		"user":             apiv1.StreamingReplicationUser,
-		"port":             fmt.Sprintf("%d", GetServerPort()),
-		"sslkey":           postgres.StreamingReplicaKeyLocation,
-		"sslcert":          postgres.StreamingReplicaCertificateLocation,
-		"sslrootcert":      postgres.ServerCACertificateLocation,
-		"application_name": applicationName,
-		"sslmode":          "verify-ca",
-	}
-	return configfile.CreateConnectionString(primaryConnInfoParameters)
+	// We should have been using configfile.CreateConnectionString
+	// but doing that we would cause an unnecessary restart of
+	// existing PostgreSQL 12 clusters.
+	primaryConnInfo := fmt.Sprintf("host=%v ", primaryHostname) +
+		fmt.Sprintf("user=%v ", apiv1.StreamingReplicationUser) +
+		fmt.Sprintf("port=%v ", GetServerPort()) +
+		fmt.Sprintf("sslkey=%v ", postgres.StreamingReplicaKeyLocation) +
+		fmt.Sprintf("sslcert=%v ", postgres.StreamingReplicaCertificateLocation) +
+		fmt.Sprintf("sslrootcert=%v ", postgres.ServerCACertificateLocation) +
+		fmt.Sprintf("application_name=%v ", applicationName) +
+		"sslmode=verify-ca"
+	return primaryConnInfo
 }
