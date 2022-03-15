2f42976e607a60a37c9dc1fd44c26395433cc7d3
Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>
2022-03-15 06:05:35 -0300

    fix: support `Opaque` secrets for certificates
    We were only accepting TLS and basic-auth type secrets for the
    PgBouncer configuration. Now we also support `Opaque` secrets.
    
    When the secret has type `Opaque`, we rely on the the keys inside
    the secrets to determine the real secret type.
    
    Co-authored-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>

---
+++ pkg/management/pgbouncer/config/suite_test.go
@@ -0,0 +1,20 @@
+/*
+This file is part of Cloud Native PostgreSQL.
+
+Copyright (C) 2019-2021 EnterpriseDB Corporation.
+*/
+
+package config
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestConfig(t *testing.T) {
+	RegisterFailHandler(Fail)
+
+	RunSpecs(t, "PgBouncer configuration test suite")
+}
