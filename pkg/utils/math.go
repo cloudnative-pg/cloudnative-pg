d52eec3df18b498c0c261473e5ac9fb9fe312ba1
Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>
2022-03-24 15:53:17 -0300

    chore: only allow power of two numbers for WAL segment size
    Co-authored-by: John Long <john.long@enterprisedb.com>

---
+++ pkg/utils/math.go
@@ -0,0 +1,14 @@
+/*
+This file is part of Cloud Native PostgreSQL.
+
+Copyright (C) 2019-2021 EnterpriseDB Corporation.
+*/
+
+package utils
+
+// IsPowerOfTwo calculates if a number is power of two or not
+// reference: https://github.com/golang/go/blob/master/src/strconv/itoa.go#L204 #wokeignore:rule=master
+// This function will return false if the number is zero
+func IsPowerOfTwo(n int) bool {
+	return (n != 0) && (n&(n-1) == 0)
+}
