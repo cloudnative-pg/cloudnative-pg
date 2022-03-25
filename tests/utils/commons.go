9f08b10e4aed8d86ea8289327687d53df883d532
Jitendra Wadle <jitendra.wadle@enterprisedb.com>
2022-03-25 20:07:28 +0530

    test(AKS): improves E2E test resiliency to transient network failures
    This patch wraps several AKS API calls inside `Eventually` or `Retry`
    statements to avoid faulty E2E test results due to transient network issues.
    
    Co-authored-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- tests/utils/commons.go
+++ tests/utils/commons.go
@@ -6,7 +6,13 @@ Copyright (C) 2019-2021 EnterpriseDB Corporation.
 
 package utils
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/avast/retry-go/v4"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
 
 // ForgeArchiveWalOnMinio instead of using `switchWalCmd` to generate a real WAL archive, directly forges a WAL archive
 // file on Minio by copying and renaming an existing WAL archive file for the sake of more control of testing. To make
@@ -18,7 +24,7 @@ func ForgeArchiveWalOnMinio(namespace, clusterName, miniClientPodName, existingW
 	existingWALPath := minioWALBasePath + "/" + existingWALName + ".gz"
 	newWALNamePath := minioWALBasePath + "/" + newWALName
 	forgeWALOnMinioCmd := "mc cp " + existingWALPath + " " + newWALNamePath
-	_, _, err := RunUnchecked(fmt.Sprintf(
+	_, _, err := RunUncheckedRetry(fmt.Sprintf(
 		"kubectl exec -n %v %v -- %v",
 		namespace,
 		miniClientPodName,
@@ -32,7 +38,7 @@ func ForgeArchiveWalOnMinio(namespace, clusterName, miniClientPodName, existingW
 func TestFileExist(namespace, podName, directoryPath, fileName string) bool {
 	filePath := directoryPath + "/" + fileName
 	testFileExistCommand := "test -f " + filePath
-	_, _, err := RunUnchecked(fmt.Sprintf(
+	_, _, err := RunUncheckedRetry(fmt.Sprintf(
 		"kubectl exec -n %v %v -- %v",
 		namespace,
 		podName,
@@ -44,11 +50,75 @@ func TestFileExist(namespace, podName, directoryPath, fileName string) bool {
 // TestDirectoryEmpty tests if a directory `directoryPath` exists on pod `podName` in namespace `namespace`
 func TestDirectoryEmpty(namespace, podName, directoryPath string) bool {
 	testDirectoryEmptyCommand := "test \"$(ls -A" + directoryPath + ")\""
-	_, _, err := RunUnchecked(fmt.Sprintf(
+	_, _, err := RunUncheckedRetry(fmt.Sprintf(
 		"kubectl exec -n %v %v -- %v",
 		namespace,
 		podName,
 		testDirectoryEmptyCommand))
 
 	return err == nil
+}
+
+// CreateObject create object in the Kubernetes cluster
+func CreateObject(env *TestingEnvironment, object client.Object, opts ...client.CreateOption) error {
+	err := retry.Do(
+		func() error {
+			err := env.Client.Create(env.Ctx, object, opts...)
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+		retry.Delay(PollingTime*time.Second),
+		retry.Attempts(RetryTimeout),
+		retry.DelayType(retry.FixedDelay),
+	)
+	return err
+}
+
+// DeleteObject delete object in the Kubernetes cluster
+func DeleteObject(env *TestingEnvironment, object client.Object, opts ...client.DeleteOption) error {
+	err := retry.Do(
+		func() error {
+			return env.Client.Delete(env.Ctx, object, opts...)
+		},
+		retry.Delay(PollingTime*time.Second),
+		retry.Attempts(RetryTimeout),
+		retry.DelayType(retry.FixedDelay),
+	)
+	return err
+}
+
+// GetObjectList retrieves list of objects for a given namespace and list options
+func GetObjectList(env *TestingEnvironment, objectList client.ObjectList, opts ...client.ListOption) error {
+	err := retry.Do(
+		func() error {
+			err := env.Client.List(env.Ctx, objectList, opts...)
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+		retry.Delay(PollingTime*time.Second),
+		retry.Attempts(RetryTimeout),
+		retry.DelayType(retry.FixedDelay),
+	)
+	return err
+}
+
+// GetObject retrieves an objects for the given object key from the Kubernetes Cluster
+func GetObject(env *TestingEnvironment, objectKey client.ObjectKey, object client.Object) error {
+	err := retry.Do(
+		func() error {
+			err := env.Client.Get(env.Ctx, objectKey, object)
+			if err != nil {
+				return err
+			}
+			return nil
+		},
+		retry.Delay(PollingTime*time.Second),
+		retry.Attempts(RetryTimeout),
+		retry.DelayType(retry.FixedDelay),
+	)
+	return err
 }
