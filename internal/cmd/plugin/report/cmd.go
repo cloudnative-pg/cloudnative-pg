7988db1cb0da0de27fae24290a534393aa5b4f8a
Jaime Silvela <jaime.silvela@enterprisedb.com>
2022-03-29 23:01:50 +0200

    feat: add `kubectl cnp report`
    The `kubectl cnp report` command requests the operator to provide
    information regarding the operator deployment, configuration and events.
    
    Co-authored-by: Gabriele Quaresima <gabriele.quaresima@enterprisedb.com>
    Co-authored-by: Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>

---
+++ internal/cmd/plugin/report/cmd.go
@@ -0,0 +1,23 @@
+/*
+This file is part of Cloud Native PostgreSQL.
+
+Copyright (C) 2019-2021 EnterpriseDB Corporation.
+*/
+
+package report
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// NewCmd creates the new "report" command
+func NewCmd() *cobra.Command {
+	reportCmd := &cobra.Command{
+		Use:   "report operator/cluster",
+		Short: "Report on the operator",
+	}
+
+	reportCmd.AddCommand(operatorCmd())
+
+	return reportCmd
+}
