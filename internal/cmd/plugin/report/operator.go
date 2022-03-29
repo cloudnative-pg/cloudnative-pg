8b75d659604cd511b3aab249bb38371030e694db
Jaime Silvela <jaime.silvela@enterprisedb.com>
2022-03-29 23:01:50 +0200

    feat: add `kubectl cnp report`
    The `kubectl cnp report` command requests the operator to provide
    information regarding the operator deployment, configuration and events.
    
    Co-authored-by: Gabriele Quaresima <gabriele.quaresima@enterprisedb.com>
    Co-authored-by: Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>

---
+++ internal/cmd/plugin/report/operator.go
@@ -0,0 +1,40 @@
+/*
+This file is part of Cloud Native PostgreSQL.
+
+Copyright (C) 2019-2021 EnterpriseDB Corporation.
+*/
+
+package report
+
+import (
+	"github.com/EnterpriseDB/cloud-native-postgresql/internal/cmd/plugin"
+	"github.com/spf13/cobra"
+)
+
+func operatorCmd() *cobra.Command {
+	var (
+		file, output  string
+		stopRedaction bool
+	)
+	cmd := &cobra.Command{
+		Use:   "operator -f <filename.zip>",
+		Short: "Report operator deployment, pod, events",
+		Long:  "Collects combined information on the operator in a Zip file",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return Operator(cmd.Context(), plugin.OutputFormat(output),
+				file, stopRedaction)
+		},
+	}
+
+	cmd.AddCommand()
+
+	cmd.Flags().StringVarP(&file, "file", "f", "report.zip",
+		"Output file")
+	_ = cmd.MarkFlagRequired("file")
+	cmd.Flags().StringVarP(&output, "output", "o", "yaml",
+		"Output format. One of yaml|json")
+	cmd.Flags().BoolVarP(&stopRedaction, "stopRedaction", "S", false,
+		"Don't redact secrets")
+
+	return cmd
+}
