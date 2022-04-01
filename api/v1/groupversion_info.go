c08c91d0b8a961808011315271376ba025172be0
Marco Nenciarini <marco.nenciarini@enterprisedb.com>
2022-04-01 14:21:28 +0200

    Upgrade license headers

--- api/v1/groupversion_info.go
+++ api/v1/groupversion_info.go
@@ -1,7 +1,7 @@
 /*
 This file is part of Cloud Native PostgreSQL.
 
-Copyright (C) 2019-2021 EnterpriseDB Corporation.
+Copyright (C) 2019-2022 EnterpriseDB Corporation.
 */
 
 // Package v1 contains API Schema definitions for the postgresql v1 API group
--- pkg/management/external/external.go
+++ pkg/management/external/external.go
@@ -1,7 +1,7 @@
 /*
 This file is part of Cloud Native PostgreSQL.
 
-Copyright (C) 2019-2021 EnterpriseDB Corporation.
+Copyright (C) 2019-2022 EnterpriseDB Corporation.
 */
 
 // Package external contains the functions needed to manage servers which are external to this
