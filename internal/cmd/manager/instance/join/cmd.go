8eb513926b82a0ad94b60f2c09501fe719018f96
Marco Nenciarini <marco.nenciarini@enterprisedb.com>
2022-04-01 14:21:28 +0200

    Upgrade license headers

--- internal/cmd/manager/instance/join/cmd.go
+++ internal/cmd/manager/instance/join/cmd.go
@@ -1,7 +1,7 @@
 /*
 This file is part of Cloud Native PostgreSQL.
 
-Copyright (C) 2019-2021 EnterpriseDB Corporation.
+Copyright (C) 2019-2022 EnterpriseDB Corporation.
 */
 
 // Package join implements the "instance join" subcommand of the operator
--- pkg/management/logtest/logtest.go
+++ pkg/management/logtest/logtest.go
@@ -1,7 +1,7 @@
 /*
 This file is part of Cloud Native PostgreSQL.
 
-Copyright (C) 2019-2021 EnterpriseDB Corporation.
+Copyright (C) 2019-2022 EnterpriseDB Corporation.
 */
 
 // Package logtest contains the testing utils for the logging subsystem of PGK
