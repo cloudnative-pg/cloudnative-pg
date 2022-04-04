f756d110491af954d1c5c9ce3ed2a67e0adedd96
Jaime Silvela <jaime.silvela@enterprisedb.com>
2022-04-04 13:04:27 +0200

    chore: add webhook configuration and service to cnp report plugin
    Co-authored-by: Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>
    Co-authored-by: Philippe Scorsolini <philippe.scorsolini@enterprisedb.com>

--- internal/cmd/plugin/report/redactors.go
+++ internal/cmd/plugin/report/redactors.go
@@ -6,7 +6,10 @@ Copyright (C) 2019-2022 EnterpriseDB Corporation.
 
 package report
 
-import corev1 "k8s.io/api/core/v1"
+import (
+	admissionv1 "k8s.io/api/admissionregistration/v1"
+	corev1 "k8s.io/api/core/v1"
+)
 
 // redactSecret creates a version of a Secret with only the Data map's KEYS
 func redactSecret(secret corev1.Secret) corev1.Secret {
@@ -36,4 +39,20 @@ func redactConfigMap(configMap corev1.ConfigMap) corev1.ConfigMap {
 // passConfigMap passes an unmodified ConfigMap
 func passConfigMap(configMap corev1.ConfigMap) corev1.ConfigMap {
 	return configMap
+}
+
+// redactWebhookClientConfig makes a copy of a WebhookClientConfig with the CA obfuscated
+//
+// Useful to redact validating/mutating webhook configurations
+//
+// WARN: the CABundle in the WebhookClientConfig is optional, and if blank / missing,
+// the trust roots on API server are used. So, in this case, leaving the secret blank
+// may change the behavior. See https://pkg.go.dev/k8s.io/api@v0.23.5/admissionregistration/v1#WebhookClientConfig
+// If the CABundle is present, override with a small string. The one chosen is "-"
+// which will print in Base64 as "LQ=="
+func redactWebhookClientConfig(config admissionv1.WebhookClientConfig) admissionv1.WebhookClientConfig {
+	if len(config.CABundle) != 0 {
+		config.CABundle = []byte("-") // will print in Base64 as "LQ=="
+	}
+	return config
 }
