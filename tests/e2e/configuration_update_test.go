a5e4f9291ded108a1270c9936559f9843fcdb551
Jitendra Wadle <jitendra.wadle@enterprisedb.com>
2022-04-13 01:19:52 +0530

    test: E2E for primaryUpdateMethod
    Co-authored-by: Philippe Scorsolini <philippe.scorsolini@enterprisedb.com>
    Co-authored-by: Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>

--- tests/e2e/configuration_update_test.go
+++ tests/e2e/configuration_update_test.go
@@ -7,13 +7,17 @@ Copyright (C) 2019-2022 EnterpriseDB Corporation.
 package e2e
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	apiv1 "github.com/EnterpriseDB/cloud-native-postgresql/api/v1"
 	"github.com/EnterpriseDB/cloud-native-postgresql/pkg/specs"
@@ -345,5 +349,175 @@ var _ = Describe("Configuration update", func() {
 					}, timeout).Should(BeEquivalentTo(oldPrimary))
 				})
 			})
+	})
+})
+
+var _ = Describe("Configuration update with primaryUpdateMethod", func() {
+	const level = tests.High
+
+	BeforeEach(func() {
+		if testLevelEnv.Depth < int(level) {
+			Skip("Test depth is lower than the amount requested for this test")
+		}
+	})
+
+	Context("primaryUpdateMethod value set to restart", Ordered, func() {
+		clusterFileWithPrimaryUpdateRestart := fixturesDir +
+			"/config_update/primary_update_method/primary-update-restart.yaml"
+		var namespace, clusterName string
+		var sourceClusterNamespacedName *types.NamespacedName
+
+		JustAfterEach(func() {
+			if CurrentSpecReport().Failed() {
+				env.DumpClusterEnv(namespace, clusterName,
+					"out/"+CurrentSpecReport().LeafNodeText+".log")
+			}
+		})
+
+		BeforeAll(func() {
+			namespace = "config-change-primary-update-restart"
+			// Create a cluster in a namespace we'll delete after the test
+			err := env.CreateNamespace(namespace)
+			Expect(err).ToNot(HaveOccurred())
+
+			clusterName, err = env.GetResourceNameFromYAML(clusterFileWithPrimaryUpdateRestart)
+			Expect(err).ToNot(HaveOccurred())
+			sourceClusterNamespacedName = &types.NamespacedName{
+				Namespace: namespace,
+				Name:      clusterName,
+			}
+
+			By("setting up cluster with primaryUpdateMethod value set to restart", func() {
+				AssertCreateCluster(namespace, clusterName, clusterFileWithPrimaryUpdateRestart, env)
+			})
+		})
+
+		AfterAll(func() {
+			err := env.DeleteNamespace(namespace)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("should restart primary in place after increasing config parameter `max_connection` value", func() {
+			const (
+				maxConnectionParamKey = "max_connections"
+			)
+			var oldPrimaryPodName string
+			var newMaxConnectionsValue int
+			var primaryStartTime time.Time
+
+			By("getting old primary info", func() {
+				commandTimeout := time.Second * 2
+				primaryPodInfo, err := env.GetClusterPrimary(namespace, clusterName)
+				Expect(err).ToNot(HaveOccurred())
+
+				oldPrimaryPodName = primaryPodInfo.GetName()
+				stdout, _, cmdErr := env.EventuallyExecCommand(env.Ctx, *primaryPodInfo, specs.PostgresContainerName,
+					&commandTimeout, "psql", "-U", "postgres", "-tAc",
+					"select to_char(pg_postmaster_start_time(), 'YYYY-MM-DD HH24:MI:SS');")
+				Expect(cmdErr).ToNot(HaveOccurred())
+
+				primaryStartTime, err = devUtils.ParseTargetTime(nil, strings.Trim(stdout, "\n"))
+				Expect(err).NotTo(HaveOccurred())
+
+				stdout, _, cmdErr = env.EventuallyExecCommand(env.Ctx, *primaryPodInfo, specs.PostgresContainerName,
+					&commandTimeout, "psql", "-U", "postgres", "-tAc", "show max_connections")
+				Expect(cmdErr).ToNot(HaveOccurred())
+
+				v, err := strconv.Atoi(strings.Trim(stdout, "\n"))
+				Expect(err).NotTo(HaveOccurred())
+
+				newMaxConnectionsValue = v + 10
+			})
+
+			By(fmt.Sprintf("updating max_connection value to %v", newMaxConnectionsValue), func() {
+				var cluster apiv1.Cluster
+				err := env.Client.Get(env.Ctx, *sourceClusterNamespacedName, &cluster)
+				Expect(err).ToNot(HaveOccurred())
+
+				updated := cluster.DeepCopy()
+				updated.Spec.PostgresConfiguration.Parameters[maxConnectionParamKey] = fmt.Sprintf("%v",
+					newMaxConnectionsValue)
+				err = env.Client.Patch(env.Ctx, updated, client.MergeFrom(&cluster))
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+			By("verifying the new value for max_connections is updated for all instances", func() {
+				podList, err := env.GetClusterPodList(namespace, clusterName)
+				Expect(err).ToNot(HaveOccurred())
+
+				commandTimeout := time.Second * 2
+				for _, pod := range podList.Items {
+					Eventually(func() (int, error, error) {
+						stdout, _, err := env.ExecCommand(env.Ctx, pod, specs.PostgresContainerName, &commandTimeout,
+							"psql", "-U", "postgres", "-tAc", "show max_connections")
+						value, atoiErr := strconv.Atoi(strings.Trim(stdout, "\n"))
+						return value, err, atoiErr
+					}, 180).Should(BeEquivalentTo(newMaxConnectionsValue),
+						"Pod %v should have updated its configuration", pod.Name)
+				}
+			})
+
+			By("verifying the old primary is still the primary", func() {
+				cluster := apiv1.Cluster{}
+				Eventually(func() (string, error) {
+					err := env.Client.Get(env.Ctx, *sourceClusterNamespacedName, &cluster)
+					return cluster.Status.CurrentPrimary, err
+				}, 60).Should(BeEquivalentTo(oldPrimaryPodName))
+			})
+
+			By("verifying that old primary was actually restarted", func() {
+				commandTimeout := time.Second * 2
+				pod := corev1.Pod{}
+				err := env.Client.Get(env.Ctx, types.NamespacedName{
+					Namespace: namespace,
+					Name:      oldPrimaryPodName,
+				}, &pod)
+				Expect(err).ToNot(HaveOccurred())
+
+				// take pg postmaster start time
+				stdout, _, cmdErr := env.EventuallyExecCommand(env.Ctx, pod, specs.PostgresContainerName, &commandTimeout,
+					"psql", "-U", "postgres", "-tAc",
+					"select to_char(pg_postmaster_start_time(), 'YYYY-MM-DD HH24:MI:SS');")
+				Expect(cmdErr).ToNot(HaveOccurred())
+
+				newStartTime, err := devUtils.ParseTargetTime(nil, strings.Trim(stdout, "\n"))
+				Expect(err).NotTo(HaveOccurred())
+
+				// verify that pg postmaster start time is greater than currentTimestamp which was taken before restart
+				Expect(newStartTime).Should(BeTemporally(">", primaryStartTime))
+			})
+		})
+		It("work_mem config change should not require a restart", func() {
+			const expectedNewValueForWorkMem = "10MB"
+			commandTimeout := time.Second * 2
+
+			By("updating work mem ", func() {
+				var cluster apiv1.Cluster
+				err := env.Client.Get(env.Ctx, *sourceClusterNamespacedName, &cluster)
+				Expect(err).ToNot(HaveOccurred())
+
+				updated := cluster.DeepCopy()
+				updated.Spec.PostgresConfiguration.Parameters["work_mem"] = expectedNewValueForWorkMem
+				err = env.Client.Patch(env.Ctx, updated, client.MergeFrom(&cluster))
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+			By("verify that work_mem result as expected", func() {
+				podList, err := env.GetClusterPodList(namespace, clusterName)
+				Expect(err).ToNot(HaveOccurred())
+
+				// Check that the parameter has been modified in every pod
+				for _, pod := range podList.Items {
+					pod := pod // pin the variable
+					Eventually(func() (int, error, error) {
+						stdout, _, err := env.ExecCommand(env.Ctx, pod, specs.PostgresContainerName, &commandTimeout,
+							"psql", "-U", "postgres", "-tAc", "show work_mem")
+						value, atoiErr := strconv.Atoi(strings.Trim(stdout, "MB\n"))
+						return value, err, atoiErr
+					}, 160).Should(BeEquivalentTo(10))
+				}
+			})
+			AssertPostgresNoPendingRestart(namespace, clusterName, commandTimeout, 120)
+		})
 	})
 })
