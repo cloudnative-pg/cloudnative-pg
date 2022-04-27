8fc83f8c50064f2bb893322abc7d9919929fa422
Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>
2022-04-27 18:12:59 -0400

    chore: check recovery and backup object not equal
    Make sure that the backup object store it is not the same as the
    external cluster source in the recovery process.
    
    The idea it's to prevent user to overwrite the source cluster
    object store with new data from the new cluster.
    
    Closes #62
    
    Signed-off-by: Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>

--- api/v1/cluster_webhook.go
+++ api/v1/cluster_webhook.go
@@ -19,6 +19,7 @@ package v1
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 	"strconv"
 	"strings"
 
@@ -237,6 +238,7 @@ func (r *Cluster) Validate() (allErrs field.ErrorList) {
 	allErrs = append(allErrs, r.validateName()...)
 	allErrs = append(allErrs, r.validateBootstrapPgBaseBackupSource()...)
 	allErrs = append(allErrs, r.validateBootstrapRecoverySource()...)
+	allErrs = append(allErrs, r.validateRecoveryAndBackupTarget()...)
 	allErrs = append(allErrs, r.validateExternalClusters()...)
 	allErrs = append(allErrs, r.validateTolerations()...)
 	allErrs = append(allErrs, r.validateAntiAffinity()...)
@@ -1153,6 +1155,39 @@ func (r *Cluster) validateBackupConfiguration() field.ErrorList {
 				"not a valid retention policy",
 			))
 		}
+	}
+
+	return allErrors
+}
+
+// validateRecoveryAndBackupTarget validates that the recovery point and
+// the backup point are not the same
+func (r *Cluster) validateRecoveryAndBackupTarget() field.ErrorList {
+	allErrors := field.ErrorList{}
+
+	if r.Spec.Bootstrap == nil || r.Spec.Bootstrap.Recovery == nil || r.Spec.Bootstrap.Recovery.Source == "" ||
+		r.Spec.Backup == nil || r.Spec.Backup.BarmanObjectStore == nil {
+		return allErrors
+	}
+
+	var sourceCluster *ExternalCluster
+	for i, cluster := range r.Spec.ExternalClusters {
+		if cluster.Name == r.Spec.Bootstrap.Recovery.Source {
+			sourceCluster = &r.Spec.ExternalClusters[i]
+		}
+	}
+
+	if sourceCluster == nil || sourceCluster.BarmanObjectStore == nil {
+		return allErrors
+	}
+
+	if reflect.DeepEqual(r.Spec.Backup.BarmanObjectStore, sourceCluster.BarmanObjectStore) {
+		allErrors = append(
+			allErrors,
+			field.Invalid(
+				field.NewPath("spec", "backup", "barmanObjectStore"),
+				"",
+				"Cannot be equal to the ExternalCluster used to recover from"))
 	}
 
 	return allErrors
--- api/v1/cluster_webhook_test.go
+++ api/v1/cluster_webhook_test.go
@@ -1744,3 +1744,67 @@ var _ = Describe("Default monitoring queries", func() {
 			To(ContainElements(testCluster.Spec.Monitoring.CustomQueriesSecret))
 	})
 })
+
+var _ = Describe("Recovery and Backup Target", func() {
+	cluster := &Cluster{
+		Spec: ClusterSpec{
+			Bootstrap: &BootstrapConfiguration{
+				Recovery: &BootstrapRecovery{
+					Source: "one",
+				},
+			},
+			Backup: &BackupConfiguration{
+				BarmanObjectStore: &BarmanObjectStoreConfiguration{
+					S3Credentials: &S3Credentials{
+						AccessKeyIDReference: &SecretKeySelector{
+							LocalObjectReference: LocalObjectReference{
+								Name: "s3-creds",
+							},
+							Key: "access_key",
+						},
+						SecretAccessKeyReference: &SecretKeySelector{
+							LocalObjectReference: LocalObjectReference{
+								Name: "s3-creds",
+							},
+							Key: "secret_key",
+						},
+					},
+					DestinationPath: "/destination/path",
+				},
+			},
+			ExternalClusters: []ExternalCluster{
+				{
+					Name: "one",
+					BarmanObjectStore: &BarmanObjectStoreConfiguration{
+						S3Credentials: &S3Credentials{
+							AccessKeyIDReference: &SecretKeySelector{
+								LocalObjectReference: LocalObjectReference{
+									Name: "s3-creds",
+								},
+								Key: "access_key",
+							},
+							SecretAccessKeyReference: &SecretKeySelector{
+								LocalObjectReference: LocalObjectReference{
+									Name: "s3-creds",
+								},
+								Key: "secret_key",
+							},
+						},
+						DestinationPath: "/destination/path",
+					},
+				},
+			},
+		},
+	}
+
+	It("complain if external cluster and target backup are equal", func() {
+		result := cluster.validateRecoveryAndBackupTarget()
+		Expect(result).NotTo(BeEmpty())
+	})
+
+	It("don't complain if target backup is different ot external backup in path", func() {
+		cluster.Spec.Backup.BarmanObjectStore.DestinationPath = "/destination/new/path"
+		result := cluster.validateRecoveryAndBackupTarget()
+		Expect(result).To(BeEmpty())
+	})
+})
