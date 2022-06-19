76e1ea7a6b74f3ff4695474b2a16cd8ecf5e12f8
Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>
2022-06-19 10:56:57 -0400

    chore: use QuoteIdentifier for the role
    Signed-off-by: Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>

--- internal/management/controller/instance_controller.go
+++ internal/management/controller/instance_controller.go
@@ -1179,8 +1179,8 @@ func (r *InstanceReconciler) reconcileUser(ctx context.Context, username string,
 		return fmt.Errorf("wrong username '%v' in secret, expected '%v'", usernameFromSecret, username)
 	}
 
-	_, err = tx.Exec(fmt.Sprintf("ALTER ROLE \"%v\" WITH PASSWORD %v",
-		username,
+	_, err = tx.Exec(fmt.Sprintf("ALTER ROLE %v WITH PASSWORD %v",
+		pq.QuoteIdentifier(username),
 		pq.QuoteLiteral(password)))
 	if err == nil {
 		r.secretVersions[secret.Name] = secret.ResourceVersion
