9d7b2b4148faaa8f671a614473419b3189911e15
Jaime Silvela <jaime.silvela@enterprisedb.com>
2022-07-11 12:06:59 +0200

    fix: add valid default image
    Signed-off-by: Jaime Silvela <jaime.silvela@enterprisedb.com>

--- pkg/versions/versions.go
+++ pkg/versions/versions.go
@@ -23,7 +23,7 @@ const (
 	Version = "1.15.1"
 
 	// DefaultImageName is the default image used by the operator to create pods
-	DefaultImageName = "ghcr.io/cloudnative-pg/postgresql:14.6"
+	DefaultImageName = "ghcr.io/cloudnative-pg/postgresql:14.4"
 
 	// DefaultOperatorImageName is the default operator image used by the controller in the pods running PostgreSQL
 	DefaultOperatorImageName = "ghcr.io/cloudnative-pg/cloudnative-pg:1.15.1"
