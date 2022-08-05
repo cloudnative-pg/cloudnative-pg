b3585071ec11d248f3b00cd9e209fc2c05e8642e
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2022-08-05 10:54:53 +0200

    chore(lint): fix imports order
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>

--- pkg/specs/volumes_test.go
+++ pkg/specs/volumes_test.go
@@ -17,12 +17,12 @@ limitations under the License.
 package specs
 
 import (
+	corev1 "k8s.io/api/core/v1"
+
 	apiv1 "github.com/cloudnative-pg/cloudnative-pg/api/v1"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-
-	corev1 "k8s.io/api/core/v1"
 )
 
 var _ = Describe("test createVolumesAndVolumeMountsForPostInitApplicationSQLRefs", func() {
