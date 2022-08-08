d1a14a4458532ec582c9415dbbebe39ff4b52190
John Long <john.long@enterprisedb.com>
2022-08-08 12:32:36 -0400

    review: fixing some comments

--- tests/e2e/fastswitchover_test.go
+++ tests/e2e/fastswitchover_test.go
@@ -141,7 +141,7 @@ var _ = Describe("Fast switchover", Serial, Label(tests.LabelPerformance), func(
 			Expect(err).ToNot(HaveOccurred())
 		})
 		By("starting load", func() {
-			// We set up hey and webtest. Hey, a load generator,
+			// We set up Apache Benchmark and webtest. Apache Benchmark, a load generator,
 			// continuously calls the webtest api to execute inserts
 			// on the postgres primary. We make sure that the first
 			// records appear on the database before moving to the next
