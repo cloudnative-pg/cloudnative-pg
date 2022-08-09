6538c45633914296928241d373a89206368247ca
Armando Ruocco <armando.ruocco@enterprisedb.com>
2022-08-09 19:18:04 +0200

    chore: adds instanceName labels on jobs, pods, pvcs (#534)
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- controllers/cluster_create.go
+++ controllers/cluster_create.go
@@ -897,7 +897,7 @@ func (r *ClusterReconciler) createPrimaryInstance(
 		return ctrl.Result{}, fmt.Errorf("cannot generate node serial: %w", err)
 	}
 
-	pvcSpec, err := specs.CreatePVC(cluster.Spec.StorageConfiguration, cluster.Name, cluster.Namespace, nodeSerial)
+	pvcSpec, err := specs.CreatePVC(cluster, cluster.Spec.StorageConfiguration, nodeSerial)
 	if err != nil {
 		if err == specs.ErrorInvalidSize {
 			// This error should have been caught by the validating
@@ -1072,7 +1072,7 @@ func (r *ClusterReconciler) joinReplicaInstance(
 		return ctrl.Result{}, err
 	}
 
-	pvcSpec, err := specs.CreatePVC(cluster.Spec.StorageConfiguration, cluster.Name, cluster.Namespace, nodeSerial)
+	pvcSpec, err := specs.CreatePVC(cluster, cluster.Spec.StorageConfiguration, nodeSerial)
 	if err != nil {
 		if err == specs.ErrorInvalidSize {
 			// This error should have been caught by the validating
