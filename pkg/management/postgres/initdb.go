b8691c2de33a08596c5b31a08108ac51c5a9a744
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2022-08-25 16:20:40 +0200

    chore: review
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>

--- pkg/management/postgres/initdb.go
+++ pkg/management/postgres/initdb.go
@@ -27,7 +27,6 @@ import (
 	"os/exec"
 	"path"
 	"sort"
-	"strings"
 
 	"github.com/lib/pq"
 	ctrl "sigs.k8s.io/controller-runtime/pkg/client"
@@ -41,7 +40,6 @@ import (
 	"github.com/cloudnative-pg/cloudnative-pg/pkg/management/postgres/constants"
 	"github.com/cloudnative-pg/cloudnative-pg/pkg/management/postgres/logicalimport"
 	"github.com/cloudnative-pg/cloudnative-pg/pkg/management/postgres/pool"
-	postgresutils "github.com/cloudnative-pg/cloudnative-pg/pkg/management/postgres/utils"
 	"github.com/cloudnative-pg/cloudnative-pg/pkg/postgres"
 )
 
@@ -322,17 +320,29 @@ func (info InitInfo) Bootstrap(ctx context.Context) error {
 
 	instance := info.GetInstance()
 
-	if libs := strings.Join(cluster.Spec.PostgresConfiguration.AdditionalLibraries, ","); libs != "" {
-		libsConfig := fmt.Sprintf("%s='%s'", postgres.SharedPreloadLibraries, libs)
+	postgresVersion, err := cluster.GetPostgresqlVersion()
+	if err != nil {
+		return fmt.Errorf("while reading the PostgreSQL version: %w", err)
+	}
+
+	configurationInfo := postgres.ConfigurationInfo{
+		Settings:                         postgres.CnpgConfigurationSettings,
+		MajorVersion:                     postgresVersion,
+		UserSettings:                     cluster.Spec.PostgresConfiguration.Parameters,
+		AdditionalSharedPreloadLibraries: cluster.Spec.PostgresConfiguration.AdditionalLibraries,
+		IsReplicaCluster:                 cluster.IsReplica(),
+		IncludingSharedPreloadLibraries:  true,
+		PreserveFixedSettingsFromUser:    true,
+	}
+	postgresConfiguration := postgres.CreatePostgresqlConfiguration(configurationInfo)
+
+	sharedPreloadLibraries := postgresConfiguration.GetConfigurationParameters()[postgres.SharedPreloadLibraries]
+	if sharedPreloadLibraries != "" {
+		libsConfig := fmt.Sprintf("%s='%s'", postgres.SharedPreloadLibraries, sharedPreloadLibraries)
 		instance.StartupOptions = append(instance.StartupOptions, libsConfig)
 	}
 
-	majorVersion, err := postgresutils.GetMajorVersion(instance.PgData)
-	if err != nil {
-		return fmt.Errorf("while reading major version: %w", err)
-	}
-
-	if majorVersion >= 12 {
+	if postgresVersion >= 120000 {
 		primaryConnInfo := buildPrimaryConnInfo(info.ClusterName, info.PodName)
 		_, err = configurePostgresAutoConfFile(info.PgData, primaryConnInfo)
 		if err != nil {
