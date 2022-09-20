3171ff6570c8e6686e4e45123ffcbd280c73dad7
Armando Ruocco <armando.ruocco@enterprisedb.com>
2022-09-20 16:50:51 +0200

    test: adds more unit test coverage to the queryCollector
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- pkg/management/postgres/metrics/collect_test.go
+++ pkg/management/postgres/metrics/collect_test.go
@@ -17,6 +17,8 @@ limitations under the License.
 package metrics
 
 import (
+	"github.com/prometheus/client_golang/prometheus"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
@@ -57,5 +59,54 @@ var _ = Describe("Set default queries", func() {
 		Expect(len(q.mappings)).To(BeEquivalentTo(1))
 		Expect(q.mappings["collector"]["test"].Name).To(BeEquivalentTo("test"))
 		Expect(q.variableLabels["collector"]).To(BeEquivalentTo(VariableSet{"test"}))
+	})
+})
+
+var _ = Describe("QueryCollector tests", func() {
+	Context("collect metric tests", func() {
+		It("should ensure that a metric without conversion is discarded", func() {
+			qc := QueryCollector{}
+			ch := make(chan prometheus.Metric, 10)
+			metricMap := MetricMap{
+				Name:       "MALFORMED_COUNTER",
+				Discard:    true,
+				Conversion: nil,
+				Label:      false,
+			}
+			qc.collectConstMetric(
+				metricMap,
+				"COUNTER_TEST",
+				[]string{"COUNTER_TEST"},
+				ch,
+			)
+			Expect(ch).To(BeEmpty())
+		})
+
+		It("should ensure that a metric with a bad conversion value is discarded", func() {
+			qc := QueryCollector{}
+			ch := make(chan prometheus.Metric, 10)
+			cm := ColumnMapping{Usage: COUNTER}
+			cs := cm.ToMetricMap("TEST_COLUMN", "TEST_NAMESPACE", []string{"TEST_VARIABLE"})
+			for _, metricMap := range cs {
+				// int are not converted
+				qc.collectConstMetric(metricMap, 12, []string{"TEST"}, ch)
+			}
+			Expect(ch).To(HaveLen(0))
+		})
+
+		It("should ensure that a correctly formed counter is sent", func() {
+			qc := QueryCollector{}
+			ch := make(chan prometheus.Metric, 10)
+			cm := ColumnMapping{Usage: COUNTER}
+			cs := cm.ToMetricMap("TEST_COLUMN", "TEST_NAMESPACE", []string{"TEST_VARIABLE"})
+			for _, metricMap := range cs {
+				qc.collectConstMetric(metricMap, int64(12), []string{"TEST"}, ch)
+			}
+			Expect(ch).To(HaveLen(1))
+			res := <-ch
+			desc := res.Desc()
+			Expect(desc).To(ContainSubstring("TEST_NAMESPACE_TEST_COLUMN"))
+			Expect(desc).To(ContainSubstring("TEST_VARIABLE"))
+		})
 	})
 })
