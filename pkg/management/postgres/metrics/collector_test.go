b76292d5ad4c6323c389dac588a85f47fe1c1795
Marco Nenciarini <marco.nenciarini@enterprisedb.com>
2022-09-21 16:49:55 +0200

    fix: honour MAPPEDMETRIC and DURATION metric types conversion (#765)
    The collectConstMetric() function was ignoring the Conversion member of the passed MetricMap entry.
    So any conversion closure generated by MAPPEDMETRIC and DURATION mappings would get ignored.
    This could result in errors e.g. while trying to parse a string value as a float.
    
    Closes #764
    
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    Co-authored-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

---
+++ pkg/management/postgres/metrics/collector_test.go
@@ -0,0 +1,152 @@
+/*
+Copyright The CloudNativePG Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Set default queries", func() {
+	q := NewQueriesCollector("test", nil, "db")
+
+	It("does assign nothing with empty default queries", func() {
+		Expect(q.userQueries).To(BeEmpty())
+		Expect(q.mappings).To(BeEmpty())
+		Expect(q.variableLabels).To(BeEmpty())
+		Expect(q.userQueries).To(BeEmpty())
+		Expect(q.mappings).To(BeEmpty())
+		Expect(q.variableLabels).To(BeEmpty())
+	})
+
+	It("properly works", func() {
+		Expect(q.userQueries).To(BeEmpty())
+		Expect(q.mappings).To(BeEmpty())
+		Expect(q.variableLabels).To(BeEmpty())
+
+		defaultQueries := UserQueries{
+			"collector": UserQuery{
+				Query: "SELECT FROM unit_tests",
+				Metrics: []Mapping{
+					{
+						"test": ColumnMapping{
+							Usage:       LABEL,
+							Description: "test query",
+						},
+					},
+				},
+			},
+		}
+
+		q.InjectUserQueries(defaultQueries)
+		Expect(len(q.userQueries)).To(BeEquivalentTo(1))
+		Expect(len(q.mappings)).To(BeEquivalentTo(1))
+		Expect(q.mappings["collector"]["test"].Name).To(BeEquivalentTo("test"))
+		Expect(q.variableLabels["collector"]).To(BeEquivalentTo(VariableSet{"test"}))
+	})
+})
+
+var _ = Describe("QueryCollector tests", func() {
+	Context("collect metric tests", func() {
+		It("should ensure that a metric without conversion is discarded", func() {
+			qc := QueryCollector{}
+			ch := make(chan prometheus.Metric, 10)
+			metricMap := MetricMap{
+				Name:       "MALFORMED_COUNTER",
+				Discard:    true,
+				Conversion: nil,
+				Label:      false,
+			}
+			qc.collectConstMetric(
+				metricMap,
+				"COUNTER_TEST",
+				[]string{"COUNTER_TEST"},
+				ch,
+			)
+			Expect(ch).To(BeEmpty())
+		})
+
+		It("should ensure that a metric with a bad conversion value is discarded", func() {
+			qc := QueryCollector{}
+			ch := make(chan prometheus.Metric, 10)
+			cm := ColumnMapping{Usage: COUNTER}
+			cs := cm.ToMetricMap("TEST_COLUMN", "TEST_NAMESPACE", []string{"TEST_VARIABLE"})
+			for _, metricMap := range cs {
+				// int are not converted
+				qc.collectConstMetric(metricMap, 12, []string{"TEST"}, ch)
+			}
+			Expect(ch).To(HaveLen(0))
+		})
+
+		It("should ensure that a correctly formed counter is sent", func() {
+			qc := QueryCollector{}
+			ch := make(chan prometheus.Metric, 10)
+			cm := ColumnMapping{Usage: COUNTER}
+			cs := cm.ToMetricMap("TEST_COLUMN", "TEST_NAMESPACE", []string{"TEST_VARIABLE"})
+			for _, metricMap := range cs {
+				qc.collectConstMetric(metricMap, int64(12), []string{"TEST"}, ch)
+			}
+			Expect(ch).To(HaveLen(1))
+			res := <-ch
+			desc := res.Desc()
+			Expect(desc).To(ContainSubstring("TEST_NAMESPACE_TEST_COLUMN"))
+			Expect(desc).To(ContainSubstring("TEST_VARIABLE"))
+		})
+
+		Context("fetch label testing", func() {
+			It("should correctly fetch the mapped labels", func() {
+				qc := QueryCollector{
+					columnMapping: map[string]MetricMap{
+						"LABEL_ENABLED": {
+							Label: true,
+						},
+						"LABEL_NOT_ENABLED": {
+							Label: false,
+						},
+					},
+				}
+				labels, success := qc.collectLabels(
+					[]string{"LABEL_ENABLED", "LABEL_NOT_ENABLED"},
+					[]interface{}{"SHOULD_FETCH", "SHOULD_NOT_FETCH"},
+				)
+				Expect(success).To(BeTrue())
+				Expect(labels).To(HaveLen(1))
+				Expect(labels).To(ContainElements("SHOULD_FETCH"))
+			})
+
+			It("should report success false when the fetched data conversion is not supported", func() {
+				qc := QueryCollector{
+					columnMapping: map[string]MetricMap{
+						"LABEL_ENABLED": {
+							Label: true,
+						},
+					},
+				}
+				labels, success := qc.collectLabels(
+					[]string{"LABEL_ENABLED"},
+					// int is not supported
+					[]interface{}{234},
+				)
+
+				Expect(success).To(BeFalse())
+				Expect(labels).To(BeZero())
+			})
+		})
+	})
+})
