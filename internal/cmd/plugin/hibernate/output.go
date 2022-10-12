8f85512390ef3ff4b135ef39bc0058c74666f917
Marco Nenciarini <marco.nenciarini@enterprisedb.com>
2022-10-12 11:20:10 +0200

    chore: review
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>

--- internal/cmd/plugin/hibernate/output.go
+++ internal/cmd/plugin/hibernate/output.go
@@ -1,3 +1,19 @@
+/*
+Copyright The CloudNativePG Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package hibernate
 
 import (
@@ -11,8 +27,8 @@ import (
 
 	"github.com/cheynewallace/tabby"
 	"github.com/logrusorgru/aurora/v3"
-	"gopkg.in/yaml.v3"
 	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/yaml"
 
 	apiv1 "github.com/cloudnative-pg/cloudnative-pg/api/v1"
 	"github.com/cloudnative-pg/cloudnative-pg/internal/cmd/plugin"
@@ -107,13 +123,12 @@ func (t *textStatusOutputManager) addPVCGroupInformation(
 
 	t.textPrinter.AddHeader(aurora.Green("PG CONTROL DATA"))
 	t.textPrinter.AddLine(value)
-	t.textPrinter.AddLine()
 }
 
 func (t *textStatusOutputManager) execute() error {
 	// do not remove this is to flush the writer cache into the buffer
 	t.textPrinter.Print()
-	fmt.Println(t.stdoutBuffer)
+	fmt.Print(t.stdoutBuffer)
 	return nil
 }
 
@@ -189,7 +204,7 @@ func (t *jsonStatusOutputManager) addPVCGroupInformation(
 		tmp[res[0]] = strings.Trim(res[1], " ")
 	}
 
-	t.mapToSerialize["pgData"] = tmp
+	t.mapToSerialize["pgControlData"] = tmp
 }
 
 func (t *jsonStatusOutputManager) execute() error {
