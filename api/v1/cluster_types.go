ef996b6a3644ff3f541ffaa3b1d7101502597199
Philippe Scorsolini <philippe.scorsolini@enterprisedb.com>
2022-12-16 13:33:19 +0100

    fix: use correct size to update WAL PVC (#1205)
    Previously we used the main PVC size to update the one
    dedicated to WAL storage. This led to "-wal" PVCs being created with
    the correct size at first, but then, in case the main PVC had a
    different size, to be resized to the main PVC's size.
    
    Closes: #1190
    
    Signed-off-by: Philippe Scorsolini <p.scorsolini@gmail.com>
    (cherry picked from commit 3c39d19c9300cb7412ffc505bdd6b3d974cd5487)

--- api/v1/cluster_types.go
+++ api/v1/cluster_types.go
@@ -79,6 +79,10 @@ const (
 	// get the name of the pull secret
 	ClusterSecretSuffix = "-pull-secret"
 
+	// WalArchiveVolumeSuffix is the suffix appended to the instance name to
+	// get the name of the PVC dedicated to WAL files.
+	WalArchiveVolumeSuffix = "-wal"
+
 	// StreamingReplicationUser is the name of the user we'll use for
 	// streaming replication purposes
 	StreamingReplicationUser = "streaming_replica"
@@ -1856,7 +1860,7 @@ func (cluster *Cluster) ShouldCreateWalArchiveVolume() bool {
 
 // GetWalArchiveVolumeSuffix gets the wal archive volume name suffix
 func (cluster *Cluster) GetWalArchiveVolumeSuffix() string {
-	return "-wal"
+	return WalArchiveVolumeSuffix
 }
 
 // GetPostgresUID returns the UID that is being used for the "postgres"
