e7e58a29097dfc7f4127e3f3298ae7d2016b74d0
Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>
2022-12-20 11:46:35 -0300

    Revert "feat: allow fields remapping in JSON logs (#843)"
    This reverts commit 90c2f6b6cb88c9c592ba504e4d7e506b243c35d0.

--- pkg/specs/containers.go
+++ pkg/specs/containers.go
@@ -23,7 +23,6 @@ import (
 
 	apiv1 "github.com/cloudnative-pg/cloudnative-pg/api/v1"
 	"github.com/cloudnative-pg/cloudnative-pg/internal/configuration"
-	"github.com/cloudnative-pg/cloudnative-pg/pkg/management/log"
 	"github.com/cloudnative-pg/cloudnative-pg/pkg/utils"
 )
 
@@ -55,7 +54,6 @@ func addManagerLoggingOptions(cluster apiv1.Cluster, container *corev1.Container
 	if cluster.Spec.LogLevel != "" {
 		container.Command = append(container.Command, fmt.Sprintf("--log-level=%s", cluster.Spec.LogLevel))
 	}
-	container.Command = append(container.Command, log.GetFieldsRemapFlags()...)
 }
 
 // CreateContainerSecurityContext initializes container security context
