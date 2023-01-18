637c34633dbf7bfd411792e7a23a9e0f503ce4c1
John Long <jlong49@users.noreply.github.com>
2023-01-18 11:20:32 -0500

    feat: support adding walStorage on existing clusters (#1066)
    Closes: #675
    
    Signed-off-by: John Long <john.long@enterprisedb.com>
    Signed-off-by: Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    Co-authored-by: Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>
    Co-authored-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    Co-authored-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>

--- internal/management/controller/instance_startup.go
+++ internal/management/controller/instance_startup.go
@@ -18,6 +18,8 @@ package controller
 
 import (
 	"context"
+	"io/fs"
+	"os"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/util/retry"
@@ -25,8 +27,10 @@ import (
 
 	apiv1 "github.com/cloudnative-pg/cloudnative-pg/api/v1"
 	"github.com/cloudnative-pg/cloudnative-pg/controllers"
+	"github.com/cloudnative-pg/cloudnative-pg/pkg/fileutils"
 	"github.com/cloudnative-pg/cloudnative-pg/pkg/management/log"
 	postgresSpec "github.com/cloudnative-pg/cloudnative-pg/pkg/postgres"
+	"github.com/cloudnative-pg/cloudnative-pg/pkg/specs"
 	pkgUtils "github.com/cloudnative-pg/cloudnative-pg/pkg/utils"
 )
 
@@ -252,4 +256,49 @@ func (r *InstanceReconciler) verifyPgDataCoherenceForPrimary(
 		// Now I can demote myself
 		return r.instance.Demote(cluster)
 	}
+}
+
+// ReconcileWalStorage moves the files from PGDATA/pg_wal to the volume attached, if exists, and
+// creates a symlink for it
+func (r *InstanceReconciler) ReconcileWalStorage(ctx context.Context) error {
+	contextLogger := log.FromContext(ctx)
+
+	if pgWalExists, err := fileutils.FileExists(specs.PgWalVolumePath); err != nil {
+		return err
+	} else if !pgWalExists {
+		return nil
+	}
+
+	pgWalDirInfo, err := os.Lstat(specs.PgWalPath)
+	if err != nil {
+		return err
+	}
+	// The pgWalDir it's already a symlink meaning that there's nothing to do
+	mode := pgWalDirInfo.Mode() & fs.ModeSymlink
+	if !pgWalDirInfo.IsDir() && mode != 0 {
+		return nil
+	}
+
+	// We discarded every possibility that this has been done, let's move the current file to their
+	// new location
+	contextLogger.Info("Moving data", "from", specs.PgWalPath, "to", specs.PgWalVolumePgWalPath)
+	if err := fileutils.MoveDirectoryContent(specs.PgWalPath, specs.PgWalVolumePgWalPath); err != nil {
+		contextLogger.Error(err, "Moving data", "from", specs.PgWalPath, "to",
+			specs.PgWalVolumePgWalPath)
+		return err
+	}
+
+	contextLogger.Debug("Deleting old path", "path", specs.PgWalPath)
+	if err := fileutils.RemoveFile(specs.PgWalPath); err != nil {
+		contextLogger.Error(err, "Deleting old path", "path", specs.PgWalPath)
+		return err
+	}
+
+	// We moved all the files now we should create the proper symlink
+	contextLogger.Debug("Creating symlink", "from", specs.PgWalPath, "to", specs.PgWalVolumePgWalPath)
+	if err := os.Symlink(specs.PgWalVolumePgWalPath, specs.PgWalPath); err != nil {
+		return err
+	}
+
+	return nil
 }
