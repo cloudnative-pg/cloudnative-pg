a5d341d0f5a1cbf4b488c01c682f01e8f1c5dc1d
Jaime Silvela <jaime.silvela@enterprisedb.com>
2023-03-03 18:44:58 +0100

    test: add E2E test for declarative roles
    Signed-off-by: Jaime Silvela <jaime.silvela@enterprisedb.com>

--- internal/management/controller/roles/runnable_test.go
+++ internal/management/controller/roles/runnable_test.go
@@ -114,11 +114,11 @@ var _ = Describe("Role synchronizer tests", func() {
 		err := synchronizeRoles(ctx, &rm, &syncInstance, &managedConf)
 		Expect(err).ShouldNot(HaveOccurred())
 		Expect(rm.callHistory).To(ConsistOf(
-			([]funcCall{
+			[]funcCall{
 				{"list", ""},
 				{"create", "edb_test"},
 				{"create", "foo_bar"},
-			}),
+			},
 		))
 		Expect(rm.callHistory).To(ConsistOf(
 			funcCall{"list", ""},
