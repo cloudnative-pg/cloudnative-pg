3798c23aa10baaf677a2aff2731fd3162e409a81
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2023-03-31 05:46:05 +0200

    feat: configurable creation of the `-any` service (#1755)
    The creation of the `-any` service is opted-out by default. which could be
     opted-in again via the `CREATE_ANY_SERVICE` operator configuration 
    parameter,  to be used inside the operator Secret or as an environment variable. 
    
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@gmail.com>
    Signed-off-by: Tao Li <tao.li@enterprisedb.com>
    Co-authored-by: Tao Li <tao.li@enterprisedb.com>

--- pkg/specs/jobs.go
+++ pkg/specs/jobs.go
@@ -25,6 +25,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	apiv1 "github.com/cloudnative-pg/cloudnative-pg/api/v1"
+	"github.com/cloudnative-pg/cloudnative-pg/internal/configuration"
 	"github.com/cloudnative-pg/cloudnative-pg/pkg/management/log"
 	"github.com/cloudnative-pg/cloudnative-pg/pkg/utils"
 )
@@ -266,8 +267,7 @@ func createPrimaryJob(cluster apiv1.Cluster, nodeSerial int, role jobRole, initC
 					},
 				},
 				Spec: corev1.PodSpec{
-					Hostname:  jobName,
-					Subdomain: cluster.GetServiceAnyName(),
+					Hostname: jobName,
 					InitContainers: []corev1.Container{
 						createBootstrapContainer(cluster),
 					},
@@ -294,6 +294,10 @@ func createPrimaryJob(cluster apiv1.Cluster, nodeSerial int, role jobRole, initC
 				},
 			},
 		},
+	}
+
+	if configuration.Current.CreateAnyService {
+		job.Spec.Template.Spec.Subdomain = cluster.GetServiceAnyName()
 	}
 
 	cluster.SetInheritedDataAndOwnership(&job.ObjectMeta)
