4df08a85f91b4ccf8e61027de7c5ae5c658f0d1d
Niccolò Fei <niccolo.fei@enterprisedb.com>
2023-04-13 11:12:14 +0200

    test: always run E2Es in unique namespaces (#1904)
    fixes some cases where a namespace being terminated would
    induce errors in the creation of a namespace with the same name.
    
    Signed-off-by: Niccolò Fei <niccolo.fei@enterprisedb.com>

--- tests/e2e/fastswitchover_test.go
+++ tests/e2e/fastswitchover_test.go
@@ -36,7 +36,6 @@ import (
 
 var _ = Describe("Fast switchover", Serial, Label(tests.LabelPerformance, tests.LabelSelfHealing), func() {
 	const (
-		namespace                      = "primary-switchover-time"
 		sampleFileWithReplicationSlots = fixturesDir +
 			"/fastswitchover/cluster-fast-switchover-with-repl-slots.yaml.template"
 		sampleFileWithoutReplicationSlots = fixturesDir + "/fastswitchover/cluster-fast-switchover.yaml.template"
@@ -53,11 +52,7 @@ var _ = Describe("Fast switchover", Serial, Label(tests.LabelPerformance, tests.
 			Skip("This test is not run on an IBM architecture")
 		}
 	})
-	JustAfterEach(func() {
-		if CurrentSpecReport().Failed() {
-			env.DumpNamespaceObjects(namespace, "out/"+CurrentSpecReport().LeafNodeText+".log")
-		}
-	})
+
 	// Confirm that a standby closely following the primary doesn't need more
 	// than maxSwitchoverTime seconds to be promoted and be able to start
 	// inserting records. We then expect the old primary to be back in
@@ -68,9 +63,13 @@ var _ = Describe("Fast switchover", Serial, Label(tests.LabelPerformance, tests.
 	Context("without HA Replication Slots", func() {
 		It("can do a fast switchover", func() {
 			// Create a cluster in a namespace we'll delete after the test
+			namespace := "primary-switchover-time"
 			err := env.CreateNamespace(namespace)
 			Expect(err).ToNot(HaveOccurred())
 			DeferCleanup(func() error {
+				if CurrentSpecReport().Failed() {
+					env.DumpNamespaceObjects(namespace, "out/"+CurrentSpecReport().LeafNodeText+".log")
+				}
 				return env.DeleteNamespaceAndWait(namespace, 60)
 			})
 			assertFastSwitchover(namespace, sampleFileWithoutReplicationSlots, clusterName, webTestFile, webTestJob)
@@ -79,9 +78,13 @@ var _ = Describe("Fast switchover", Serial, Label(tests.LabelPerformance, tests.
 	Context("with HA Replication Slots", func() {
 		It("can do a fast switchover", func() {
 			// Create a cluster in a namespace we'll delete after the test
+			namespace := "primary-switchover-time-with-slots"
 			err := env.CreateNamespace(namespace)
 			Expect(err).ToNot(HaveOccurred())
 			DeferCleanup(func() error {
+				if CurrentSpecReport().Failed() {
+					env.DumpNamespaceObjects(namespace, "out/"+CurrentSpecReport().LeafNodeText+".log")
+				}
 				return env.DeleteNamespaceAndWait(namespace, 60)
 			})
 			assertFastSwitchover(namespace, sampleFileWithReplicationSlots, clusterName, webTestFile, webTestJob)
