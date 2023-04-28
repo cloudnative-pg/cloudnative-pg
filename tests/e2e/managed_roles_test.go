b4beb19635927e5d7584ec9817fe8178718c9922
Jaime Silvela <jaime.silvela@enterprisedb.com>
2023-04-28 18:12:49 +0200

    ci: update to CIclops 1.2.0
    Signed-off-by: Jaime Silvela <jaime.silvela@enterprisedb.com>

--- tests/e2e/managed_roles_test.go
+++ tests/e2e/managed_roles_test.go
@@ -211,7 +211,7 @@ var _ = Describe("Managed roles tests", Label(tests.LabelSmoke, tests.LabelBasic
 
 			By("Verify the role has been updated in the database", func() {
 				primaryPodInfo, err := env.GetClusterPrimary(namespace, clusterName)
-				Expect(err).ToNot(HaveOccurred())
+				Expect(err).To(HaveOccurred())
 
 				Eventually(func() string {
 					cmd := fmt.Sprintf("psql -U postgres postgres -tAc "+
