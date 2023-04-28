df73c9c1f9a7bffc38a58e196310e4f6ca62547f
Tao Li <tao.li@enterprisedb.com>
2023-04-28 22:05:13 +0800

    feat: configure deployment strategy of a `Pooler` resource (#1983)
    Introduce the `deploymentStrategy` stanza from the Kubernetes
    `Deployment` API to the `Pooler` resource in order to manage
    the strategy to replace old pods by new ones.
    For details, please refer to:
    https://kubernetes.io/docs/concepts/workloads/controllers/deployment/#strategy
    
    Closes #1982
    
    Signed-off-by: Tao Li <tao.li@enterprisedb.com>
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
    Co-authored-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
    (cherry picked from commit ef0dd5fdfed2e3918f5e1d89a485c25f4b4a554a)

--- api/v1/pooler_types.go
+++ api/v1/pooler_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1
 
 import (
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -68,6 +69,9 @@ type PoolerSpec struct {
 
 	// The PgBouncer configuration
 	PgBouncer *PgBouncerSpec `json:"pgbouncer"`
+
+	// The deployment strategy to use for pgbouncer to replace existing pods with new ones
+	DeploymentStrategy *appsv1.DeploymentStrategy `json:"deploymentStrategy,omitempty"`
 }
 
 // PodTemplateSpec is a structure allowing the user to set
