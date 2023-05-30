f076be0449b21a4f49d3866fe49b9253b93e206a
Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>
2023-05-30 14:08:50 +0200

    feat: Add `pg_failover_slots` to managed extensions (#2057)
    Add [pg_failover_slots](https://github.com/EnterpriseDB/pg_failover_slots),
    which was recently released by EDB under the PostgreSQL license,
    to the list of extensions managed by the operator.
    
    When a user adds a `pg_failover_slots.*` option in the Postgres configuration
    file, CloudNativePG automatically manages the `shared_preload_libraries` entry.
    
    Closes #2042
    
    Signed-off-by: Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>
    Signed-off-by: Niccolò Fei <niccolo.fei@enterprisedb.com>
    Signed-off-by: Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
    
    Co-authored-by: Niccolò Fei <niccolo.fei@enterprisedb.com>
    Co-authored-by: Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>
    Co-authored-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    Co-authored-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>

--- pkg/postgres/configuration_test.go
+++ pkg/postgres/configuration_test.go
@@ -250,7 +250,8 @@ var _ = Describe("pg_hba.conf generation", func() {
 
 var _ = Describe("pgaudit", func() {
 	var pgaudit *ManagedExtension
-	It("manages pgaudit", func() {
+	BeforeEach(func() {
+		pgaudit = nil
 		for i, ext := range ManagedExtensions {
 			if ext.Name == "pgaudit" {
 				pgaudit = &ManagedExtensions[i]
@@ -259,15 +260,18 @@ var _ = Describe("pgaudit", func() {
 		}
 		Expect(pgaudit).ToNot(BeNil())
 	})
+
 	It("is enabled", func() {
 		userConfigsWithPgAudit := make(map[string]string, 1)
 		userConfigsWithPgAudit["pgaudit.xxx"] = "test"
 		Expect(pgaudit.IsUsed(userConfigsWithPgAudit)).To(BeTrue())
 	})
+
 	It("is not enabled", func() {
 		userConfigsWithNoPgAudit := make(map[string]string, 1)
 		Expect(pgaudit.IsUsed(userConfigsWithNoPgAudit)).To(BeFalse())
 	})
+
 	It("adds pgaudit to shared_preload_library", func() {
 		info := ConfigurationInfo{
 			Settings:                        CnpgConfigurationSettings,
@@ -285,6 +289,7 @@ var _ = Describe("pgaudit", func() {
 		Expect(libraries).ToNot(ContainElement(""))
 		Expect(libraries).To(ContainElements("pgaudit", "other_library"))
 	})
+
 	It("adds pg_stat_statements to shared_preload_library", func() {
 		info := ConfigurationInfo{
 			Settings:                        CnpgConfigurationSettings,
@@ -302,7 +307,8 @@ var _ = Describe("pgaudit", func() {
 		Expect(libraries).ToNot(ContainElement(""))
 		Expect(libraries).To(ContainElements("pg_stat_statements", "other_library"))
 	})
-	It("adds pg_stat_statements and pg_audit to shared_preload_library", func() {
+
+	It("adds pg_stat_statements and pgaudit to shared_preload_library", func() {
 		info := ConfigurationInfo{
 			Settings:     CnpgConfigurationSettings,
 			MajorVersion: 130000,
@@ -319,5 +325,23 @@ var _ = Describe("pgaudit", func() {
 		Expect(len(libraries)).To(BeNumerically("==", 2))
 		Expect(libraries).ToNot(ContainElement(""))
 		Expect(libraries).To(ContainElements("pg_stat_statements", "pgaudit"))
+	})
+})
+
+var _ = Describe("pg_failover_slots", func() {
+	It("adds pg_failover_slots to shared_preload_library", func() {
+		info := ConfigurationInfo{
+			Settings:                        CnpgConfigurationSettings,
+			MajorVersion:                    130000,
+			UserSettings:                    map[string]string{"pg_failover_slots.something": "something"},
+			IncludingMandatory:              true,
+			IncludingSharedPreloadLibraries: true,
+			SyncReplicas:                    0,
+		}
+		config := CreatePostgresqlConfiguration(info)
+		libraries := strings.Split(config.GetConfig(SharedPreloadLibraries), ",")
+		Expect(len(libraries)).To(BeNumerically("==", 1))
+		Expect(libraries).ToNot(ContainElement(""))
+		Expect(libraries).To(ContainElements("pg_failover_slots"))
 	})
 })
