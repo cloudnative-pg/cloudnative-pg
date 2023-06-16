8d9e0ccf44ede78be55db2c689c64ab280d29fe1
Armando Ruocco <armando.ruocco@enterprisedb.com>
2023-06-16 11:54:43 +0200

    e2e: add nodes_used coverage
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- tests/e2e/asserts_test.go
+++ tests/e2e/asserts_test.go
@@ -2346,7 +2346,7 @@ func collectAndAssertDefaultMetricsPresentOnEachPod(namespace, clusterName, curl
 	By("collecting and verify default set of metrics on each pod", func() {
 		podList, err := env.GetClusterPodList(namespace, clusterName)
 		Expect(err).ToNot(HaveOccurred())
-		expectedKeywordInMetricsOutput := [7]string{
+		expectedKeywordInMetricsOutput := [8]string{
 			"cnpg_pg_settings_setting",
 			"cnpg_backends_waiting_total",
 			"cnpg_pg_postmaster_start_time",
@@ -2354,6 +2354,7 @@ func collectAndAssertDefaultMetricsPresentOnEachPod(namespace, clusterName, curl
 			"cnpg_pg_stat_archiver",
 			"cnpg_pg_stat_bgwriter",
 			"cnpg_pg_stat_database",
+			"cnpg_pg_nodes_used",
 		}
 		for _, pod := range podList.Items {
 			podName := pod.GetName()
