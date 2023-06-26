3661a62fb9b788ea6120a7607e50a8fe361acb8c
Armando Ruocco <armando.ruocco@enterprisedb.com>
2023-06-26 16:38:33 +0200

    chore: update pooler interface usage and add test files
    - Replace pooler interface with pool.Pooler in PostgresManager and pgBouncerInstance structures
    - Add new test files for instance, refresh, secrets, and suite in pgbouncer management controller
    
    Closes #2362
    Tied #2358
    
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

---
+++ internal/pgbouncer/management/controller/secrets_test.go
@@ -0,0 +1,76 @@
+/*
+Copyright The CloudNativePG Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	apiv1 "github.com/cloudnative-pg/cloudnative-pg/api/v1"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("getSecrets tests", func() {
+	var (
+		ctx    context.Context
+		client client.WithWatch
+		pooler *apiv1.Pooler
+	)
+
+	BeforeEach(func() {
+		ctx = context.TODO()
+		client, pooler = buildTestEnv()
+	})
+
+	Context("when status is not populated yet", func() {
+		It("should return error", func() {
+			pooler.Status.Secrets = nil
+
+			_, err := getSecrets(ctx, client, pooler)
+
+			Expect(err).To(HaveOccurred())
+			Expect(err.Error()).To(Equal("status not populated yet"))
+		})
+	})
+
+	Context("when all secrets are found", func() {
+		It("should return secrets without error", func() {
+			res, err := getSecrets(ctx, client, pooler)
+
+			Expect(err).ToNot(HaveOccurred())
+			Expect(res.ClientCA.Name).To(Equal(clientCAName))
+			Expect(res.Client.Name).To(Equal(serverTLSName))
+			Expect(res.ServerCA.Name).To(Equal(serverCAName))
+			Expect(res.AuthQuery.Name).To(Equal(authQueryName))
+		})
+	})
+
+	Context("when a secret is not found", func() {
+		BeforeEach(func() {
+			pooler.Status.Secrets.ServerCA = apiv1.SecretVersion{Name: "nonexistent"}
+		})
+
+		It("should return error", func() {
+			_, err := getSecrets(ctx, client, pooler)
+
+			Expect(err).To(HaveOccurred())
+		})
+	})
+})
