67d746436fc5adf84b134fab8dcf7160a415e0a1
Armando Ruocco <armando.ruocco@enterprisedb.com>
2023-06-30 09:26:45 +0200

    chore: introduce Pooler interface for improved unit testing (#2363)
    This patch replace pooler interface with pool.Pooler in PostgresManager
    and pgBouncerInstance structures, and add new test files for instance,
    refresh, secrets, and suite in pgbouncer management controller
    
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    Signed-off-by: Jaime Silvela <jaime.silvela@enterprisedb.com>
    Co-authored-by: Jaime Silvela <jaime.silvela@enterprisedb.com>
    (cherry picked from commit 16d73f9aac49ffe208548c5b342932f73a73dcd5)

---
+++ internal/pgbouncer/management/controller/instance_test.go
@@ -0,0 +1,106 @@
+/*
+Copyright The CloudNativePG Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"database/sql"
+	"sync"
+
+	"github.com/DATA-DOG/go-sqlmock"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("PgBouncerInstance", func() {
+	var (
+		db   *sql.DB
+		mock sqlmock.Sqlmock
+		err  error
+	)
+
+	BeforeEach(func() {
+		db, mock, err = sqlmock.New()
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		Expect(mock.ExpectationsWereMet()).To(Succeed())
+	})
+
+	Context("when the instance is paused", func() {
+		It("should not return an error", func() {
+			mock.ExpectExec("PAUSE").WillReturnResult(sqlmock.NewResult(1, 1))
+
+			pgBouncerInstance := &pgBouncerInstance{
+				mu:     &sync.RWMutex{},
+				paused: false,
+				pool:   &fakePooler{DB: db},
+			}
+
+			err := pgBouncerInstance.Pause()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(pgBouncerInstance.Paused()).To(BeTrue())
+		})
+	})
+
+	Context("when the instance is resumed", func() {
+		It("should not return an error", func() {
+			mock.ExpectExec("RESUME").WillReturnResult(sqlmock.NewResult(1, 1))
+
+			pgBouncerInstance := &pgBouncerInstance{
+				mu:     &sync.RWMutex{},
+				paused: true,
+				pool:   &fakePooler{DB: db},
+			}
+
+			err := pgBouncerInstance.Resume()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(pgBouncerInstance.Paused()).To(BeFalse())
+		})
+	})
+
+	Context("when the instance configuration is reloaded", func() {
+		It("should not return an error", func() {
+			mock.ExpectExec("RELOAD").WillReturnResult(sqlmock.NewResult(1, 1))
+
+			pgBouncerInstance := &pgBouncerInstance{
+				mu:     &sync.RWMutex{},
+				paused: false,
+				pool:   &fakePooler{DB: db},
+			}
+
+			err := pgBouncerInstance.Reload()
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+})
+
+type fakePooler struct {
+	DB *sql.DB
+}
+
+func (f *fakePooler) Connection(_ string) (*sql.DB, error) {
+	return f.DB, nil
+}
+
+func (f *fakePooler) GetDsn(_ string) string {
+	return "postgres://user:password@localhost:5432/testdb"
+}
+
+func (f *fakePooler) ShutdownConnections() {
+}
---
+++ internal/pgbouncer/management/controller/suite_test.go
@@ -0,0 +1,82 @@
+/*
+Copyright The CloudNativePG Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"testing"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	apiv1 "github.com/cloudnative-pg/cloudnative-pg/api/v1"
+	"github.com/cloudnative-pg/cloudnative-pg/internal/scheme"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+const (
+	authQueryName = "authquery"
+	serverCAName  = "serverca"
+	serverTLSName = "servertls"
+	clientCAName  = "clientca"
+)
+
+func TestController(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "PGBouncer Controller Suite")
+}
+
+func buildTestEnv() (client.WithWatch, *apiv1.Pooler) {
+	authQuerySecret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{Name: authQueryName, Namespace: "default"},
+	}
+	serverCASecret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{Name: serverCAName, Namespace: "default"},
+	}
+	serverCertSecret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{Name: serverTLSName, Namespace: "default"},
+	}
+	clientCASecret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{Name: clientCAName, Namespace: "default"},
+	}
+
+	pooler := &apiv1.Pooler{
+		ObjectMeta: metav1.ObjectMeta{Name: "test-pooler", Namespace: "default"},
+		Spec: apiv1.PoolerSpec{
+			PgBouncer: &apiv1.PgBouncerSpec{
+				AuthQuerySecret: &apiv1.LocalObjectReference{Name: authQueryName},
+			},
+			Cluster: apiv1.LocalObjectReference{
+				Name: "cluster",
+			},
+		},
+		Status: apiv1.PoolerStatus{
+			Secrets: &apiv1.PoolerSecrets{
+				ServerCA:  apiv1.SecretVersion{Name: serverCAName},
+				ServerTLS: apiv1.SecretVersion{Name: serverTLSName},
+				ClientCA:  apiv1.SecretVersion{Name: clientCAName},
+			},
+		},
+	}
+
+	return fake.NewClientBuilder().WithScheme(scheme.BuildWithAllKnownScheme()).
+		WithObjects(pooler, authQuerySecret, serverCASecret, serverCertSecret, clientCASecret).
+		Build(), pooler
+}
