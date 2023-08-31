9282ee9b1b448d069bd84ef4710782ec9795c3c8
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2023-08-31 14:39:55 +0200

    chore: review
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>

--- pkg/management/url/url.go
+++ pkg/management/url/url.go
@@ -37,6 +37,9 @@ const (
 	// PathReady is the URL oath for Ready State
 	PathReady string = "/readyz"
 
+	// PathPGControlData is the URL path for PostgreSQL pg_controldata output
+	PathPGControlData string = "/pg/controldata"
+
 	// PathPgStatus is the URL path for PostgreSQL Status
 	PathPgStatus string = "/pg/status"
 
@@ -51,9 +54,6 @@ const (
 
 	// PathCache is the URL path for cached resources
 	PathCache string = "/cache/"
-
-	// PathPGControlData is the URL path for PostgreSQL pg_controldata output
-	PathPGControlData string = "/pg/controldata"
 
 	// StatusPort is the port for status HTTP requests
 	StatusPort int = 8000
