8ee1a9d824419739ef3b91560b080bb027a4940d
Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>
2023-10-02 05:25:25 -0300

    chore: change webhook name from kb.io to cnpg.io (#2851)
    Signed-off-by: Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>
    Signed-off-by: Jaime Silvela <jaime.silvela@enterprisedb.com>
    Co-authored-by: Jaime Silvela <jaime.silvela@enterprisedb.com>

--- tests/e2e/upgrade_test.go
+++ tests/e2e/upgrade_test.go
@@ -146,7 +146,7 @@ var _ = Describe("Upgrade", Label(tests.LabelUpgrade, tests.LabelNoOpenshift), O
 			oldPrimary := cluster.Status.CurrentPrimary
 			oldPrimaryTimestamp := cluster.Status.CurrentPrimaryTimestamp
 			// Update the configuration. It may take some time after the
-			// upgrade for the webhook "mcluster.kb.io" to work and accept
+			// upgrade for the webhook "mcluster.cnpg.io" to work and accept
 			// the `apply` command
 
 			Eventually(func() error {
