91ed4bd5d320ce24d53b3d55778fa7858d0f49b5
Lennart Krauch <lennart.krauch@inovex.de>
2023-10-05 17:45:10 +0200

    removed too-many-open-files fix as discussed
    Signed-off-by: Lennart Krauch <lennart.krauch@inovex.de>

--- hack/setup-cluster.sh
+++ hack/setup-cluster.sh
@@ -168,15 +168,6 @@ EOF
   if [ -n "${ENABLE_REGISTRY:-}" ]; then
     docker network connect "kind" "${registry_name}" &>/dev/null || true
   fi
-
-  if [[ "$OS" == "darwin" && "$ARCH" == "arm64" ]]; then
-    echo "disabled too-many-open-files workaround on darwin arm64"
-  else
-    # Workaround for https://kind.sigs.k8s.io/docs/user/known-issues/#pod-errors-due-to-too-many-open-files
-    for node in $(kind get nodes --name "${cluster_name}"); do
-      docker exec "$node" sysctl fs.inotify.max_user_watches=524288 fs.inotify.max_user_instances=512
-    done
-  fi
 }
 
 export_logs_kind() {
