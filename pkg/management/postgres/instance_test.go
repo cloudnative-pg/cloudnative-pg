34ca687b3441aa5af040d4c3d12478c47e6d8f75
Armando Ruocco <armando.ruocco@enterprisedb.com>
2023-10-09 17:11:54 +0200

    test: fix unit test
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- pkg/management/postgres/instance_test.go
+++ pkg/management/postgres/instance_test.go
@@ -228,11 +228,11 @@ var _ = Describe("check atomic bool", func() {
 var _ = Describe("testing the compute of smart shutdown time", func() {
 	It("should be 30 as the result of 'MaxStopDelay - SmartStopDelay'  is less than it", func() {
 		instance := Instance{MaxStopDelay: 100, SmartStopDelay: 100}
-		Expect(instance.GetSmartShutdownTimeout()).To(BeNumerically("==", 30))
+		Expect(instance.GetSmartShutdownTimeout()).To(BeEquivalentTo(85))
 	})
 
 	It("should be the result of  'MaxStopDelay - SmartStopDelay'", func() {
 		instance := Instance{MaxStopDelay: 200, SmartStopDelay: 100}
-		Expect(instance.GetSmartShutdownTimeout()).To(BeNumerically("==", 100))
+		Expect(instance.GetSmartShutdownTimeout()).To(BeEquivalentTo(100))
 	})
 })
