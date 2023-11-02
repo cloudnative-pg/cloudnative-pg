4d071c3ea57e5cec7338d3b5796d3333435ce696
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2023-11-02 18:21:17 +0100

    chore: add todo entry to allow concurrent reconciliations
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>

--- controllers/backup_controller.go
+++ controllers/backup_controller.go
@@ -578,6 +578,8 @@ func (r *BackupReconciler) SetupWithManager(ctx context.Context, mgr ctrl.Manage
 			builder.WithPredicates(volumeSnapshotsPredicate),
 		)
 	}
+	// TODO: allow concurrent reconciliations when the hot snapshot backup reconciler
+	// will allow that
 	controllerBuilder = controllerBuilder.WithOptions(controller.Options{MaxConcurrentReconciles: 1})
 	return controllerBuilder.Complete(r)
 }
