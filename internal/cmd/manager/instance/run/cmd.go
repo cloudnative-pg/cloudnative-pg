feaa7745a8ec4cbcf2c0cb6070c9ce6c3d76f902
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2023-12-14 01:57:48 +0100

    feat: reconciler for `externalClusters` entries (#3533)
    Introduce a reconciler for external clusters.
    
    Whenever a password is supplied within an `externalClusters` entry, the
    instance manager autonomously manages a PostgreSQL password file for it,
    residing at `/controller/external/NAME/pgpass` in each instance (where
    "NAME" is the name of the external cluster entry).
    
    This approach empowers CloudNativePG to securely establish connections
    with an external server without exposing any passwords in the connection
    string. Instead, the connection safely references the aforementioned
    file through the `passfile` connection parameter.
    
    Closes #2386
    
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    Signed-off-by: Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>
    Co-authored-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    Co-authored-by: Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>

--- internal/cmd/manager/instance/run/cmd.go
+++ internal/cmd/manager/instance/run/cmd.go
@@ -38,6 +38,7 @@ import (
 	apiv1 "github.com/cloudnative-pg/cloudnative-pg/api/v1"
 	"github.com/cloudnative-pg/cloudnative-pg/internal/cmd/manager/instance/run/lifecycle"
 	"github.com/cloudnative-pg/cloudnative-pg/internal/management/controller"
+	"github.com/cloudnative-pg/cloudnative-pg/internal/management/controller/externalservers"
 	"github.com/cloudnative-pg/cloudnative-pg/internal/management/controller/roles"
 	"github.com/cloudnative-pg/cloudnative-pg/internal/management/controller/slots/runner"
 	"github.com/cloudnative-pg/cloudnative-pg/internal/management/controller/tablespaces"
@@ -256,6 +257,13 @@ func runSubCommand(ctx context.Context, instance *postgres.Instance) error {
 	if err := tablespaces.NewTablespaceReconciler(instance, mgr.GetClient()).
 		SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create tablespace reconciler")
+		return err
+	}
+
+	setupLog.Info("starting external server manager")
+	if err := externalservers.NewReconciler(instance, mgr.GetClient()).
+		SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create external servers reconciler")
 		return err
 	}
 
