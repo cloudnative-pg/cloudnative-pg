319675f1595b5fd5f2d4a5e8b56cb77d6cf2a36c
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2023-12-19 13:49:28 +0000

    chore: review
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>

--- pkg/management/postgres/join.go
+++ pkg/management/postgres/join.go
@@ -70,18 +70,26 @@ func ClonePgData(connectionString, targetPgData, walDir string) error {
 
 // Join creates a new instance joined to an existing PostgreSQL cluster
 func (info InitInfo) Join(cluster *apiv1.Cluster) error {
-	// We explicitly set a high-enough wal_sender_timeout for join-related pg_basebackup executions.
-	// A short timeout could not be enough in case the instance is slow to send data, like when the I/O is overloaded.
-	primaryConnInfo := buildPrimaryConnInfo(info.ParentNode, info.PodName) +
-		" dbname=postgres connect_timeout=5 options='-c wal_sender_timeout=300s'"
+	primaryConnInfo := buildPrimaryConnInfo(info.ParentNode, info.PodName) + " dbname=postgres connect_timeout=5"
+
+	pgVersion, err := cluster.GetPostgresqlVersion()
+	if err != nil {
+		log.Warning(
+			"Error while parsing PostgreSQL server version, defaulting to PostgreSQL 11",
+			"imageName", cluster.GetImageName(),
+			"err", err)
+	} else if pgVersion >= 120000 {
+		// We explicitly set a high-enough wal_sender_timeout for join-related pg_basebackup executions.
+		// A short timeout could not be enough in case the instance is slow to send data, like when the I/O is overloaded.
+		primaryConnInfo += " options='-c wal_sender_timeout=300s'"
+	}
 
 	coredumpFilter := cluster.GetCoredumpFilter()
 	if err := system.SetCoredumpFilter(coredumpFilter); err != nil {
 		return err
 	}
 
-	err := ClonePgData(primaryConnInfo, info.PgData, info.PgWal)
-	if err != nil {
+	if err = ClonePgData(primaryConnInfo, info.PgData, info.PgWal); err != nil {
 		return err
 	}
 
