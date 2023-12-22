de3623dcf927b2e1bddbf220cd36394415b0f733
Marco Nenciarini <marco.nenciarini@enterprisedb.com>
2023-12-22 14:34:03 +0100

    fix: release script must update the release branch in docs (#3636)
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    Signed-off-by: Niccolò Fei <niccolo.fei@enterprisedb.com>
    Co-authored-by: Niccolò Fei <niccolo.fei@enterprisedb.com>

--- hack/release.sh
+++ hack/release.sh
@@ -88,7 +88,8 @@ sed -i -e "/Version *= *.*/Is/\".*\"/\"${release_version}\"/" \
     -e "/DefaultOperatorImageName *= *.*/Is/\"\(.*\):.*\"/\"\1:${release_version}\"/" \
     pkg/versions/versions.go
 
-sed -i "s/cnpg-[0-9.]*.yaml/cnpg-${release_version}.yaml/g" \
+sed -i -e "s@release-[0-9.]*/releases/cnpg-[0-9.]*.yaml@${branch}/releases/cnpg-${release_version}.yaml@g" \
+    -e "s@artifacts/release-[0-9.]*/@artifacts/${branch}/@g" \
     docs/src/installation_upgrade.md
 
 CONFIG_TMP_DIR=$(mktemp -d)
