34608837064455a0cf19ce128302eeb46af93297
Armando Ruocco <armando.ruocco@enterprisedb.com>
2024-01-03 14:43:01 +0100

    test: add unit test coverage for `cluster_cleanup` (#3648)
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    Signed-off-by: Jaime Silvela <jaime.silvela@enterprisedb.com>
    Co-authored-by: Jaime Silvela <jaime.silvela@enterprisedb.com>
    (cherry picked from commit 958ce044ef7ccbd65bfb2daa70c3373cee103fa8)

---
+++ controllers/cluster_cleanup_test.go
@@ -0,0 +1,78 @@
+/*
+Copyright The CloudNativePG Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	batchv1 "k8s.io/api/batch/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	schemeBuilder "github.com/cloudnative-pg/cloudnative-pg/internal/scheme"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("cluster_cleanup", func() {
+	var (
+		r      ClusterReconciler
+		scheme *runtime.Scheme
+	)
+
+	BeforeEach(func(ctx SpecContext) {
+		scheme = schemeBuilder.BuildWithAllKnownScheme()
+		r = ClusterReconciler{
+			Scheme: scheme,
+		}
+	})
+
+	It("should delete completed jobs", func(ctx SpecContext) {
+		jobList := &batchv1.JobList{Items: []batchv1.Job{
+			{
+				ObjectMeta: metav1.ObjectMeta{Name: "test-1", Namespace: "test"},
+				Spec: batchv1.JobSpec{
+					Completions: ptr.To(int32(1)),
+				},
+				Status: batchv1.JobStatus{
+					Succeeded: 1,
+				},
+			},
+			{
+				ObjectMeta: metav1.ObjectMeta{Name: "test-2", Namespace: "test"},
+				Spec: batchv1.JobSpec{
+					Completions: ptr.To(int32(1)),
+				},
+				Status: batchv1.JobStatus{
+					Succeeded: 0,
+				},
+			},
+		}}
+		cli := fake.NewClientBuilder().WithScheme(scheme).WithLists(jobList).Build()
+		r.Client = cli
+		r.cleanupCompletedJobs(ctx, *jobList)
+
+		err := cli.Get(ctx, client.ObjectKeyFromObject(&jobList.Items[0]), &batchv1.Job{})
+		Expect(apierrors.IsNotFound(err)).To(BeTrue())
+
+		err = cli.Get(ctx, client.ObjectKeyFromObject(&jobList.Items[1]), &batchv1.Job{})
+		Expect(err).ToNot(HaveOccurred())
+	})
+})
