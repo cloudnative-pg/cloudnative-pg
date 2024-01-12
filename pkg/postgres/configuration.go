cloudnative-pg/cloudnative-pg

commit c2381d1b6cc1fa580d621e0765fa5a0f9e26fb5c

feat: configuration of PostgreSQL ident map file (#3534)
Allow customization of PostgreSQL's `pg_ident.conf` file via the
`.spec.postgresql.pg_ident` stanza, through a list of user name
maps.

Closes #3362

Signed-off-by: nils <nils@june.global>
Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
Signed-off-by: Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>
Co-authored-by: nils <nils@june.global>
Co-authored-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
Co-authored-by: Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>

pkg/postgres/configuration.go
@@ -29,7 +29,11 @@ const (
 	// hbaTemplateString is the template used to generate the pg_hba.conf
 	// configuration file
 	hbaTemplateString = `
-# Grant local access
+#
+# FIXED RULES
+#
+
+# Grant local access (`local` user map)
 local all all peer map=local
 
 # Require client certificate authentication for the streaming_replica user
@@ -37,17 +41,44 @@ hostssl postgres streaming_replica all cert
 hostssl replication streaming_replica all cert
 hostssl all cnpg_pooler_pgbouncer all cert
 
+#
+# USER-DEFINED RULES
+#
+
 {{ range $rule := .UserRules }}
 {{ $rule -}}
 {{ end }}
-{{ if .LDAPConfiguration }}
 
-# LDAP Configuration
+{{ if .LDAPConfiguration }}
+#
+# LDAP CONFIGURATION (optional)
+#
 {{.LDAPConfiguration}}
 {{ end }}
 
-# Otherwise use the default authentication method
+#
+# DEFAULT RULES
+#
 host all all all {{.DefaultAuthenticationMethod}}
+`
+
+	// identTemplateString is the template used to generate the pg_ident.conf
+	// configuration file
+	identTemplateString = `
+#
+# FIXED RULES
+#
+
+# Grant local access (`local` user map)
+local {{.Username}} postgres
+
+#
+# USER-DEFINED RULES
+#
+
+{{ range $rule := .Mappings }}
+{{ $rule -}}
+{{ end }}
 `
 
 	// fixedConfigurationParameter are the configuration parameters
@@ -151,6 +182,9 @@ host all all all {{.DefaultAuthenticationMethod}}
 
 // hbaTemplate is the template used to create the HBA configuration
 var hbaTemplate = template.Must(template.New("pg_hba.conf").Parse(hbaTemplateString))
+
+// identTemplate is the template used to create the HBA configuration
+var identTemplate = template.Must(template.New("pg_ident.conf").Parse(identTemplateString))
 
 // MajorVersionRangeUnlimited is used to represent an unbound limit in a MajorVersionRange
 const MajorVersionRangeUnlimited = 0
@@ -442,6 +476,26 @@ func CreateHBARules(hba []string,
 	}
 
 	return hbaContent.String(), nil
+}
+
+// CreateIdentRules will create the content of pg_ident.conf file given
+// the rules set by the cluster spec
+func CreateIdentRules(ident []string, username string) (string, error) {
+	var identContent bytes.Buffer
+
+	templateData := struct {
+		Mappings []string
+		Username string
+	}{
+		Mappings: ident,
+		Username: username,
+	}
+
+	if err := identTemplate.Execute(&identContent, templateData); err != nil {
+		return "", err
+	}
+
+	return identContent.String(), nil
 }
 
 // PgConfiguration wraps configuration parameters with some checks

pkg/management/postgres/configuration.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"os/user"
 	"path"
 	"path/filepath"
 	"sort"
@@ -195,6 +196,42 @@ func quoteHbaLiteral(literal string) string {
 	literal = strings.ReplaceAll(literal, `"`, `""`)
 	literal = strings.ReplaceAll(literal, "\n", "\\\n")
 	return fmt.Sprintf(`"%s"`, literal)
+}
+
+// GeneratePostgresqlIdent generates the pg_ident.conf content
+func (instance *Instance) GeneratePostgresqlIdent(cluster *apiv1.Cluster) (string, error) {
+	var username string
+	currentUser, err := user.Current()
+	if err != nil {
+		log.Info("Unable to identify the current user. Falling back to insecure mapping.")
+		username = "/"
+	} else {
+		username = currentUser.Username
+	}
+
+	return postgres.CreateIdentRules(
+		cluster.Spec.PostgresConfiguration.PgIdent,
+		username)
+}
+
+// RefreshPGIdent generates and writes down the pg_ident.conf file
+func (instance *Instance) RefreshPGIdent(cluster *apiv1.Cluster) (postgresIdentChanged bool, err error) {
+	// Generate pg_hba.conf file
+	pgIdentContent, err := instance.GeneratePostgresqlIdent(cluster)
+	if err != nil {
+		return false, nil
+	}
+	postgresIdentChanged, err = InstallPgDataFileContent(
+		instance.PgData,
+		pgIdentContent,
+		constants.PostgresqlIdentFile)
+	if err != nil {
+		return postgresIdentChanged, fmt.Errorf(
+			"installing postgresql Ident rules: %w",
+			err)
+	}
+
+	return postgresIdentChanged, err
 }
 
 // UpdateReplicaConfiguration updates the override.conf or recovery.conf file for the proper version

api/v1/cluster_types.go
@@ -1160,6 +1160,11 @@ type PostgresConfiguration struct {
 	// +optional
 	PgHBA []string `json:"pg_hba,omitempty"`
 
+	// PostgreSQL User Name Maps rules (lines to be appended
+	// to the pg_ident.conf file)
+	// +optional
+	PgIdent []string `json:"pg_ident,omitempty"`
+
 	// Requirements to be met by sync replicas. This will affect how the "synchronous_standby_names" parameter will be
 	// set up.
 	// +optional

api/v1/zz_generated.deepcopy.go
@@ -1826,6 +1826,11 @@ func (in *PostgresConfiguration) DeepCopyInto(out *PostgresConfiguration) {
 		*out = make([]string, len(*in))
 		copy(*out, *in)
 	}
+	if in.PgIdent != nil {
+		in, out := &in.PgIdent, &out.PgIdent
+		*out = make([]string, len(*in))
+		copy(*out, *in)
+	}
 	in.SyncReplicaElectionConstraint.DeepCopyInto(&out.SyncReplicaElectionConstraint)
 	if in.AdditionalLibraries != nil {
 		in, out := &in.AdditionalLibraries, &out.AdditionalLibraries

config/crd/bases/postgresql.cnpg.io_clusters.yaml
@@ -2806,6 +2806,12 @@ spec:
                     items:
                       type: string
                     type: array
+                  pg_ident:
+                    description: PostgreSQL User Name Maps rules (lines to be appended
+                      to the pg_ident.conf file)
+                    items:
+                      type: string
+                    type: array
                   promotionTimeout:
                     description: Specifies the maximum number of seconds to wait when
                       promoting an instance to primary. Default value is 40000000,

internal/management/controller/instance_controller.go
@@ -290,6 +290,12 @@ func (r *InstanceReconciler) refreshConfigurationFiles(
 		return false, err
 	}
 
+	reloadIdent, err := r.instance.RefreshPGIdent(cluster)
+	if err != nil {
+		return false, err
+	}
+	reloadNeeded = reloadNeeded || reloadIdent
+
 	// Reconcile PostgreSQL configuration
 	// This doesn't need the PG connection, but it needs to reload it in case of changes
 	reloadConfig, err := r.instance.RefreshConfigurationFilesFromCluster(cluster, false)

pkg/management/postgres/restore.go
@@ -711,6 +711,10 @@ func (info InitInfo) WriteInitialPostgresqlConf(cluster *apiv1.Cluster) error {
 	if err != nil {
 		return fmt.Errorf("while reading configuration files from ConfigMap: %w", err)
 	}
+	_, err = temporaryInstance.RefreshPGIdent(cluster)
+	if err != nil {
+		return fmt.Errorf("while reading configuration files from ConfigMap: %w", err)
+	}
 	_, err = temporaryInstance.RefreshConfigurationFilesFromCluster(cluster, false)
 	if err != nil {
 		return fmt.Errorf("while reading configuration files from ConfigMap: %w", err)

pkg/postgres/configuration_test.go
@@ -248,6 +248,23 @@ var _ = Describe("pg_hba.conf generation", func() {
 	})
 })
 
+var _ = Describe("pg_ident.conf generation", func() {
+	specRules := []string{
+		"test someone else",
+	}
+
+	It("contains the default map when no mappings are added", func() {
+		Expect(CreateIdentRules(make([]string, 0), "someone")).To(
+			ContainSubstring("\nlocal someone postgres\n"))
+	})
+
+	It("contains the default map and additional mappings when added", func() {
+		rules, _ := CreateIdentRules(specRules, "someone")
+		Expect(rules).To(ContainSubstring("\nlocal someone postgres\n"))
+		Expect(rules).To(ContainSubstring("\ntest someone else\n"))
+	})
+})
+
 var _ = Describe("pgaudit", func() {
 	var pgaudit *ManagedExtension
 	BeforeEach(func() {

commit 319ce11e3a07532584961eff47a85b6f8ac68c85

fix: properly quote comment lines (#3690)
This patch resolves a bug in the code execution caused by incorrect
quoting introduced with PR #3534.

Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

pkg/postgres/configuration.go
@@ -33,7 +33,7 @@ const (
 # FIXED RULES
 #
 
-# Grant local access (`local` user map)
+# Grant local access ('local' user map)
 local all all peer map=local
 
 # Require client certificate authentication for the streaming_replica user
@@ -69,7 +69,7 @@ host all all all {{.DefaultAuthenticationMethod}}
 # FIXED RULES
 #
 
-# Grant local access (`local` user map)
+# Grant local access ('local' user map)
 local {{.Username}} postgres
 
 #