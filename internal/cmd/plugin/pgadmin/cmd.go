3ee38b89e691688e232b1f96386f944ac1c7adf3
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2024-01-23 14:19:37 +0100

    feat: add `pgadmin4` command to `cnpg` plugin (#3701)
    Introduce the `pgadmin4` command in the `cnpg` plugin for `kubectl`.
    This provides a simple way to connect to a specific database `Cluster`
    and browse its content in a local environment, such as `kind`,
    for demonstration and evaluation purposes.
    
    This patch implements two modes for deploying pgAdmin4:
    
    - `server` mode: authentication is required using a randomly generated
       password, and users must manually specify the database to connect to
    - `desktop` mode: initiates a pgAdmin web interface without requiring
    authentication; it automatically connects to the `app` database as the
      `app` user, making it ideal for quick demos
    
    Closes: #3696
    
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    Signed-off-by: Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    Co-authored-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    Co-authored-by: Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>
    Co-authored-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    (cherry picked from commit 02e420d7af0004f3a23bd3cad329135df9fba0f0)

---
+++ internal/cmd/plugin/pgadmin/cmd.go
@@ -0,0 +1,130 @@
+/*
+Copyright The CloudNativePG Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package pgadmin
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"text/template"
+
+	"github.com/spf13/cobra"
+	"k8s.io/utils/strings/slices"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	apiv1 "github.com/cloudnative-pg/cloudnative-pg/api/v1"
+	"github.com/cloudnative-pg/cloudnative-pg/internal/cmd/plugin"
+)
+
+const pgadminExample = `
+  # Dry-run command with default values"
+  kubectl-cnpg pgadmin <cluster-name> --dry-run
+
+  # Create a pgadmin job with default values.
+  kubectl-cnpg pgadmin <cluster-name>
+
+  # Dry-run command with given values and clusterName "cluster-example"
+  kubectl-cnpg pgadmin cluster-example -n <namespace> --dry-run
+`
+
+var usageExampleTemplate = template.Must(template.New("pgadmin-example").Parse(`
+{{ if eq .Mode "server" }}
+To access this pgAdmin instance, use the following credentials:
+
+username: {{ .PgadminUsername }}
+password: {{ .PgadminPassword }}
+
+
+To establish a connection to the database server, you'll need the password for
+the '{{ .ApplicationDatabaseOwnerName }}' user. Retrieve it with the following
+command:
+
+kubectl get secret {{ .ApplicationDatabaseSecretName }} -o 'jsonpath={.data.password}' | base64 -d; echo ""
+{{ end }}
+
+Easily reach the new pgAdmin4 instance by forwarding your local 8080 port using:
+
+kubectl rollout status deployment {{ .DeploymentName }}
+kubectl port-forward deployment/{{ .DeploymentName }} 8080:80
+
+Then, navigate to http://localhost:8080 in your browser.
+
+To remove this pgAdmin deployment, execute:
+
+kubectl cnpg pgadmin4 {{ .ClusterName }} --dry-run | kubectl delete -f -
+`))
+
+// NewCmd initializes the pgadmin command
+func NewCmd() *cobra.Command {
+	var dryRun bool
+	var mode string
+
+	pgadminCmd := &cobra.Command{
+		Use:     "pgadmin4 [name]",
+		Short:   "Creates a pgadmin deployment.",
+		Args:    cobra.MinimumNArgs(1),
+		Long:    `Creates a pgadmin deployment configured to work with a CNPG Cluster.`,
+		Example: pgadminExample,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := context.Background()
+			clusterName := args[0]
+
+			if !slices.Contains([]string{string(ModeDesktop), string(ModeServer)}, mode) {
+				return fmt.Errorf("unknown mode: %s", mode)
+			}
+
+			// Get the Cluster object
+			var cluster apiv1.Cluster
+			if err := plugin.Client.Get(
+				ctx,
+				client.ObjectKey{Namespace: plugin.Namespace, Name: clusterName},
+				&cluster); err != nil {
+				return fmt.Errorf("could not get cluster: %v", err)
+			}
+
+			pgAdminCmd, err := newCommand(&cluster, Mode(mode), dryRun)
+			if err != nil {
+				return err
+			}
+
+			if err := pgAdminCmd.execute(ctx); err != nil {
+				return err
+			}
+
+			if !pgAdminCmd.dryRun {
+				_ = usageExampleTemplate.Execute(os.Stdout, pgAdminCmd)
+			}
+
+			return nil
+		},
+	}
+	pgadminCmd.Flags().BoolVar(
+		&dryRun,
+		"dry-run",
+		false,
+		"When true prints the deployment manifest instead of creating it",
+	)
+
+	pgadminCmd.Flags().StringVar(
+		&mode,
+		"mode",
+		"server",
+		"Chooses between 'server' and 'desktop' (insecure) mode.",
+	)
+
+	return pgadminCmd
+}
---
+++ internal/cmd/plugin/pgadmin/pgadmin_test.go
@@ -0,0 +1,156 @@
+/*
+Copyright The CloudNativePG Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package pgadmin
+
+import (
+	"bytes"
+	"path"
+	"text/template"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+
+	"github.com/cloudnative-pg/cloudnative-pg/internal/cmd/plugin"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("command methods", func() {
+	var cmd *command
+
+	BeforeEach(func() {
+		cmd = &command{
+			ClusterName:                   "example-cluster",
+			ApplicationDatabaseSecretName: "example-secret",
+			ApplicationDatabaseOwnerName:  "example-owner",
+			DeploymentName:                "example-deployment",
+			ConfigMapName:                 "example-configmap",
+			ServiceName:                   "example-service",
+			SecretName:                    "example-secret-name",
+			PgadminUsername:               "example-username",
+			PgadminPassword:               "example-password",
+			Mode:                          ModeServer, // or ModeDesktop for the desktop mode
+		}
+	})
+
+	It("should generate the Deployment object", func() {
+		deployment := cmd.generateDeployment()
+
+		Expect(deployment).ToNot(BeNil())
+		Expect(deployment.APIVersion).To(Equal("apps/v1"))
+		Expect(deployment.Kind).To(Equal("Deployment"))
+		Expect(deployment.ObjectMeta.Name).To(Equal("example-deployment"))
+		Expect(deployment.ObjectMeta.Namespace).To(Equal(plugin.Namespace))
+		Expect(*deployment.Spec.Replicas).To(Equal(int32(1)))
+		Expect(deployment.Spec.Selector.MatchLabels).To(HaveKeyWithValue("app", "example-deployment"))
+
+		podTemplate := deployment.Spec.Template
+		Expect(podTemplate.ObjectMeta.Labels).To(HaveKeyWithValue("app", "example-deployment"))
+		Expect(podTemplate.Spec.Containers).To(HaveLen(1))
+
+		container := podTemplate.Spec.Containers[0]
+		Expect(container.Image).To(Equal("dpage/pgadmin4:latest"))
+		Expect(container.Name).To(Equal("pgadmin4"))
+		Expect(container.Ports).To(HaveLen(1))
+		Expect(container.Ports[0].Name).To(Equal("http"))
+		Expect(container.Ports[0].ContainerPort).To(Equal(int32(80)))
+
+		envVars := container.Env
+		Expect(envVars).To(ContainElement(corev1.EnvVar{
+			Name:  "PGADMIN_SERVER_JSON_FILE",
+			Value: path.Join("/config", "servers.json"),
+		}))
+		Expect(envVars).To(ContainElement(corev1.EnvVar{Name: "PGADMIN_CONFIG_SERVER_MODE", Value: "True"}))
+		Expect(envVars).To(ContainElement(corev1.EnvVar{Name: "PGADMIN_CONFIG_MASTER_PASSWORD_REQUIRED", Value: "False"}))
+
+		volumeMounts := container.VolumeMounts
+		Expect(volumeMounts).To(ContainElement(corev1.VolumeMount{Name: "pgadmin-cfg", MountPath: "/config"}))
+		Expect(volumeMounts).To(ContainElement(corev1.VolumeMount{Name: "app-secret", MountPath: "/secret"}))
+
+		readinessProbe := container.ReadinessProbe
+		Expect(readinessProbe).ToNot(BeNil())
+		Expect(readinessProbe.HTTPGet).ToNot(BeNil())
+		Expect(readinessProbe.HTTPGet.Path).To(Equal("/"))
+		Expect(readinessProbe.HTTPGet.Port).To(Equal(intstr.FromInt32(80)))
+	})
+
+	It("should generate the Service object", func() {
+		service := cmd.generateService()
+
+		Expect(service).ToNot(BeNil())
+		Expect(service.APIVersion).To(Equal("v1"))
+		Expect(service.Kind).To(Equal("Service"))
+		Expect(service.ObjectMeta.Name).To(Equal("example-service"))
+		Expect(service.ObjectMeta.Namespace).To(Equal(plugin.Namespace))
+
+		serviceSpec := service.Spec
+		Expect(serviceSpec.Ports).To(HaveLen(1))
+		Expect(serviceSpec.Ports[0].Port).To(Equal(int32(80)))
+		Expect(serviceSpec.Ports[0].Protocol).To(Equal(corev1.ProtocolTCP))
+		Expect(serviceSpec.Ports[0].TargetPort).To(Equal(intstr.FromInt32(80)))
+		Expect(serviceSpec.Selector).To(HaveKeyWithValue("app", "example-deployment"))
+	})
+
+	It("should generate the configuration template", func() {
+		data := map[string]string{
+			"ClusterName":                  "example-cluster",
+			"ApplicationDatabaseOwnerName": "example-owner",
+			"Mode":                         "desktop",
+		}
+
+		renderedTemplate := renderTemplate(configurationTemplate, data)
+
+		Expect(renderedTemplate).To(ContainSubstring(`"Username": "example-owner"`))
+		Expect(renderedTemplate).To(ContainSubstring(`"PasswordExecCommand": "cat /secret/password"`))
+	})
+
+	It("should generate the ConfigMap object", func() {
+		configMap, err := cmd.generateConfigMap()
+
+		Expect(err).ToNot(HaveOccurred())
+
+		Expect(configMap).ToNot(BeNil())
+		Expect(configMap.APIVersion).To(Equal("v1"))
+		Expect(configMap.Kind).To(Equal("ConfigMap"))
+		Expect(configMap.ObjectMeta.Name).To(Equal("example-configmap"))
+		Expect(configMap.ObjectMeta.Namespace).To(Equal(plugin.Namespace))
+
+		Expect(configMap.Data).To(HaveKey("servers.json"))
+		Expect(configMap.Data["servers.json"]).To(ContainSubstring(`"Username": "example-owner"`))
+	})
+
+	It("should generate the Secret object", func() {
+		secret := cmd.generateSecret()
+
+		Expect(secret).ToNot(BeNil())
+		Expect(secret.APIVersion).To(Equal("v1"))
+		Expect(secret.Kind).To(Equal("Secret"))
+		Expect(secret.ObjectMeta.Name).To(Equal("example-secret-name"))
+		Expect(secret.ObjectMeta.Namespace).To(Equal(plugin.Namespace))
+
+		Expect(secret.StringData).To(HaveKeyWithValue("username", "example-username"))
+		Expect(secret.StringData).To(HaveKeyWithValue("password", "example-password"))
+	})
+})
+
+func renderTemplate(t *template.Template, data map[string]string) string {
+	var renderedTemplate bytes.Buffer
+	err := t.Execute(&renderedTemplate, data)
+	Expect(err).ToNot(HaveOccurred())
+	return renderedTemplate.String()
+}
