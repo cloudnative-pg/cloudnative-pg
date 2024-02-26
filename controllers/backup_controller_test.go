62c1f2ee75a22279cdc93991eb289dbcd1303a6a
Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>
2024-02-26 11:45:16 +0100

    feat: switch external-snapshotter from v6 to v7
    Signed-off-by: Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>

--- controllers/backup_controller_test.go
+++ controllers/backup_controller_test.go
@@ -20,7 +20,7 @@ import (
 	"context"
 	"time"
 
-	volumesnapshot "github.com/kubernetes-csi/external-snapshotter/client/v6/apis/volumesnapshot/v1"
+	volumesnapshot "github.com/kubernetes-csi/external-snapshotter/client/v7/apis/volumesnapshot/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
--- internal/cmd/plugin/plugin.go
+++ internal/cmd/plugin/plugin.go
@@ -23,7 +23,7 @@ import (
 	"os"
 	"time"
 
-	storagesnapshotv1 "github.com/kubernetes-csi/external-snapshotter/client/v6/apis/volumesnapshot/v1"
+	storagesnapshotv1 "github.com/kubernetes-csi/external-snapshotter/client/v7/apis/volumesnapshot/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
