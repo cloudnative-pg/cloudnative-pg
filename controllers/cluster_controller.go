4f4e4a9e34aa81c6303386eb5ed2df6ece81e964
Armando Ruocco <armando.ruocco@enterprisedb.com>
2024-04-08 15:43:29 +0200

    fix: do not execute updateTargetPrimary logic if no instance HTTP status is present
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- controllers/cluster_controller.go
+++ controllers/cluster_controller.go
@@ -416,7 +416,7 @@ func (r *ClusterReconciler) handleSwitchover(
 
 	// Update the target primary name from the Pods status.
 	// This means issuing a failover or switchover when needed.
-	selectedPrimary, err := r.updateTargetPrimaryFromPods(ctx, cluster, instancesStatus, resources)
+	selectedPrimary, err := r.reconcileTargetPrimaryFromPods(ctx, cluster, instancesStatus, resources)
 	if err != nil {
 		if errors.Is(err, ErrWaitingOnFailOverDelay) {
 			contextLogger.Info("Waiting for the failover delay to expire")
