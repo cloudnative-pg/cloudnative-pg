cloudnative-pg/cloudnative-pg

commit 15fe7c8e5f631949318af7ebcf48f4c5c6093ef4

feat: allow users to have tab-completion for the CNPG plugin (#3875)
The kubectl plugin supports smart tab completion for cluster names.

This patch is the first step to support the full feature, which involves
pod names and instance numbers, too.

Closes #3874

Signed-off-by: Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>
Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
Co-authored-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
Co-authored-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>

internal/cmd/plugin/maintenance/cmd.go
@@ -20,6 +20,8 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+
+	"github.com/cloudnative-pg/cloudnative-pg/internal/cmd/plugin"
 )
 
 // NewCmd creates the new 'maintenance' command
@@ -38,7 +40,10 @@ func NewCmd() *cobra.Command {
 		Short: "Sets maintenance mode",
 		Long: "This command will set maintenance mode on a single cluster or on all clusters " +
 			"in the current namespace if not specified differently through flags",
-		Args: cobra.MaximumNArgs(1),
+		Args: plugin.RequiresArguments(1),
+		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+			return plugin.CompleteClusters(cmd.Context(), args, toComplete), cobra.ShellCompDirectiveNoFileComp
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			var clusterName string
 			if len(args) > 0 {
@@ -56,7 +61,10 @@ func NewCmd() *cobra.Command {
 		Short: "Removes maintenance mode",
 		Long: "This command will unset maintenance mode on a single cluster or on all clusters " +
 			"in the current namespace if not specified differently through flags",
-		Args: cobra.MaximumNArgs(1),
+		Args: plugin.RequiresArguments(1),
+		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+			return plugin.CompleteClusters(cmd.Context(), args, toComplete), cobra.ShellCompDirectiveNoFileComp
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			var clusterName string
 			if len(args) > 0 {

internal/cmd/plugin/backup/cmd.go
@@ -63,7 +63,10 @@ func NewCmd() *cobra.Command {
 	backupSubcommand := &cobra.Command{
 		Use:   "backup [cluster]",
 		Short: "Request an on-demand backup for a PostgreSQL Cluster",
-		Args:  cobra.ExactArgs(1),
+		Args:  plugin.RequiresArguments(1),
+		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+			return plugin.CompleteClusters(cmd.Context(), args, toComplete), cobra.ShellCompDirectiveNoFileComp
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clusterName := args[0]
 

internal/cmd/plugin/certificate/cmd.go
@@ -32,7 +32,7 @@ func NewCmd() *cobra.Command {
 		Long: `This command creates a new Kubernetes secret containing the crypto-material.
 This is needed to configure TLS with Certificate authentication access for an application to
 connect to the PostgreSQL cluster.`,
-		Args: cobra.ExactArgs(1),
+		Args: plugin.RequiresArguments(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ctx := context.Background()
 			secretName := args[0]

internal/cmd/plugin/fio/cmd.go
@@ -31,7 +31,7 @@ func NewCmd() *cobra.Command {
 
 	fioCmd := &cobra.Command{
 		Use:     "fio [name]",
-		Short:   "Creates a fio deployment,pvc and configmap.",
+		Short:   "Creates a fio deployment, pvc and configmap",
 		Args:    cobra.MinimumNArgs(1),
 		Long:    `Creates a fio deployment that will execute a fio job on the specified pvc.`,
 		Example: jobExample,

internal/cmd/plugin/hibernate/cmd.go
@@ -28,7 +28,10 @@ var (
 	hibernateOnCmd = &cobra.Command{
 		Use:   "on [cluster]",
 		Short: "Hibernates the cluster named [cluster]",
-		Args:  cobra.ExactArgs(1),
+		Args:  plugin.RequiresArguments(1),
+		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+			return plugin.CompleteClusters(cmd.Context(), args, toComplete), cobra.ShellCompDirectiveNoFileComp
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clusterName := args[0]
 			force, err := cmd.Flags().GetBool("force")
@@ -48,7 +51,10 @@ var (
 	hibernateOffCmd = &cobra.Command{
 		Use:   "off [cluster]",
 		Short: "Bring the cluster named [cluster] back from hibernation",
-		Args:  cobra.ExactArgs(1),
+		Args:  plugin.RequiresArguments(1),
+		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+			return plugin.CompleteClusters(cmd.Context(), args, toComplete), cobra.ShellCompDirectiveNoFileComp
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clusterName := args[0]
 			off := newOffCommand(cmd.Context(), clusterName)
@@ -59,7 +65,10 @@ var (
 	hibernateStatusCmd = &cobra.Command{
 		Use:   "status [cluster]",
 		Short: "Prints the hibernation status for the [cluster]",
-		Args:  cobra.ExactArgs(1),
+		Args:  plugin.RequiresArguments(1),
+		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+			return plugin.CompleteClusters(cmd.Context(), args, toComplete), cobra.ShellCompDirectiveNoFileComp
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clusterName := args[0]
 			rawOutput, err := cmd.Flags().GetString("output")

internal/cmd/plugin/pgadmin/cmd.go
@@ -75,7 +75,7 @@ func NewCmd() *cobra.Command {
 
 	pgadminCmd := &cobra.Command{
 		Use:     "pgadmin4 [name]",
-		Short:   "Creates a pgadmin deployment.",
+		Short:   "Creates a pgadmin deployment",
 		Args:    cobra.MinimumNArgs(1),
 		Long:    `Creates a pgadmin deployment configured to work with a CNPG Cluster.`,
 		Example: pgadminExample,

internal/cmd/plugin/psql/cmd.go
@@ -34,7 +34,10 @@ func NewCmd() *cobra.Command {
 		Use:   "psql [cluster] [-- psqlArgs...]",
 		Short: "Start a psql session targeting a CloudNativePG cluster",
 		Args:  validatePsqlArgs,
-		Long:  "This command will start an interactive psql session inside a PostgreSQL Pod created by CloudNativePG.",
+		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+			return plugin.CompleteClusters(cmd.Context(), args, toComplete), cobra.ShellCompDirectiveNoFileComp
+		},
+		Long: "This command will start an interactive psql session inside a PostgreSQL Pod created by CloudNativePG.",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clusterName := args[0]
 			psqlArgs := args[1:]

internal/cmd/plugin/reload/cmd.go
@@ -20,6 +20,8 @@ import (
 	"context"
 
 	"github.com/spf13/cobra"
+
+	"github.com/cloudnative-pg/cloudnative-pg/internal/cmd/plugin"
 )
 
 // NewCmd creates the new "reset" command
@@ -28,7 +30,10 @@ func NewCmd() *cobra.Command {
 		Use:   "reload [clusterName]",
 		Short: `Reload the cluster`,
 		Long:  `Triggers a reconciliation loop for all the cluster's instances, rolling out new configurations if present.`,
-		Args:  cobra.ExactArgs(1),
+		Args:  plugin.RequiresArguments(1),
+		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+			return plugin.CompleteClusters(cmd.Context(), args, toComplete), cobra.ShellCompDirectiveNoFileComp
+		},
 		RunE: func(_ *cobra.Command, args []string) error {
 			ctx := context.Background()
 			clusterName := args[0]

commit 439d67d234856ea5685ba0eb5ca513406dd50f9b

fix: regression related to the arguments
When we added this regression with PR #3875 when we replaced MaxiumNArgs()
function with RequiresArguments() which will actually enforce the number of
arguments, but in this case, the maintenance command will work even without
arguments just the `-A` flag.

Closes #4263

Signed-off-by: Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>

internal/cmd/plugin/maintenance/cmd.go
@@ -40,7 +40,7 @@ func NewCmd() *cobra.Command {
 		Short: "Sets maintenance mode",
 		Long: "This command will set maintenance mode on a single cluster or on all clusters " +
 			"in the current namespace if not specified differently through flags",
-		Args: plugin.RequiresArguments(1),
+		Args: cobra.MaximumNArgs(1),
 		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 			return plugin.CompleteClusters(cmd.Context(), args, toComplete), cobra.ShellCompDirectiveNoFileComp
 		},
@@ -61,7 +61,7 @@ func NewCmd() *cobra.Command {
 		Short: "Removes maintenance mode",
 		Long: "This command will unset maintenance mode on a single cluster or on all clusters " +
 			"in the current namespace if not specified differently through flags",
-		Args: plugin.RequiresArguments(1),
+		Args: cobra.MaximumNArgs(1),
 		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 			return plugin.CompleteClusters(cmd.Context(), args, toComplete), cobra.ShellCompDirectiveNoFileComp
 		},