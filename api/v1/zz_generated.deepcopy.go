e46ad0cd9d3eb4f7373e7dc82ec32a056f17fbdb
Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>
2024-05-18 18:16:21 +0200

    fix: update to make compatible with the new version
    Signed-off-by: Jonathan Gonzalez V <jonathan.gonzalez@enterprisedb.com>

--- api/v1/zz_generated.deepcopy.go
+++ api/v1/zz_generated.deepcopy.go
@@ -1383,24 +1383,16 @@ func (in *MonitoringConfiguration) DeepCopyInto(out *MonitoringConfiguration) {
 	}
 	if in.PodMonitorMetricRelabelConfigs != nil {
 		in, out := &in.PodMonitorMetricRelabelConfigs, &out.PodMonitorMetricRelabelConfigs
-		*out = make([]*monitoringv1.RelabelConfig, len(*in))
+		*out = make([]monitoringv1.RelabelConfig, len(*in))
 		for i := range *in {
-			if (*in)[i] != nil {
-				in, out := &(*in)[i], &(*out)[i]
-				*out = new(monitoringv1.RelabelConfig)
-				(*in).DeepCopyInto(*out)
-			}
+			(*in)[i].DeepCopyInto(&(*out)[i])
 		}
 	}
 	if in.PodMonitorRelabelConfigs != nil {
 		in, out := &in.PodMonitorRelabelConfigs, &out.PodMonitorRelabelConfigs
-		*out = make([]*monitoringv1.RelabelConfig, len(*in))
+		*out = make([]monitoringv1.RelabelConfig, len(*in))
 		for i := range *in {
-			if (*in)[i] != nil {
-				in, out := &(*in)[i], &(*out)[i]
-				*out = new(monitoringv1.RelabelConfig)
-				(*in).DeepCopyInto(*out)
-			}
+			(*in)[i].DeepCopyInto(&(*out)[i])
 		}
 	}
 }
@@ -1666,24 +1658,16 @@ func (in *PoolerMonitoringConfiguration) DeepCopyInto(out *PoolerMonitoringConfi
 	*out = *in
 	if in.PodMonitorMetricRelabelConfigs != nil {
 		in, out := &in.PodMonitorMetricRelabelConfigs, &out.PodMonitorMetricRelabelConfigs
-		*out = make([]*monitoringv1.RelabelConfig, len(*in))
+		*out = make([]monitoringv1.RelabelConfig, len(*in))
 		for i := range *in {
-			if (*in)[i] != nil {
-				in, out := &(*in)[i], &(*out)[i]
-				*out = new(monitoringv1.RelabelConfig)
-				(*in).DeepCopyInto(*out)
-			}
+			(*in)[i].DeepCopyInto(&(*out)[i])
 		}
 	}
 	if in.PodMonitorRelabelConfigs != nil {
 		in, out := &in.PodMonitorRelabelConfigs, &out.PodMonitorRelabelConfigs
-		*out = make([]*monitoringv1.RelabelConfig, len(*in))
+		*out = make([]monitoringv1.RelabelConfig, len(*in))
 		for i := range *in {
-			if (*in)[i] != nil {
-				in, out := &(*in)[i], &(*out)[i]
-				*out = new(monitoringv1.RelabelConfig)
-				(*in).DeepCopyInto(*out)
-			}
+			(*in)[i].DeepCopyInto(&(*out)[i])
 		}
 	}
 }
