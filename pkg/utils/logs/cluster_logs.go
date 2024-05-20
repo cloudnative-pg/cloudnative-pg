845a4f7f346d4e48fc4ab70a7564d00c84391cb0
Niccolò Fei <niccolo.fei@enterprisedb.com>
2024-05-20 17:02:12 +0200

    chore: fix import order
    Signed-off-by: Niccolò Fei <niccolo.fei@enterprisedb.com>

--- pkg/utils/logs/cluster_logs.go
+++ pkg/utils/logs/cluster_logs.go
@@ -19,12 +19,12 @@ package logs
 import (
 	"context"
 	"io"
-	apierrs "k8s.io/apimachinery/pkg/api/errors"
 	"log"
 	"sync"
 	"time"
 
 	v1 "k8s.io/api/core/v1"
+	apierrs "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	ctrl "sigs.k8s.io/controller-runtime"
