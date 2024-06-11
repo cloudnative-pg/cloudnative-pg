1c9330b2c60998dfececc5e82bf4c55dc685a5ab
huyantian <31943844+YanniHu1996@users.noreply.github.com>
2024-06-12 05:32:02 +0800

    feat(plugin): support color control via  flags and TTY detection (#4775)
    Introduced support for ANSI colors in the `cnpg` plugin through the
    `--color` option, which accepts `always`, `never`, and `auto` (default)
    as values. When `auto` is selected, the plugin automatically detects if
    the standard output channel is connected to a terminal and adds ANSI
    colors to the command output.
    
    Closes: #4722
    
    Signed-off-by: YanniHu1996 <yantian.hu@enterprisedb.com>
    Signed-off-by: Jaime Silvela <jaime.silvela@enterprisedb.com>
    Signed-off-by: Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>
    Signed-off-by: Tao Li <tao.li@enterprisedb.com>
    Co-authored-by: Jaime Silvela <jaime.silvela@enterprisedb.com>
    Co-authored-by: Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>
    Co-authored-by: Tao Li <tao.li@enterprisedb.com>
    (cherry picked from commit 696a11d703d245d0abbe881a13630fb47b6f0c97)

--- go.mod
+++ go.mod
@@ -35,6 +35,7 @@ require (
 	go.uber.org/multierr v1.11.0
 	go.uber.org/zap v1.27.0
 	golang.org/x/sys v0.20.0
+	golang.org/x/term v0.20.0
 	google.golang.org/grpc v1.64.0
 	gopkg.in/yaml.v3 v3.0.1
 	k8s.io/api v0.30.1
@@ -100,7 +101,6 @@ require (
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/oauth2 v0.18.0 // indirect
 	golang.org/x/sync v0.7.0 // indirect
-	golang.org/x/term v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	golang.org/x/tools v0.21.0 // indirect
---
+++ internal/cmd/plugin/color.go
@@ -0,0 +1,101 @@
+/*
+Copyright The CloudNativePG Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package plugin
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/logrusorgru/aurora/v4"
+	"github.com/spf13/cobra"
+	"golang.org/x/term"
+)
+
+// colorConfiguration represents how the output should be colorized.
+// It is a `pflag.Value`, therefore implements String(), Set(), Type()
+type colorConfiguration string
+
+const (
+	// colorAlways configures the output to always be colorized
+	colorAlways colorConfiguration = "always"
+	// colorAuto configures the the output to be colorized only when attached to a terminal
+	colorAuto colorConfiguration = "auto"
+	// colorNever configures the the output never to be colorized
+	colorNever colorConfiguration = "never"
+)
+
+// String returns the string representation
+func (e colorConfiguration) String() string {
+	return string(e)
+}
+
+// Set sets the color configuration
+func (e *colorConfiguration) Set(val string) error {
+	colorVal := colorConfiguration(val)
+	switch colorVal {
+	case colorAlways, colorAuto, colorNever:
+		*e = colorVal
+		return nil
+	default:
+		return fmt.Errorf("should be one of 'always', 'auto', or 'never'")
+	}
+}
+
+// Type returns the data type of the flag used for the color configuration
+func (e *colorConfiguration) Type() string {
+	return "string"
+}
+
+// ConfigureColor renews aurora.DefaultColorizer based on flags and TTY
+func ConfigureColor(cmd *cobra.Command) {
+	configureColor(cmd, term.IsTerminal(int(os.Stdout.Fd())))
+}
+
+func configureColor(cmd *cobra.Command, isTTY bool) {
+	colorFlag := cmd.Flag("color")
+	colorConfig := colorAuto // default config
+	if colorFlag != nil {
+		colorConfig = colorConfiguration(colorFlag.Value.String())
+	}
+
+	var shouldColorize bool
+	switch colorConfig {
+	case colorAlways:
+		shouldColorize = true
+	case colorNever:
+		shouldColorize = false
+	case colorAuto:
+		shouldColorize = isTTY
+	}
+
+	aurora.DefaultColorizer = aurora.New(
+		aurora.WithColors(shouldColorize),
+		aurora.WithHyperlinks(true),
+	)
+}
+
+// AddColorControlFlag adds color control flags to the command
+func AddColorControlFlag(cmd *cobra.Command) {
+	// By default, color is set to 'auto'
+	colorValue := colorAuto
+	cmd.Flags().Var(&colorValue, "color", "Control color output; options include 'always', 'auto', or 'never'")
+	_ = cmd.RegisterFlagCompletionFunc("color",
+		func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
+			return []string{colorAlways.String(), colorAuto.String(), colorNever.String()},
+				cobra.ShellCompDirectiveDefault | cobra.ShellCompDirectiveKeepOrder
+		})
+}
