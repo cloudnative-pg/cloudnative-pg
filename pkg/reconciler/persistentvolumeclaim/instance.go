c401383408e902ab1b917166c555fc5a61639239
Marco Nenciarini <marco.nenciarini@enterprisedb.com>
2024-06-11 18:30:56 +0200

    chore(deps): remove `golang.org/x/exp` dependency (#4817)
    It was only used for the `constraints` and `slices` packages. The
    `slices` package is now part of the standard library[1], and the two
    definitions we use from constraints are trivial, concise, and static.
    
    [1]: https://go.dev/doc/go1.21#slices
    
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    Co-authored-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    (cherry picked from commit ee82d6399e407b09f49975b688692e26ba4e3a23)

--- pkg/reconciler/persistentvolumeclaim/instance.go
+++ pkg/reconciler/persistentvolumeclaim/instance.go
@@ -18,9 +18,9 @@ package persistentvolumeclaim
 
 import (
 	"context"
+	"slices"
 	"time"
 
-	"golang.org/x/exp/slices"
 	corev1 "k8s.io/api/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
