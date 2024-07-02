d9dc96a4431faf254feb8bf1a2988034f95cdd04
Armando Ruocco <armando.ruocco@enterprisedb.com>
2024-07-02 17:15:09 +0200

    chore: remove `cluster_plugin` file
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- api/v1/cluster_types.go
+++ api/v1/cluster_types.go
@@ -523,6 +523,16 @@ type ClusterSpec struct {
 // configuration parameters
 type PluginConfigurationList []PluginConfiguration
 
+// GetNames gets the name of the plugins that are involved
+// in the reconciliation of this cluster
+func (pluginList PluginConfigurationList) GetNames() (result []string) {
+	pluginNames := make([]string, len(pluginList))
+	for i, pluginDeclaration := range pluginList {
+		pluginNames[i] = pluginDeclaration.Name
+	}
+	return pluginNames
+}
+
 const (
 	// PhaseSwitchover when a cluster is changing the primary node
 	PhaseSwitchover = "Switchover in progress"
@@ -2783,6 +2793,11 @@ func (secretResourceVersion *SecretsResourceVersion) SetExternalClusterSecretVer
 	}
 
 	secretResourceVersion.ExternalClusterSecretVersions[secretName] = *version
+}
+
+// SetInContext records the cluster in the given context
+func (cluster *Cluster) SetInContext(ctx context.Context) context.Context {
+	return context.WithValue(ctx, utils.ContextKeyCluster, cluster)
 }
 
 // GetImageName get the name of the image that should be used
