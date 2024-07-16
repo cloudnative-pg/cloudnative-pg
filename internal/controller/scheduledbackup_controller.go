94dfcc40b06183d0e9a34a0ba8cb33bf0f43bad9
Gabriele Fedi <91485518+GabriFedi97@users.noreply.github.com>
2024-07-16 14:45:10 +0200

    fix: retry immediate backup on failure (#4982)
    This patch makes the operator retry an immediate backup when
    the previous reconciliation attempt failed.
    
    Closes #4981
    
    Signed-off-by: Gabriele Fedi <gabriele.fedi@enterprisedb.com>
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    Co-authored-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    Co-authored-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    (cherry picked from commit 694e11da7457542e6a0f8d5ca0765e02d2ccd529)

--- internal/controller/scheduledbackup_controller.go
+++ internal/controller/scheduledbackup_controller.go
@@ -172,9 +172,15 @@ func ReconcileScheduledBackup(
 	}
 
 	now := time.Now()
-	origScheduled := scheduledBackup.DeepCopy()
+
+	if scheduledBackup.Status.LastCheckTime == nil && scheduledBackup.IsImmediate() {
+		// we populate the status (lastCheckTime...) by following the same rules of the scheduled backup
+		event.Eventf(scheduledBackup, "Normal", "BackupSchedule", "Scheduled immediate backup now: %v", now)
+		return createBackup(ctx, event, cli, scheduledBackup, now, now, schedule, true)
+	}
 
 	if scheduledBackup.Status.LastCheckTime == nil {
+		origScheduled := scheduledBackup.DeepCopy()
 		// This is the first time we check this schedule,
 		// let's wait until the first job will be actually
 		// scheduled
@@ -184,11 +190,6 @@ func ReconcileScheduledBackup(
 		err := cli.Status().Patch(ctx, scheduledBackup, client.MergeFrom(origScheduled))
 		if err != nil {
 			return ctrl.Result{}, err
-		}
-
-		if scheduledBackup.IsImmediate() {
-			event.Eventf(scheduledBackup, "Normal", "BackupSchedule", "Scheduled immediate backup now: %v", now)
-			return createBackup(ctx, event, cli, scheduledBackup, now, now, schedule, true)
 		}
 
 		nextTime := schedule.Next(now)
