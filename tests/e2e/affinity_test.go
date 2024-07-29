fa12c3c33336a5d8ed64f463ba877a47802388f0
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2024-07-29 10:21:24 +0200

    fix: PostgreSQL instances anti-affinity configuration (#5156)
    This patch fixes the PodAntiAffinity configuration for PostgreSQL
    Pods, allowing PostgreSQL and PgBouncer to coexist on the same
    node when the anti-affinity configuration is set to `required`.
    
    Closes: #4293
    
    Signed-off-by: Nicolò Ciraci <ciraci.nicolo@gmail.com>
    Signed-off-by: ceriath <gh@ceriath.net>
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
    Co-authored-by: Nicolò Ciraci <ciraci.nicolo@gmail.com>
    Co-authored-by: ceriath <gh@ceriath.net>

---
+++ tests/e2e/affinity_test.go
@@ -0,0 +1,63 @@
+/*
+Copyright The CloudNativePG Contributors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	"fmt"
+
+	"github.com/cloudnative-pg/cloudnative-pg/tests"
+	"github.com/cloudnative-pg/cloudnative-pg/tests/utils"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("E2E Affinity", Serial, Label(tests.LabelPodScheduling), func() {
+	const (
+		clusterFile     = fixturesDir + "/affinity/cluster-affinity.yaml"
+		poolerFile      = fixturesDir + "/affinity/pooler-affinity.yaml"
+		clusterName     = "cluster-affinity"
+		namespacePrefix = "test-affinity"
+		level           = tests.Medium
+	)
+	var namespace string
+	var err error
+
+	BeforeEach(func() {
+		if testLevelEnv.Depth < int(level) {
+			Skip("Test depth is lower than the amount requested for this test")
+		}
+	})
+
+	It("can create a cluster and a pooler with required affinity", func() {
+		namespace, err = env.CreateUniqueNamespace(namespacePrefix)
+		Expect(err).ToNot(HaveOccurred())
+		DeferCleanup(func() error {
+			if CurrentSpecReport().Failed() {
+				env.DumpNamespaceObjects(namespace, "out/"+CurrentSpecReport().LeafNodeText+".log")
+			}
+			return env.DeleteNamespace(namespace)
+		})
+
+		AssertCreateCluster(namespace, clusterName, clusterFile, env)
+		createAndAssertPgBouncerPoolerIsSetUp(namespace, poolerFile, 3)
+
+		_, _, err := utils.Run(fmt.Sprintf("kubectl scale --replicas=3 -n %v cluster/%v", namespace, clusterName))
+		Expect(err).ToNot(HaveOccurred())
+		AssertClusterIsReady(namespace, clusterName, 300, env)
+	})
+})
