ed334164146d1cac68d9b3979e3fcad2d2162459
Marco Nenciarini <marco.nenciarini@enterprisedb.com>
2024-09-04 10:48:16 +0200

    chore: revierw
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>

--- tests/utils/release_test.go
+++ tests/utils/release_test.go
@@ -17,7 +17,6 @@ limitations under the License.
 package utils
 
 import (
-	"os"
 	"path/filepath"
 	"strings"
 
@@ -36,29 +35,31 @@ var _ = Describe("Release tag extraction", func() {
 
 var _ = Describe("Most recent tag", func() {
 	It("properly works with release branch", func() {
-		wd, err := os.Getwd()
+		releasesDir, err := filepath.Abs("../../releases")
 		Expect(err).ToNot(HaveOccurred())
-		releasesDir := filepath.Join(filepath.Dir(filepath.Dir(wd)), "releases")
+
 		versionList, err := GetAvailableReleases(releasesDir)
 		Expect(err).ToNot(HaveOccurred())
 		if len(versionList) < 2 {
 			Skip("because we need two or more releases")
 		}
-		err = os.Setenv("BRANCH_NAME", "release/v"+versions.Version)
-		Expect(err).ToNot(HaveOccurred())
+
+		GinkgoT().Setenv("BRANCH_NAME", "release/v"+versions.Version)
+
 		tag, err := GetMostRecentReleaseTag(releasesDir)
+		Expect(err).ToNot(HaveOccurred())
 		Expect(tag).To(Not(BeEmpty()))
 		Expect(tag).ToNot(BeEquivalentTo(versions.Version))
-		Expect(err).ToNot(HaveOccurred())
 	})
 
 	It("properly works with dev branch", func() {
-		wd, err := os.Getwd()
+		releasesDir, err := filepath.Abs("../../releases")
 		Expect(err).ToNot(HaveOccurred())
-		releasesDir := filepath.Join(filepath.Dir(filepath.Dir(wd)), "releases")
-		err = os.Setenv("BRANCH_NAME", "dev/"+versions.Version)
-		Expect(err).ToNot(HaveOccurred())
+
+		GinkgoT().Setenv("BRANCH_NAME", "dev/"+versions.Version)
+
 		tag, err := GetMostRecentReleaseTag(releasesDir)
+		Expect(err).ToNot(HaveOccurred())
 		Expect(tag).To(Not(BeEmpty()))
 		if strings.Contains(versions.Version, "-rc") {
 			// an RC release should not count as the most recent release
@@ -66,26 +67,22 @@ var _ = Describe("Most recent tag", func() {
 		} else {
 			Expect(tag).To(BeEquivalentTo(versions.Version))
 		}
-		Expect(err).ToNot(HaveOccurred())
 	})
 })
 
-var _ = Describe("Fail on no-existing releases", func() {
-	It("properly error out if the release directory doesn't exists", func() {
-		currentDir, err := os.Getwd()
-		Expect(err).ToNot(HaveOccurred())
-		releaseDir := filepath.Join(filepath.Dir(currentDir), "does-no-exist")
-		versionList, err := GetAvailableReleases(releaseDir)
+var _ = Describe("GetAvailableReleases fails on wrong release directory", func() {
+	It("properly errors out if the release directory doesn't exist", func() {
+		tmpDir := GinkgoT().TempDir()
+		nonexistent := filepath.Join(filepath.Dir(tmpDir), "nonexistent")
+
+		_, err := GetAvailableReleases(nonexistent)
 		Expect(err).To(HaveOccurred())
-		Expect(versionList).To(BeEmpty())
 	})
 
-	It("properly fail if there's no tag", func() {
-		releaseDir, err := os.Getwd()
-		Expect(err).ToNot(HaveOccurred())
+	It("properly fails if there's no release files in the directory", func() {
+		tmpDir := GinkgoT().TempDir()
 
-		tag, err := GetMostRecentReleaseTag(releaseDir)
+		_, err := GetMostRecentReleaseTag(tmpDir)
 		Expect(err).To(HaveOccurred())
-		Expect(tag).To(BeEmpty())
 	})
 })
