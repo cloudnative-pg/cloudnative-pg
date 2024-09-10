266da800719bdbfe311d9db7d7397b8d7c4f3b26
Marco Nenciarini <marco.nenciarini@enterprisedb.com>
2024-09-10 18:02:30 +0200

    test: upgrade tests should not pick RC versions as starting point (#5425)
    Make sure upgrade tests do not use any pre-release versions as the test
    starting point.
    
    Partially closes #5331
    
    Split from #5297
    
    Signed-off-by: Jaime Silvela <jaime.silvela@enterprisedb.com>
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    Signed-off-by: Jonathan Gonzalez V. <jonathan.gonzalez@enterprisedb.com>
    Co-authored-by: Jaime Silvela <jaime.silvela@enterprisedb.com>
    Co-authored-by: Jonathan Gonzalez V. <jonathan.gonzalez@enterprisedb.com>
    (cherry picked from commit 930a8df92708dfd682dd853a766aa77cb318487d)

--- tests/utils/release_test.go
+++ tests/utils/release_test.go
@@ -17,8 +17,8 @@ limitations under the License.
 package utils
 
 import (
-	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/cloudnative-pg/cloudnative-pg/pkg/versions"
 
@@ -35,31 +35,54 @@ var _ = Describe("Release tag extraction", func() {
 
 var _ = Describe("Most recent tag", func() {
 	It("properly works with release branch", func() {
-		wd, err := os.Getwd()
+		releasesDir, err := filepath.Abs("../../releases")
 		Expect(err).ToNot(HaveOccurred())
-		releasesDir := filepath.Join(filepath.Dir(filepath.Dir(wd)), "releases")
+
 		versionList, err := GetAvailableReleases(releasesDir)
 		Expect(err).ToNot(HaveOccurred())
 		if len(versionList) < 2 {
 			Skip("because we need two or more releases")
 		}
-		err = os.Setenv("BRANCH_NAME", "release/v"+versions.Version)
-		Expect(err).ToNot(HaveOccurred())
+
+		GinkgoT().Setenv("BRANCH_NAME", "release/v"+versions.Version)
+
 		tag, err := GetMostRecentReleaseTag(releasesDir)
+		Expect(err).ToNot(HaveOccurred())
 		Expect(tag).To(Not(BeEmpty()))
 		Expect(tag).ToNot(BeEquivalentTo(versions.Version))
-		Expect(err).ToNot(HaveOccurred())
 	})
 
 	It("properly works with dev branch", func() {
-		wd, err := os.Getwd()
+		releasesDir, err := filepath.Abs("../../releases")
 		Expect(err).ToNot(HaveOccurred())
-		releasesDir := filepath.Join(filepath.Dir(filepath.Dir(wd)), "releases")
-		err = os.Setenv("BRANCH_NAME", "dev/"+versions.Version)
-		Expect(err).ToNot(HaveOccurred())
+
+		GinkgoT().Setenv("BRANCH_NAME", "dev/"+versions.Version)
+
 		tag, err := GetMostRecentReleaseTag(releasesDir)
-		Expect(tag).To(Not(BeEmpty()))
-		Expect(tag).To(BeEquivalentTo(versions.Version))
 		Expect(err).ToNot(HaveOccurred())
+		Expect(tag).To(Not(BeEmpty()))
+		if strings.Contains(versions.Version, "-rc") {
+			// an RC release should not count as the most recent release
+			Expect(tag).NotTo(BeEquivalentTo(versions.Version))
+		} else {
+			Expect(tag).To(BeEquivalentTo(versions.Version))
+		}
+	})
+})
+
+var _ = Describe("GetAvailableReleases fails on wrong release directory", func() {
+	It("properly errors out if the release directory doesn't exist", func() {
+		tmpDir := GinkgoT().TempDir()
+		nonexistent := filepath.Join(filepath.Dir(tmpDir), "nonexistent")
+
+		_, err := GetAvailableReleases(nonexistent)
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("properly fails if there's no release files in the directory", func() {
+		tmpDir := GinkgoT().TempDir()
+
+		_, err := GetMostRecentReleaseTag(tmpDir)
+		Expect(err).To(HaveOccurred())
 	})
 })
