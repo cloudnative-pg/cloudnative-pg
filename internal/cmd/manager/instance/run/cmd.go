69ce58afd39899a6317d35092ec17ef71aa5b3db
Armando Ruocco <armando.ruocco@enterprisedb.com>
2024-09-11 17:47:12 +0200

    fix(webserver): use manager client and recorder (#5397)
    Update the local webserver to use the event recorder and the client
    created by the manager instead of initializing separate instances.
    
    Closes #5398
    
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    Signed-off-by: Jaime Silvela <jaime.silvela@enterprisedb.com>
    Co-authored-by: Jaime Silvela <jaime.silvela@enterprisedb.com>
    (cherry picked from commit ea631b0a83621bc6f1ccb9f07dd9a9234ba623b6)

--- internal/cmd/manager/instance/run/cmd.go
+++ internal/cmd/manager/instance/run/cmd.go
@@ -136,6 +136,9 @@ func runSubCommand(ctx context.Context, instance *postgres.Instance) error {
 				DisableFor: []client.Object{
 					&corev1.Secret{},
 					&corev1.ConfigMap{},
+					// we don't have the permissions to cache backups, as the ServiceAccount
+					// doesn't have watch permission on the backup status
+					&apiv1.Backup{},
 				},
 			},
 		},
@@ -240,7 +243,11 @@ func runSubCommand(ctx context.Context, instance *postgres.Instance) error {
 		return err
 	}
 
-	localSrv, err := webserver.NewLocalWebServer(instance)
+	localSrv, err := webserver.NewLocalWebServer(
+		instance,
+		mgr.GetClient(),
+		mgr.GetEventRecorderFor("local-webserver"),
+	)
 	if err != nil {
 		return err
 	}
