bf362f01c990f45ee62cf7216d221acbe17c3856
Niccolò Fei <niccolo.fei@enterprisedb.com>
2024-09-18 17:23:07 +0200

    chore: refactor to enable reconciler unit-tests
    Signed-off-by: Niccolò Fei <niccolo.fei@enterprisedb.com>

--- internal/management/controller/database_controller_test.go
+++ internal/management/controller/database_controller_test.go
@@ -24,16 +24,28 @@ import (
 	"github.com/jackc/pgx/v5"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/utils/ptr"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
 	apiv1 "github.com/cloudnative-pg/cloudnative-pg/api/v1"
 	schemeBuilder "github.com/cloudnative-pg/cloudnative-pg/internal/scheme"
+	"github.com/cloudnative-pg/cloudnative-pg/pkg/management/postgres"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
+
+type fakeInstanceData struct {
+	instanceData
+	db *sql.DB
+}
+
+func (f *fakeInstanceData) GetSuperUserDB() (*sql.DB, error) {
+	return f.db, nil
+}
 
 var _ = Describe("Managed Database SQL", func() {
 	var (
@@ -184,25 +196,101 @@ var _ = Describe("Managed Database SQL", func() {
 
 var _ = Describe("Managed Database status", func() {
 	var (
+		dbMock     sqlmock.Sqlmock
+		db         *sql.DB
 		database   *apiv1.Database
+		cluster    *apiv1.Cluster
 		r          *DatabaseReconciler
 		fakeClient client.Client
+		err        error
 	)
 
 	BeforeEach(func() {
-		database = &apiv1.Database{
+		cluster = &apiv1.Cluster{
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      "db-one",
+				Name:      "cluster-example",
 				Namespace: "default",
 			},
+			Status: apiv1.ClusterStatus{
+				CurrentPrimary: "cluster-example-1",
+				TargetPrimary:  "cluster-example-1",
+			},
 		}
+		database = &apiv1.Database{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:       "db-one",
+				Namespace:  "default",
+				Generation: 1,
+			},
+			Spec: apiv1.DatabaseSpec{
+				ClusterRef: corev1.LocalObjectReference{
+					Name: cluster.Name,
+				},
+				Name:  "db-one",
+				Owner: "app",
+			},
+		}
+
+		db, dbMock, err = sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+		Expect(err).ToNot(HaveOccurred())
+
+		pgInstance := &postgres.Instance{
+			Namespace:   "default",
+			PodName:     "cluster-example-1",
+			ClusterName: "cluster-example",
+		}
+
+		f := fakeInstanceData{
+			instanceData: instanceData{instance: pgInstance},
+			db:           db,
+		}
+
 		fakeClient = fake.NewClientBuilder().WithScheme(schemeBuilder.BuildWithAllKnownScheme()).
-			WithObjects(database).WithStatusSubresource(database).Build()
+			WithObjects(cluster, database).
+			WithStatusSubresource(&apiv1.Cluster{}, &apiv1.Database{}).
+			Build()
 
 		r = &DatabaseReconciler{
-			Client: fakeClient,
-			Scheme: schemeBuilder.BuildWithAllKnownScheme(),
+			Client:   fakeClient,
+			Scheme:   schemeBuilder.BuildWithAllKnownScheme(),
+			instance: &f,
 		}
+	})
+
+	AfterEach(func() {
+		Expect(dbMock.ExpectationsWereMet()).To(Succeed())
+	})
+
+	It("database object inherits error after patching", func(ctx SpecContext) {
+		// Mocking DetectDB
+		expectedValue := sqlmock.NewRows([]string{""}).AddRow("1")
+		dbMock.ExpectQuery(`SELECT count(*)
+		FROM pg_database
+		WHERE datname = $1`).WithArgs(database.Spec.Name).WillReturnRows(expectedValue)
+
+		// Mocking Alter Database
+		expectedError := fmt.Errorf("no permission")
+		expectedQuery := fmt.Sprintf("ALTER DATABASE %s OWNER TO %s",
+			pgx.Identifier{database.Spec.Name}.Sanitize(),
+			pgx.Identifier{database.Spec.Owner}.Sanitize(),
+		)
+		dbMock.ExpectExec(expectedQuery).WillReturnError(expectedError)
+
+		_, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: types.NamespacedName{
+			Namespace: database.Namespace,
+			Name:      database.Spec.Name,
+		}})
+		Expect(err).ToNot(HaveOccurred())
+
+		var updatedDatabase apiv1.Database
+		err = fakeClient.Get(ctx, client.ObjectKey{
+			Namespace: database.Namespace,
+			Name:      database.Name,
+		}, &updatedDatabase)
+		Expect(err).ToNot(HaveOccurred())
+
+		Expect(updatedDatabase.Status.Ready).Should(BeFalse())
+		Expect(updatedDatabase.Status.Error).Should(ContainSubstring(expectedError.Error()))
 	})
 
 	It("properly marks the status on a succeeded reconciliation", func(ctx SpecContext) {
