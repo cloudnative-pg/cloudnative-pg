a9822ae7e7c9705f32a74c42a5ecc27b4314c89b
Jaime Silvela <jaime.silvela@enterprisedb.com>
2024-09-26 16:46:50 +0200

    chore: leverage common test structure
    Signed-off-by: Jaime Silvela <jaime.silvela@enterprisedb.com>

--- internal/management/controller/tablespaces/controller_test.go
+++ internal/management/controller/tablespaces/controller_test.go
@@ -69,8 +69,8 @@ func (f fakeInstance) IsPrimary() (bool, error) {
 	return true, nil
 }
 
-var _ = Describe("Tablespace synchronizer tests", func() {
-	expectedListStmt := `
+const (
+	expectedListStmt = `
 	SELECT
 		pg_tablespace.spcname spcname,
 		COALESCE(pg_roles.rolname, '') rolname
@@ -78,337 +78,287 @@ var _ = Describe("Tablespace synchronizer tests", func() {
 	LEFT JOIN pg_roles ON pg_tablespace.spcowner = pg_roles.oid
 	WHERE spcname NOT LIKE $1
 	`
-	expectedCreateStmt := "CREATE TABLESPACE \"%s\" OWNER \"%s\" " +
+	expectedCreateStmt = "CREATE TABLESPACE \"%s\" OWNER \"%s\" " +
 		"LOCATION '%s'"
 
-	expectedUpdateStmt := "ALTER TABLESPACE \"%s\" OWNER TO \"%s\""
+	expectedUpdateStmt = "ALTER TABLESPACE \"%s\" OWNER TO \"%s\""
 
-	expectedReadinessCheck := `
+	expectedReadinessCheck = `
 		SELECT
 			NOT pg_is_in_recovery()
 			OR (SELECT coalesce(setting, '') = '' FROM pg_settings WHERE name = 'primary_conninfo')
 			OR pg_last_wal_replay_lsn() IS NOT NULL
 		`
+)
 
-	var (
-		dbMock               sqlmock.Sqlmock
-		db                   *sql.DB
-		err                  error
-		fakeClient           client.Client
-		cluster              *apiv1.Cluster
-		tablespaceReconciler TablespaceReconciler
-		instance             fakeInstance
-	)
+func getCluster(ctx context.Context, c client.Client, cluster *apiv1.Cluster) (*apiv1.Cluster, error) {
+	var updatedCluster apiv1.Cluster
+	err := c.Get(ctx, client.ObjectKey{
+		Namespace: cluster.Namespace,
+		Name:      cluster.Name,
+	}, &updatedCluster)
+	return &updatedCluster, err
+}
 
-	tablespaceReconciler = TablespaceReconciler{
-		instance: postgres.NewInstance().WithNamespace("myPod"),
+type tablespaceTest struct {
+	tablespacesInSpec        []apiv1.TablespaceConfiguration
+	postgresExpectations     func(sqlmock.Sqlmock)
+	shouldRequeue            bool
+	storageManager           tablespaceStorageManager
+	expectedTablespaceStatus []apiv1.TablespaceState
+}
+
+func assertTablespaceReconciled(ctx context.Context, tt tablespaceTest) {
+	db, dbMock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual), sqlmock.MonitorPingsOption(true))
+	Expect(err).ToNot(HaveOccurred())
+
+	cluster := &apiv1.Cluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "cluster-example",
+			Namespace: "default",
+		},
+	}
+	cluster.Spec.Tablespaces = tt.tablespacesInSpec
+
+	fakeClient := fake.NewClientBuilder().WithScheme(schemeBuilder.BuildWithAllKnownScheme()).
+		WithObjects(cluster).
+		WithStatusSubresource(&apiv1.Cluster{}).
+		Build()
+	instance := fakeInstance{
+		Instance: &postgres.Instance{
+			Namespace:   "default",
+			ClusterName: "cluster-example",
+		},
+		db: db,
+	}
+	tablespaceReconciler := TablespaceReconciler{
+		instance:       &instance,
+		client:         fakeClient,
+		storageManager: tt.storageManager,
 	}
 
-	BeforeEach(func() {
-		db, dbMock, err = sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual), sqlmock.MonitorPingsOption(true))
-		Expect(err).ToNot(HaveOccurred())
-		cluster = &apiv1.Cluster{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      "cluster-example",
-				Namespace: "default",
-			},
-		}
-		fakeClient = fake.NewClientBuilder().WithScheme(schemeBuilder.BuildWithAllKnownScheme()).
-			WithObjects(cluster).
-			WithStatusSubresource(&apiv1.Cluster{}).
-			Build()
-		instance = fakeInstance{
-			Instance: &postgres.Instance{
-				Namespace:   "default",
-				ClusterName: "cluster-example",
-			},
-			db: db,
-		}
-		tablespaceReconciler = TablespaceReconciler{
-			instance: &instance,
-			client:   fakeClient,
-		}
+	// these bits happen because the reconciler checks for instance readiness
+	dbMock.ExpectPing()
+	expectedReadiness := sqlmock.NewRows([]string{""}).AddRow("t")
+	dbMock.ExpectQuery(expectedReadinessCheck).WillReturnRows(expectedReadiness)
 
-		// these bits happen since the reconciler checks for instance readiness
-		dbMock.ExpectPing()
-		expectedReadiness := sqlmock.NewRows([]string{""}).AddRow("t")
-		dbMock.ExpectQuery(expectedReadinessCheck).WillReturnRows(expectedReadiness)
-	})
+	tt.postgresExpectations(dbMock)
 
-	AfterEach(func() {
-		Expect(dbMock.ExpectationsWereMet()).To(Succeed())
-	})
+	results, err := tablespaceReconciler.Reconcile(ctx, reconcile.Request{})
+	Expect(err).ShouldNot(HaveOccurred())
+	if tt.shouldRequeue {
+		Expect(results).NotTo(BeZero())
+	} else {
+		Expect(results).To(BeZero())
+	}
 
+	updatedCluster, err := getCluster(ctx, fakeClient, cluster)
+	Expect(err).ToNot(HaveOccurred())
+	Expect(updatedCluster.Status.TablespacesStatus).To(Equal(tt.expectedTablespaceStatus))
+
+	Expect(dbMock.ExpectationsWereMet()).To(Succeed())
+}
+
+var _ = Describe("Tablespace synchronizer tests", func() {
 	When("tablespace configurations are realizable", func() {
 		It("will do nothing if the DB contains the tablespaces in spec", func(ctx context.Context) {
-			initialCluster := cluster.DeepCopy()
-			cluster.Spec.Tablespaces = []apiv1.TablespaceConfiguration{
-				{
-					Name: "foo",
-					Storage: apiv1.StorageConfiguration{
-						Size: "1Gi",
-					},
-					Owner: apiv1.DatabaseRoleRef{
-						Name: "app",
+			assertTablespaceReconciled(ctx, tablespaceTest{
+				tablespacesInSpec: []apiv1.TablespaceConfiguration{
+					{
+						Name: "foo",
+						Storage: apiv1.StorageConfiguration{
+							Size: "1Gi",
+						},
+						Owner: apiv1.DatabaseRoleRef{
+							Name: "app",
+						},
 					},
 				},
-			}
-			err := fakeClient.Patch(ctx, cluster, client.MergeFrom(initialCluster))
-			Expect(err).NotTo(HaveOccurred())
-
-			// we expect the reconciler to list the tablespaces on the DB
-			rows := sqlmock.NewRows(
-				[]string{"spcname", "rolname"}).
-				AddRow("foo", "app")
-			dbMock.ExpectQuery(expectedListStmt).WithArgs("pg_").WillReturnRows(rows)
-
-			results, err := tablespaceReconciler.Reconcile(ctx, reconcile.Request{})
-			Expect(err).ShouldNot(HaveOccurred())
-			Expect(results).To(BeZero())
-
-			var updatedCluster apiv1.Cluster
-			err = fakeClient.Get(ctx, client.ObjectKey{
-				Namespace: cluster.Namespace,
-				Name:      cluster.Name,
-			}, &updatedCluster)
-			Expect(err).ToNot(HaveOccurred())
-			expectedTablespaceStatus := []apiv1.TablespaceState{
-				{
-					Name:  "foo",
-					Owner: "app",
-					State: "reconciled",
+				postgresExpectations: func(mock sqlmock.Sqlmock) {
+					// we expect the reconciler to list the tablespaces on the DB
+					rows := sqlmock.NewRows(
+						[]string{"spcname", "rolname"}).
+						AddRow("foo", "app")
+					mock.ExpectQuery(expectedListStmt).WithArgs("pg_").WillReturnRows(rows)
 				},
-			}
-			Expect(updatedCluster.Status.TablespacesStatus).To(Equal(expectedTablespaceStatus))
+				shouldRequeue: false,
+				expectedTablespaceStatus: []apiv1.TablespaceState{
+					{
+						Name:  "foo",
+						Owner: "app",
+						State: "reconciled",
+					},
+				},
+			})
 		})
 
 		It("will change the owner when needed", func(ctx context.Context) {
-			initialCluster := cluster.DeepCopy()
-			cluster.Spec.Tablespaces = []apiv1.TablespaceConfiguration{
-				{
-					Name: "foo",
-					Storage: apiv1.StorageConfiguration{
-						Size: "1Gi",
-					},
-					Owner: apiv1.DatabaseRoleRef{
-						Name: "new_user",
+			assertTablespaceReconciled(ctx, tablespaceTest{
+				tablespacesInSpec: []apiv1.TablespaceConfiguration{
+					{
+						Name: "foo",
+						Storage: apiv1.StorageConfiguration{
+							Size: "1Gi",
+						},
+						Owner: apiv1.DatabaseRoleRef{
+							Name: "new_user",
+						},
 					},
 				},
-			}
-			err := fakeClient.Patch(ctx, cluster, client.MergeFrom(initialCluster))
-			Expect(err).NotTo(HaveOccurred())
-
-			rows := sqlmock.NewRows(
-				[]string{"spcname", "rolname"}).
-				AddRow("foo", "app")
-			dbMock.ExpectQuery(expectedListStmt).WithArgs("pg_").WillReturnRows(rows)
-			stmt := fmt.Sprintf(expectedUpdateStmt, "foo", "new_user")
-			dbMock.ExpectExec(stmt).
-				WillReturnResult(sqlmock.NewResult(2, 1))
-
-			results, err := tablespaceReconciler.Reconcile(ctx, reconcile.Request{})
-			Expect(err).ShouldNot(HaveOccurred())
-			Expect(results).To(BeZero())
-
-			var updatedCluster apiv1.Cluster
-			err = fakeClient.Get(ctx, client.ObjectKey{
-				Namespace: cluster.Namespace,
-				Name:      cluster.Name,
-			}, &updatedCluster)
-			Expect(err).ToNot(HaveOccurred())
-			expectedTablespaceStatus := []apiv1.TablespaceState{
-				{
-					Name:  "foo",
-					Owner: "new_user",
-					State: "reconciled",
+				postgresExpectations: func(mock sqlmock.Sqlmock) {
+					rows := sqlmock.NewRows(
+						[]string{"spcname", "rolname"}).
+						AddRow("foo", "app")
+					mock.ExpectQuery(expectedListStmt).WithArgs("pg_").WillReturnRows(rows)
+					stmt := fmt.Sprintf(expectedUpdateStmt, "foo", "new_user")
+					mock.ExpectExec(stmt).
+						WillReturnResult(sqlmock.NewResult(2, 1))
 				},
-			}
-			Expect(updatedCluster.Status.TablespacesStatus).To(Equal(expectedTablespaceStatus))
+				shouldRequeue: false,
+				expectedTablespaceStatus: []apiv1.TablespaceState{
+					{
+						Name:  "foo",
+						Owner: "new_user",
+						State: "reconciled",
+					},
+				},
+			})
 		})
 
 		It("will create a tablespace in spec that is missing from DB if mount point exists", func(ctx context.Context) {
-			initialCluster := cluster.DeepCopy()
-			cluster.Spec.Tablespaces = []apiv1.TablespaceConfiguration{
-				{
-					Name: "foo",
-					Storage: apiv1.StorageConfiguration{
-						Size: "1Gi",
+			assertTablespaceReconciled(ctx, tablespaceTest{
+				tablespacesInSpec: []apiv1.TablespaceConfiguration{
+					{
+						Name: "foo",
+						Storage: apiv1.StorageConfiguration{
+							Size: "1Gi",
+						},
+					},
+					{
+						Name: "bar",
+						Storage: apiv1.StorageConfiguration{
+							Size: "1Gi",
+						},
+						Owner: apiv1.DatabaseRoleRef{
+							Name: "new_user",
+						},
 					},
 				},
-				{
-					Name: "bar",
-					Storage: apiv1.StorageConfiguration{
-						Size: "1Gi",
-					},
-					Owner: apiv1.DatabaseRoleRef{
-						Name: "new_user",
-					},
+				postgresExpectations: func(mock sqlmock.Sqlmock) {
+					// we expect the reconciler to list the tablespaces on DB, and to
+					// create a new tablespace
+					rows := sqlmock.NewRows(
+						[]string{"spcname", "rolname"}).
+						AddRow("foo", "")
+					mock.ExpectQuery(expectedListStmt).WithArgs("pg_").WillReturnRows(rows)
+					stmt := fmt.Sprintf(expectedCreateStmt, "bar", "new_user", "/var/lib/postgresql/tablespaces/bar/data")
+					mock.ExpectExec(stmt).
+						WillReturnResult(sqlmock.NewResult(2, 1))
 				},
-			}
-			err := fakeClient.Patch(ctx, cluster, client.MergeFrom(initialCluster))
-			Expect(err).NotTo(HaveOccurred())
-
-			// we expect the reconciler to list the tablespaces on DB, and to
-			// create a new tablespace
-			rows := sqlmock.NewRows(
-				[]string{"spcname", "rolname"}).
-				AddRow("foo", "")
-			dbMock.ExpectQuery(expectedListStmt).WithArgs("pg_").WillReturnRows(rows)
-			stmt := fmt.Sprintf(expectedCreateStmt, "bar", "new_user", "/var/lib/postgresql/tablespaces/bar/data")
-			dbMock.ExpectExec(stmt).
-				WillReturnResult(sqlmock.NewResult(2, 1))
-
-			tablespaceReconciler = TablespaceReconciler{
-				instance: instance,
-				client:   fakeClient,
+				shouldRequeue: false,
 				storageManager: mockTablespaceStorageManager{
 					unavailableStorageLocations: []string{
 						"/foo",
 					},
 				},
-			}
-
-			results, err := tablespaceReconciler.Reconcile(ctx, reconcile.Request{})
-			Expect(err).ShouldNot(HaveOccurred())
-			Expect(results).To(BeZero())
-
-			var updatedCluster apiv1.Cluster
-			err = fakeClient.Get(ctx, client.ObjectKey{
-				Namespace: cluster.Namespace,
-				Name:      cluster.Name,
-			}, &updatedCluster)
-			Expect(err).ToNot(HaveOccurred())
-			expectedTablespaceStatus := []apiv1.TablespaceState{
-				{
-					Name:  "foo",
-					Owner: "",
-					State: "reconciled",
+				expectedTablespaceStatus: []apiv1.TablespaceState{
+					{
+						Name:  "foo",
+						Owner: "",
+						State: "reconciled",
+					},
+					{
+						Name:  "bar",
+						Owner: "new_user",
+						State: "reconciled",
+					},
 				},
-				{
-					Name:  "bar",
-					Owner: "new_user",
-					State: "reconciled",
-				},
-			}
-			Expect(updatedCluster.Status.TablespacesStatus).To(Equal(expectedTablespaceStatus))
+			})
 		})
 
 		It("will mark tablespace status as pending with error when the DB CREATE fails", func(ctx context.Context) {
-			initialCluster := cluster.DeepCopy()
-			cluster.Spec.Tablespaces = []apiv1.TablespaceConfiguration{
-				{
-					Name: "foo",
-					Storage: apiv1.StorageConfiguration{
-						Size: "1Gi",
+			assertTablespaceReconciled(ctx, tablespaceTest{
+				tablespacesInSpec: []apiv1.TablespaceConfiguration{
+					{
+						Name: "foo",
+						Storage: apiv1.StorageConfiguration{
+							Size: "1Gi",
+						},
+					},
+					{
+						Name: "bar",
+						Storage: apiv1.StorageConfiguration{
+							Size: "1Gi",
+						},
+						Owner: apiv1.DatabaseRoleRef{
+							Name: "new_user",
+						},
 					},
 				},
-				{
-					Name: "bar",
-					Storage: apiv1.StorageConfiguration{
-						Size: "1Gi",
-					},
-					Owner: apiv1.DatabaseRoleRef{
-						Name: "new_user",
-					},
+				postgresExpectations: func(mock sqlmock.Sqlmock) {
+					// we expect the reconciler to list the tablespaces on DB, and to
+					// create a new tablespace
+					rows := sqlmock.NewRows(
+						[]string{"spcname", "rolname"}).
+						AddRow("foo", "")
+					mock.ExpectQuery(expectedListStmt).WithArgs("pg_").WillReturnRows(rows)
+					// we simulate DB command failure
+					stmt := fmt.Sprintf(expectedCreateStmt, "bar", "new_user", "/var/lib/postgresql/tablespaces/bar/data")
+					mock.ExpectExec(stmt).
+						WillReturnError(errors.New("boom"))
 				},
-			}
-			err := fakeClient.Patch(ctx, cluster, client.MergeFrom(initialCluster))
-			Expect(err).NotTo(HaveOccurred())
-
-			// we expect the reconciler to list the tablespaces on DB, and to
-			// create a new tablespace
-			rows := sqlmock.NewRows(
-				[]string{"spcname", "rolname"}).
-				AddRow("foo", "")
-			dbMock.ExpectQuery(expectedListStmt).WithArgs("pg_").WillReturnRows(rows)
-			// we simulate DB command failure
-			stmt := fmt.Sprintf(expectedCreateStmt, "bar", "new_user", "/var/lib/postgresql/tablespaces/bar/data")
-			dbMock.ExpectExec(stmt).
-				WillReturnError(errors.New("boom"))
-
-			tablespaceReconciler = TablespaceReconciler{
-				instance: instance,
-				client:   fakeClient,
+				shouldRequeue: true,
 				storageManager: mockTablespaceStorageManager{
 					unavailableStorageLocations: []string{
 						"/foo",
 					},
 				},
-			}
-
-			results, err := tablespaceReconciler.Reconcile(ctx, reconcile.Request{})
-			Expect(err).ShouldNot(HaveOccurred())
-			Expect(results).NotTo(BeZero())
-
-			var updatedCluster apiv1.Cluster
-			err = fakeClient.Get(ctx, client.ObjectKey{
-				Namespace: cluster.Namespace,
-				Name:      cluster.Name,
-			}, &updatedCluster)
-			Expect(err).ToNot(HaveOccurred())
-			expectedTablespaceStatus := []apiv1.TablespaceState{
-				{
-					Name:  "foo",
-					Owner: "",
-					State: "reconciled",
+				expectedTablespaceStatus: []apiv1.TablespaceState{
+					{
+						Name:  "foo",
+						Owner: "",
+						State: "reconciled",
+					},
+					{
+						Name:  "bar",
+						Owner: "new_user",
+						State: "pending",
+						Error: "while creating tablespace bar: boom",
+					},
 				},
-				{
-					Name:  "bar",
-					Owner: "new_user",
-					State: "pending",
-					Error: "while creating tablespace bar: boom",
-				},
-			}
-			Expect(updatedCluster.Status.TablespacesStatus).To(Equal(expectedTablespaceStatus))
+			})
 		})
 
 		It("will requeue the tablespace creation if the mount path doesn't exist", func(ctx context.Context) {
-			initialCluster := cluster.DeepCopy()
-			cluster.Spec.Tablespaces = []apiv1.TablespaceConfiguration{
-				{
-					Name: "foo",
-					Storage: apiv1.StorageConfiguration{
-						Size: "1Gi",
+			assertTablespaceReconciled(ctx, tablespaceTest{
+				tablespacesInSpec: []apiv1.TablespaceConfiguration{
+					{
+						Name: "foo",
+						Storage: apiv1.StorageConfiguration{
+							Size: "1Gi",
+						},
 					},
 				},
-			}
-			err := fakeClient.Patch(ctx, cluster, client.MergeFrom(initialCluster))
-			Expect(err).NotTo(HaveOccurred())
-
-			rows := sqlmock.NewRows(
-				[]string{"spcname", "rolname"})
-			dbMock.ExpectQuery(expectedListStmt).WithArgs("pg_").WillReturnRows(rows)
-
-			tablespaceReconciler = TablespaceReconciler{
-				instance: instance,
-				client:   fakeClient,
+				postgresExpectations: func(mock sqlmock.Sqlmock) {
+					rows := sqlmock.NewRows(
+						[]string{"spcname", "rolname"})
+					mock.ExpectQuery(expectedListStmt).WithArgs("pg_").WillReturnRows(rows)
+				},
+				shouldRequeue: true,
 				storageManager: mockTablespaceStorageManager{
 					unavailableStorageLocations: []string{
 						"/foo",
 					},
 				},
-			}
-
-			results, err := tablespaceReconciler.Reconcile(ctx, reconcile.Request{})
-			Expect(err).ShouldNot(HaveOccurred())
-			Expect(results).NotTo(BeNil())
-			Expect(results.RequeueAfter).NotTo(BeZero())
-
-			var updatedCluster apiv1.Cluster
-			err = fakeClient.Get(ctx, client.ObjectKey{
-				Namespace: cluster.Namespace,
-				Name:      cluster.Name,
-			}, &updatedCluster)
-			Expect(err).ToNot(HaveOccurred())
-			expectedTablespaceStatus := []apiv1.TablespaceState{
-				{
-					Name:  "foo",
-					Owner: "",
-					State: "pending",
-					Error: "deferred until mount point is created",
+				expectedTablespaceStatus: []apiv1.TablespaceState{
+					{
+						Name:  "foo",
+						Owner: "",
+						State: "pending",
+						Error: "deferred until mount point is created",
+					},
 				},
-			}
-			Expect(updatedCluster.Status.TablespacesStatus).To(Equal(expectedTablespaceStatus))
+			})
 		})
 	})
 })
