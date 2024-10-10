58bdf2b6b005d4913c07bfefed875739adf9c0c3
Jaime Silvela <jaime.silvela@enterprisedb.com>
2024-10-10 09:13:02 +0200

    Update tests/e2e/declarative_database_management_test.go
    Signed-off-by: Jaime Silvela <jaime.silvela@enterprisedb.com>

--- tests/e2e/declarative_database_management_test.go
+++ tests/e2e/declarative_database_management_test.go
@@ -239,7 +239,7 @@ var _ = Describe("Declarative database management", Label(tests.LabelSmoke, test
 				}, 300).WithPolling(10 * time.Second).Should(Succeed())
 			})
 			By("deleting the namespace and making sure it succeeds before timeout", func() {
-				err := env.DeleteNamespaceAndWait(namespace, 30)
+				err := env.DeleteNamespaceAndWait(namespace, 60)
 				Expect(err).ToNot(HaveOccurred())
 			})
 		})
