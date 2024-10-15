47d61ed36936d4f135408e6b63461ad5cf562d62
Balthazar Rouberol <br@imap.cc>
2024-10-15 10:13:24 +0200

    fix: set `TMPDIR` and `PSQL_HISTORY` environment variables (#5503)
    Ensure the `TMPDIR` environment variable is correctly set to
    `/controller/tmp` for temporary files. Additionally, define the
    `PSQL_HISTORY` variable as `/controller/tmp/.psql_history` to store
    PostgreSQL command history in a controlled location.
    
    This addresses issues with file management during execution and improves the
    isolation of temporary files and session history.
    
    Closes #5420
    Closes #4137
    
    Signed-off-by: Balthazar Rouberol <br@imap.cc>
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
    Co-authored-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>

--- pkg/specs/pods.go
+++ pkg/specs/pods.go
@@ -22,6 +22,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"math"
+	"path"
 	"reflect"
 	"slices"
 	"strconv"
@@ -132,12 +133,20 @@ func CreatePodEnvConfig(cluster apiv1.Cluster, podName string) EnvConfig {
 				Value: cluster.Name,
 			},
 			{
+				Name:  "PSQL_HISTORY",
+				Value: path.Join(postgres.TemporaryDirectory, ".psql_history"),
+			},
+			{
 				Name:  "PGPORT",
 				Value: strconv.Itoa(postgres.ServerPort),
 			},
 			{
 				Name:  "PGHOST",
 				Value: postgres.SocketDirectory,
+			},
+			{
+				Name:  "TMPDIR",
+				Value: postgres.TemporaryDirectory,
 			},
 		},
 		EnvFrom: cluster.Spec.EnvFrom,
