af4b9021ff1a3fc45feee40fd11af53fea811755
Jaime Silvela <jaime.silvela@enterprisedb.com>
2024-10-17 15:22:47 +0200

    chore: simplify
    Signed-off-by: Jaime Silvela <jaime.silvela@enterprisedb.com>

--- tests/utils/namespace.go
+++ tests/utils/namespace.go
@@ -208,13 +208,10 @@ func (env TestingEnvironment) DeleteNamespaceAndWait(name string, timeoutSeconds
 	return wait.PollUntilContextCancel(ctx, time.Second, true,
 		func(ctx context.Context) (bool, error) {
 			err := env.Client.Get(ctx, client.ObjectKey{Name: name}, &corev1.Namespace{})
-			if err != nil {
-				if apierrs.IsNotFound(err) {
-					return true, nil
-				}
-				return false, err
+			if err != nil && apierrs.IsNotFound(err) {
+				return true, nil
 			}
-			return false, nil
+			return false, err
 		},
 	)
 }
