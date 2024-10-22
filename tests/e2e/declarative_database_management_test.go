cdc28e0aaacb436e5d8f9f0aeeca28b326e6a03f
Gabriele Quaresima <gabriele.quaresima@enterprisedb.com>
2024-10-22 13:19:43 +0200

    test(database): add e2e case of declarative database with delete reclaim policy (#5774)
    Closes #5680
    
    Signed-off-by: Gabriele Quaresima <gabriele.quaresima@enterprisedb.com>
    Signed-off-by: Jaime Silvela <jaime.silvela@enterprisedb.com>
    Signed-off-by: Niccolò Fei <niccolo.fei@enterprisedb.com>
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    Co-authored-by: Jaime Silvela <jaime.silvela@enterprisedb.com>
    Co-authored-by: Niccolò Fei <niccolo.fei@enterprisedb.com>
    Co-authored-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>

--- tests/e2e/declarative_database_management_test.go
+++ tests/e2e/declarative_database_management_test.go
@@ -33,11 +33,11 @@ import (
 // - spinning up a cluster, apply a declarative database on it
 
 // Set of tests in which we use the declarative database CRD to add new databases on an existing cluster
-var _ = Describe("Declarative databases management test", Label(tests.LabelSmoke, tests.LabelBasic), func() {
+var _ = Describe("Declarative database management", Label(tests.LabelSmoke, tests.LabelBasic,
+	tests.LabelDeclarativeDatabases), func() {
 	const (
-		clusterManifest  = fixturesDir + "/declarative_databases/cluster.yaml.template"
-		databaseManifest = fixturesDir + "/declarative_databases/database.yaml.template"
-		level            = tests.Medium
+		clusterManifest = fixturesDir + "/declarative_databases/cluster.yaml.template"
+		level           = tests.Medium
 	)
 
 	BeforeEach(func() {
@@ -46,15 +46,14 @@ var _ = Describe("Declarative databases management test", Label(tests.LabelSmoke
 		}
 	})
 
-	Context("plain vanilla cluster", Ordered, func() {
+	Context("in a plain vanilla cluster", Ordered, func() {
 		const (
 			namespacePrefix = "declarative-db"
 			dbname          = "declarative"
 		)
 		var (
-			clusterName, namespace, databaseObjectName string
-			database                                   *apiv1.Database
-			err                                        error
+			clusterName, namespace string
+			err                    error
 		)
 
 		BeforeAll(func() {
@@ -70,27 +69,9 @@ var _ = Describe("Declarative databases management test", Label(tests.LabelSmoke
 			})
 		})
 
-		assertDatabaseExists := func(namespace, primaryPod, dbname string, shouldContain bool) {
-			Eventually(func(g Gomega) {
-				stdout, _, err := env.ExecQueryInInstancePod(
-					utils.PodLocator{
-						Namespace: namespace,
-						PodName:   primaryPod,
-					},
-					"postgres",
-					"\\l")
-				g.Expect(err).ToNot(HaveOccurred())
-				if shouldContain {
-					g.Expect(stdout).Should(ContainSubstring(dbname))
-				} else {
-					g.Expect(stdout).ShouldNot(ContainSubstring(dbname))
-				}
-			}, 300).Should(Succeed())
-		}
-
 		assertDatabaseHasExpectedFields := func(namespace, primaryPod string, db apiv1.Database) {
 			query := fmt.Sprintf("select count(*) from pg_database where datname = '%s' "+
-				"and encoding = %s and datctype = '%s' and datcollate = '%s'",
+				"and encoding = pg_char_to_encoding('%s') and datctype = '%s' and datcollate = '%s'",
 				db.Spec.Name, db.Spec.Encoding, db.Spec.LcCtype, db.Spec.LcCollate)
 			Eventually(func(g Gomega) {
 				stdout, _, err := env.ExecQueryInInstancePod(
@@ -101,65 +82,118 @@ var _ = Describe("Declarative databases management test", Label(tests.LabelSmoke
 					"postgres",
 					query)
 				g.Expect(err).ToNot(HaveOccurred())
-				g.Expect(stdout).Should(ContainSubstring("1"))
+				g.Expect(stdout).Should(ContainSubstring("1"), "expected database not found")
 			}, 30).Should(Succeed())
 		}
 
-		When("Database CRD reclaim policy is set to retain (default) inside spec", func() {
-			It("can add a declarative database", func() {
-				By("applying Database CRD manifest", func() {
-					CreateResourceFromFile(namespace, databaseManifest)
-					databaseObjectName, err = env.GetResourceNameFromYAML(databaseManifest)
-					Expect(err).NotTo(HaveOccurred())
-				})
-				By("ensuring the Database CRD succeeded reconciliation", func() {
-					// get database object
-					database = &apiv1.Database{}
-					databaseNamespacedName := types.NamespacedName{
-						Namespace: namespace,
-						Name:      databaseObjectName,
-					}
+		assertTestDeclarativeDatabase := func(
+			databaseManifest string,
+			retainOnDeletion bool,
+		) {
+			var (
+				database           apiv1.Database
+				databaseObjectName string
+			)
+			By("applying Database CRD manifest", func() {
+				CreateResourceFromFile(namespace, databaseManifest)
+				databaseObjectName, err = env.GetResourceNameFromYAML(databaseManifest)
+				Expect(err).NotTo(HaveOccurred())
+			})
+			By("ensuring the Database CRD succeeded reconciliation", func() {
+				// get database object
+				database = apiv1.Database{}
+				databaseNamespacedName := types.NamespacedName{
+					Namespace: namespace,
+					Name:      databaseObjectName,
+				}
 
-					Eventually(func(g Gomega) {
-						err := env.Client.Get(env.Ctx, databaseNamespacedName, database)
-						Expect(err).ToNot(HaveOccurred())
-						g.Expect(database.Status.Ready).Should(BeTrue())
-					}, 300).WithPolling(10 * time.Second).Should(Succeed())
-				})
-
-				By("verifying new database has been created with the expected fields", func() {
-					primaryPodInfo, err := env.GetClusterPrimary(namespace, clusterName)
-					Expect(err).ToNot(HaveOccurred())
-
-					assertDatabaseExists(namespace, primaryPodInfo.Name, dbname, true)
-
-					// NOTE: the `pg_database` table in Postgres does not contain fields
-					// for the owner nor the template.
-					// Its fields are dependent on the version of Postgres, so we pick
-					// a subset that is available to check even on PG v12
-					expectedDatabaseFields := apiv1.Database{
-						Spec: apiv1.DatabaseSpec{
-							Name:      "declarative",
-							LcCtype:   "en_US.utf8",
-							LcCollate: "C", // this is the default value
-							Encoding:  "0", // corresponds to SQL_ASCII
-						},
-					}
-					assertDatabaseHasExpectedFields(namespace, primaryPodInfo.Name, expectedDatabaseFields)
-				})
+				Eventually(func(g Gomega) {
+					err := env.Client.Get(env.Ctx, databaseNamespacedName, &database)
+					g.Expect(err).ToNot(HaveOccurred())
+					g.Expect(database.Status.Ready).Should(BeTrue())
+				}, 300).WithPolling(10 * time.Second).Should(Succeed())
 			})
 
-			It("keeps the db when Database CRD is removed", func() {
-				By("remove Database CRD", func() {
-					Expect(utils.DeleteObject(env, database)).To(Succeed())
-				})
+			By("verifying new database has been created with the expected fields", func() {
+				primaryPodInfo, err := env.GetClusterPrimary(namespace, clusterName)
+				Expect(err).ToNot(HaveOccurred())
 
-				By("verifying database is still existing", func() {
-					primaryPodInfo, err := env.GetClusterPrimary(namespace, clusterName)
-					Expect(err).ToNot(HaveOccurred())
+				AssertDatabaseExists(primaryPodInfo, dbname, true)
 
-					assertDatabaseExists(namespace, primaryPodInfo.Name, dbname, true)
-				})
+				assertDatabaseHasExpectedFields(namespace, primaryPodInfo.Name, database)
+			})
+
+			By("removing the Database object", func() {
+				Expect(utils.DeleteObject(env, &database)).To(Succeed())
+			})
+
+			By("verifying the retention policy in the postgres database", func() {
+				primaryPodInfo, err := env.GetClusterPrimary(namespace, clusterName)
+				Expect(err).ToNot(HaveOccurred())
+
+				AssertDatabaseExists(primaryPodInfo, dbname, retainOnDeletion)
+			})
+		}
+
+		When("Database CRD reclaim policy is set to delete", func() {
+			It("can manage a declarative database and delete it in Postgres", func() {
+				databaseManifest := fixturesDir +
+					"/declarative_databases/database-with-delete-reclaim-policy.yaml.template"
+				assertTestDeclarativeDatabase(databaseManifest,
+					false)
+			})
+		})
+
+		When("Database CRD reclaim policy is set to retain", func() {
+			It("can manage a declarative database and release it", func() {
+				databaseManifest := fixturesDir + "/declarative_databases/database.yaml.template"
+				assertTestDeclarativeDatabase(databaseManifest, true)
+			})
+		})
+	})
+
+	Context("in a Namespace to be deleted manually", func() {
+		const (
+			namespace = "declarative-db-finalizers"
+		)
+		var (
+			err                error
+			clusterName        string
+			databaseObjectName string
+		)
+		It("will not prevent the deletion of the namespace with lagging finalizers", func() {
+			By("setting up the new namespace and cluster", func() {
+				err = env.CreateNamespace(namespace)
+				Expect(err).ToNot(HaveOccurred())
+
+				clusterName, err = env.GetResourceNameFromYAML(clusterManifest)
+				Expect(err).ToNot(HaveOccurred())
+
+				AssertCreateCluster(namespace, clusterName, clusterManifest, env)
+			})
+			By("creating the database", func() {
+				databaseManifest := fixturesDir +
+					"/declarative_databases/database-with-delete-reclaim-policy.yaml.template"
+				databaseObjectName, err = env.GetResourceNameFromYAML(databaseManifest)
+				Expect(err).NotTo(HaveOccurred())
+				CreateResourceFromFile(namespace, databaseManifest)
+			})
+			By("ensuring the database is reconciled successfully", func() {
+				// get database object
+				dbObj := &apiv1.Database{}
+				databaseNamespacedName := types.NamespacedName{
+					Namespace: namespace,
+					Name:      databaseObjectName,
+				}
+				Eventually(func(g Gomega) {
+					err := env.Client.Get(env.Ctx, databaseNamespacedName, dbObj)
+					g.Expect(err).ToNot(HaveOccurred())
+					g.Expect(dbObj.Status.Ready).Should(BeTrue())
+				}, 300).WithPolling(10 * time.Second).Should(Succeed())
+			})
+			By("deleting the namespace and making sure it succeeds before timeout", func() {
+				err := env.DeleteNamespaceAndWait(namespace, 60)
+				Expect(err).ToNot(HaveOccurred())
 			})
 		})
 	})
