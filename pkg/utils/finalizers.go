1daf37d8046506bf2d0aba2ff80dc10ad552cc91
Jaime Silvela <jaime.silvela@enterprisedb.com>
2024-10-29 18:27:24 +0100

    chore: reconcile CRD Roles
    Signed-off-by: Jaime Silvela <jaime.silvela@enterprisedb.com>
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- pkg/utils/finalizers.go
+++ pkg/utils/finalizers.go
@@ -35,4 +35,8 @@ const (
 	// PluginFinalizerName is the name of the finalizer
 	// triggering the cleanup of a plugin when its service is deleted
 	PluginFinalizerName = MetadataNamespace + "/cleanupPlugin"
+
+	// RoleFinalizerName is the name of the finalizer
+	// triggering the deletion of the role
+	RoleFinalizerName = MetadataNamespace + "/deleteRole"
 )
