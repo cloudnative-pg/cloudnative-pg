91ba85b34387a5f3bfc22b9dd4fe3bc784ae83d9
Armando Ruocco <armando.ruocco@enterprisedb.com>
2024-11-04 16:22:47 +0100

    test: further trim boilerplate
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- internal/management/controller/slots/runner/runner_test.go
+++ internal/management/controller/slots/runner/runner_test.go
@@ -30,24 +30,33 @@ import (
 )
 
 var _ = Describe("Slot synchronization", Ordered, func() {
-	localPodName := "cluster-2"
-	localSlotName := "_cnpg_cluster_2"
-	slot3 := "cluster-3"
-	slot4 := "cluster-4"
-	lsnSlot3 := "0/302C4D8"
-	lsnSlot4 := "0/303C4D8"
+	const (
+		selectPgReplicationSlots = "^SELECT (.+) FROM pg_replication_slots"
+		selectPgSlotAdvance      = "SELECT pg_replication_slot_advance"
 
-	config := apiv1.ReplicationSlotsConfiguration{
-		HighAvailability: &apiv1.ReplicationSlotsHAConfiguration{
-			Enabled:    ptr.To(true),
-			SlotPrefix: "_cnpg_",
-		},
-	}
+		localPodName  = "cluster-2"
+		localSlotName = "_cnpg_cluster_2"
+		slot3         = "cluster-3"
+		slot4         = "cluster-4"
+		lsnSlot3      = "0/302C4D8"
+		lsnSlot4      = "0/303C4D8"
+	)
+
+	var (
+		config = apiv1.ReplicationSlotsConfiguration{
+			HighAvailability: &apiv1.ReplicationSlotsHAConfiguration{
+				Enabled:    ptr.To(true),
+				SlotPrefix: "_cnpg_",
+			},
+		}
+		columns = []string{"slot_name", "slot_type", "active", "restart_lsn", "holds_xmin"}
+	)
 
 	var (
 		dbLocal, dbPrimary     *sql.DB
 		mockLocal, mockPrimary sqlmock.Sqlmock
 	)
+
 	BeforeEach(func() {
 		var err error
 		dbLocal, mockLocal, err = sqlmock.New()
@@ -62,21 +71,21 @@ var _ = Describe("Slot synchronization", Ordered, func() {
 
 	It("can create slots in local from those on primary", func(ctx SpecContext) {
 		// the primary contains slots
-		mockPrimary.ExpectQuery("^SELECT (.+) FROM pg_replication_slots").
-			WillReturnRows(sqlmock.NewRows([]string{"slot_name", "slot_type", "active", "restart_lsn", "holds_xmin"}).
+		mockPrimary.ExpectQuery(selectPgReplicationSlots).
+			WillReturnRows(sqlmock.NewRows(columns).
 				AddRow(localSlotName, string(infrastructure.SlotTypePhysical), true, "0/301C4D8", false).
 				AddRow(slot3, string(infrastructure.SlotTypePhysical), true, lsnSlot3, false).
 				AddRow(slot4, string(infrastructure.SlotTypePhysical), true, lsnSlot4, false))
 
 		// but the local contains none
-		mockLocal.ExpectQuery("^SELECT (.+) FROM pg_replication_slots").
-			WillReturnRows(sqlmock.NewRows([]string{"slot_name", "slot_type", "active", "restart_lsn", "holds_xmin"}))
+		mockLocal.ExpectQuery(selectPgReplicationSlots).
+			WillReturnRows(sqlmock.NewRows(columns))
 
 		mockLocal.ExpectExec("SELECT pg_create_physical_replication_slot").
 			WithArgs(slot3, true).
 			WillReturnResult(sqlmock.NewResult(1, 1))
 
-		mockLocal.ExpectExec("SELECT pg_replication_slot_advance").
+		mockLocal.ExpectExec(selectPgSlotAdvance).
 			WithArgs(slot3, lsnSlot3).
 			WillReturnResult(sqlmock.NewResult(1, 1))
 
@@ -84,7 +93,7 @@ var _ = Describe("Slot synchronization", Ordered, func() {
 			WithArgs(slot4, true).
 			WillReturnResult(sqlmock.NewResult(1, 1))
 
-		mockLocal.ExpectExec("SELECT pg_replication_slot_advance").
+		mockLocal.ExpectExec(selectPgSlotAdvance).
 			WithArgs(slot4, lsnSlot4).
 			WillReturnResult(sqlmock.NewResult(1, 1))
 
@@ -96,21 +105,21 @@ var _ = Describe("Slot synchronization", Ordered, func() {
 		newLSN := "0/308C4D8"
 
 		// Simulate we advance slot3 in primary
-		mockPrimary.ExpectQuery("^SELECT (.+) FROM pg_replication_slots").
-			WillReturnRows(sqlmock.NewRows([]string{"slot_name", "slot_type", "active", "restart_lsn", "holds_xmin"}).
+		mockPrimary.ExpectQuery(selectPgReplicationSlots).
+			WillReturnRows(sqlmock.NewRows(columns).
 				AddRow(localSlotName, string(infrastructure.SlotTypePhysical), true, "0/301C4D8", false).
 				AddRow(slot3, string(infrastructure.SlotTypePhysical), true, newLSN, false).
 				AddRow(slot4, string(infrastructure.SlotTypePhysical), true, lsnSlot4, false))
-			// But local has the old values
-		mockLocal.ExpectQuery("^SELECT (.+) FROM pg_replication_slots").
-			WillReturnRows(sqlmock.NewRows([]string{"slot_name", "slot_type", "active", "restart_lsn", "holds_xmin"}).
+		// But local has the old values
+		mockLocal.ExpectQuery(selectPgReplicationSlots).
+			WillReturnRows(sqlmock.NewRows(columns).
 				AddRow(slot3, string(infrastructure.SlotTypePhysical), true, lsnSlot3, false).
 				AddRow(slot4, string(infrastructure.SlotTypePhysical), true, lsnSlot4, false))
 
-		mockLocal.ExpectExec("SELECT pg_replication_slot_advance").
+		mockLocal.ExpectExec(selectPgSlotAdvance).
 			WithArgs(slot3, newLSN).
 			WillReturnResult(sqlmock.NewResult(1, 1))
-		mockLocal.ExpectExec("SELECT pg_replication_slot_advance").
+		mockLocal.ExpectExec(selectPgSlotAdvance).
 			WithArgs(slot4, lsnSlot4).
 			WillReturnResult(sqlmock.NewResult(1, 1))
 
@@ -120,12 +129,12 @@ var _ = Describe("Slot synchronization", Ordered, func() {
 
 	It("can drop inactive slots in local when they are no longer in primary", func(ctx SpecContext) {
 		// Simulate primary has no longer slot4
-		mockPrimary.ExpectQuery("^SELECT (.+) FROM pg_replication_slots").
-			WillReturnRows(sqlmock.NewRows([]string{"slot_name", "slot_type", "active", "restart_lsn", "holds_xmin"}).
+		mockPrimary.ExpectQuery(selectPgReplicationSlots).
+			WillReturnRows(sqlmock.NewRows(columns).
 				AddRow(localSlotName, string(infrastructure.SlotTypePhysical), true, "0/301C4D8", false))
 		// But local still has it
-		mockLocal.ExpectQuery("^SELECT (.+) FROM pg_replication_slots").
-			WillReturnRows(sqlmock.NewRows([]string{"slot_name", "slot_type", "active", "restart_lsn", "holds_xmin"}).
+		mockLocal.ExpectQuery(selectPgReplicationSlots).
+			WillReturnRows(sqlmock.NewRows(columns).
 				AddRow(slot4, string(infrastructure.SlotTypePhysical), false, lsnSlot4, false))
 
 		mockLocal.ExpectExec("SELECT pg_drop_replication_slot").WithArgs(slot4).
@@ -137,16 +146,16 @@ var _ = Describe("Slot synchronization", Ordered, func() {
 
 	It("can drop slots in local that hold xmin", func(ctx SpecContext) {
 		slotWithXmin := "_cnpg_xmin"
-		mockPrimary.ExpectQuery("^SELECT (.+) FROM pg_replication_slots").
-			WillReturnRows(sqlmock.NewRows([]string{"slot_name", "slot_type", "active", "restart_lsn", "holds_xmin"}).
+		mockPrimary.ExpectQuery(selectPgReplicationSlots).
+			WillReturnRows(sqlmock.NewRows(columns).
 				AddRow(localSlotName, string(infrastructure.SlotTypePhysical), true, "0/301C4D8", false).
 				AddRow(slotWithXmin, string(infrastructure.SlotTypePhysical), true, "0/301C4D8", true))
-		mockLocal.ExpectQuery("^SELECT (.+) FROM pg_replication_slots").
-			WillReturnRows(sqlmock.NewRows([]string{"slot_name", "slot_type", "active", "restart_lsn", "holds_xmin"}).
+		mockLocal.ExpectQuery(selectPgReplicationSlots).
+			WillReturnRows(sqlmock.NewRows(columns).
 				AddRow(localSlotName, string(infrastructure.SlotTypePhysical), true, "0/301C4D8", false).
 				AddRow(slotWithXmin, string(infrastructure.SlotTypePhysical), false, "0/301C4D8", true)) // inactive but with Xmin
 
-		mockLocal.ExpectExec("SELECT pg_replication_slot_advance").WithArgs(slotWithXmin, "0/301C4D8").
+		mockLocal.ExpectExec(selectPgSlotAdvance).WithArgs(slotWithXmin, "0/301C4D8").
 			WillReturnResult(sqlmock.NewResult(1, 1))
 		mockLocal.ExpectExec("SELECT pg_drop_replication_slot").WithArgs(slotWithXmin).
 			WillReturnResult(sqlmock.NewResult(1, 1))
