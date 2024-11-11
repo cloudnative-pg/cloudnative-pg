2377a52c0ec677bb426dae907d50a6507aa36f71
Tao Li <tao.li@enterprisedb.com>
2024-11-11 16:24:09 +0800

    review: happy linter
    Signed-off-by: Tao Li <tao.li@enterprisedb.com>

--- internal/management/controller/roles/contract.go
+++ internal/management/controller/roles/contract.go
@@ -20,10 +20,10 @@ import (
 	"database/sql"
 	"reflect"
 
+	"github.com/cloudnative-pg/machinery/pkg/stringset"
 	"github.com/jackc/pgx/v5/pgtype"
 
 	apiv1 "github.com/cloudnative-pg/cloudnative-pg/api/v1"
-	"github.com/cloudnative-pg/machinery/pkg/stringset"
 )
 
 // DatabaseRole represents the role information read from / written to the Database
