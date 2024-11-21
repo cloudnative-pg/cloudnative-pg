607cb63d9ada6e3fbd6e83a81992e5b82326d0b5
Armando Ruocco <armando.ruocco@enterprisedb.com>
2024-11-21 17:51:16 +0100

    perf(restore): add infomers and local webserver
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- internal/cmd/manager/instance/restore/cmd.go
+++ internal/cmd/manager/instance/restore/cmd.go
@@ -26,12 +26,20 @@ import (
 	"github.com/cloudnative-pg/machinery/pkg/fileutils"
 	"github.com/cloudnative-pg/machinery/pkg/log"
 	"github.com/spf13/cobra"
-	ctrl "sigs.k8s.io/controller-runtime/pkg/client"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/fields"
+	controllerruntime "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
 
+	apiv1 "github.com/cloudnative-pg/cloudnative-pg/api/v1"
 	"github.com/cloudnative-pg/cloudnative-pg/internal/management/istio"
 	"github.com/cloudnative-pg/cloudnative-pg/internal/management/linkerd"
+	"github.com/cloudnative-pg/cloudnative-pg/internal/scheme"
 	"github.com/cloudnative-pg/cloudnative-pg/pkg/management"
 	"github.com/cloudnative-pg/cloudnative-pg/pkg/management/postgres"
+	"github.com/cloudnative-pg/cloudnative-pg/pkg/management/postgres/webserver"
 )
 
 // NewCmd creates the "restore" subcommand
@@ -40,12 +48,30 @@ func NewCmd() *cobra.Command {
 	var namespace string
 	var pgData string
 	var pgWal string
+	var cli client.Client
 
 	cmd := &cobra.Command{
 		Use:           "restore [flags]",
 		SilenceErrors: true,
 		PreRunE: func(cmd *cobra.Command, _ []string) error {
-			return management.WaitForGetCluster(cmd.Context(), ctrl.ObjectKey{
+			ctx := cmd.Context()
+			contextLogger := log.FromContext(ctx)
+			mgr, err := buildLocalWebserverMgr(ctx, clusterName, namespace)
+			if err != nil {
+				contextLogger.Error(err, "while building the manager")
+				return err
+			}
+
+			go func() {
+				if err := mgr.Start(ctx); err != nil {
+					contextLogger.Error(err, "unable to start local webserver manager")
+				}
+			}()
+
+			cli = mgr.GetClient()
+
+			// we will wait this way for the mgr and informers to be online
+			return management.WaitForGetClusterWithClient(cmd.Context(), cli, client.ObjectKey{
 				Name:      clusterName,
 				Namespace: namespace,
 			})
@@ -60,7 +86,7 @@ func NewCmd() *cobra.Command {
 				PgWal:       pgWal,
 			}
 
-			return restoreSubCommand(ctx, info)
+			return restoreSubCommand(ctx, info, cli)
 		},
 		PostRunE: func(cmd *cobra.Command, _ []string) error {
 			if err := istio.TryInvokeQuitEndpoint(cmd.Context()); err != nil {
@@ -81,14 +107,60 @@ func NewCmd() *cobra.Command {
 	return cmd
 }
 
-func restoreSubCommand(ctx context.Context, info postgres.InitInfo) error {
+func buildLocalWebserverMgr(ctx context.Context, clusterName string, namespace string) (manager.Manager, error) {
+	contextLogger := log.FromContext(ctx)
+	runtimeScheme := scheme.BuildWithAllKnownScheme()
+	mgr, err := controllerruntime.NewManager(controllerruntime.GetConfigOrDie(), controllerruntime.Options{
+		Scheme: runtimeScheme,
+		Cache: cache.Options{
+			ByObject: map[client.Object]cache.ByObject{
+				&apiv1.Cluster{}: {
+					Field: fields.OneTermEqualSelector("metadata.name", clusterName),
+					Namespaces: map[string]cache.Config{
+						namespace: {},
+					},
+				},
+			},
+		},
+		Client: client.Options{
+			Cache: &client.CacheOptions{
+				DisableFor: []client.Object{
+					&corev1.Secret{},
+					&corev1.ConfigMap{},
+					// todo(armru): we should remove the backup endpoints from the local webserver
+					&apiv1.Backup{},
+				},
+			},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	localSrv, err := webserver.NewLocalWebServer(
+		postgres.NewInstance().WithClusterName(clusterName).WithNamespace(namespace),
+		mgr.GetClient(),
+		mgr.GetEventRecorderFor("local-webserver"),
+	)
+	if err != nil {
+		return nil, err
+	}
+	if err = mgr.Add(localSrv); err != nil {
+		contextLogger.Error(err, "unable to add local webserver runnable")
+		return nil, err
+	}
+
+	return mgr, nil
+}
+
+func restoreSubCommand(ctx context.Context, info postgres.InitInfo, cli client.Client) error {
 	contextLogger := log.FromContext(ctx)
 	err := info.CheckTargetDataDirectory(ctx)
 	if err != nil {
 		return err
 	}
 
-	err = info.Restore(ctx)
+	err = info.Restore(ctx, cli)
 	if err != nil {
 		contextLogger.Error(err, "Error while restoring a backup")
 		cleanupDataDirectoryIfNeeded(ctx, err, info.PgData)
