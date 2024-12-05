e703d8a856d6448aa4a30a15015839deb2536315
Marco Nenciarini <marco.nenciarini@enterprisedb.com>
2024-12-05 17:05:34 +0100

    fix(bootstrap): clean up the WAL volume before initializing a cluster (#6265)
    This patch ensures that the WAL volume is cleaned during the bootstrap
    as we clean the data directory volume.
    
    Closes #6264
    
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
    Signed-off-by: Jaime Silvela <jaime.silvela@enterprisedb.com>
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    Co-authored-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
    Co-authored-by: Jaime Silvela <jaime.silvela@enterprisedb.com>
    Co-authored-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    (cherry picked from commit ddb8b36ee33b180c3b67b9198161a1b804c1276f)

--- internal/cmd/manager/instance/initdb/cmd.go
+++ internal/cmd/manager/instance/initdb/cmd.go
@@ -150,7 +150,7 @@ func NewCmd() *cobra.Command {
 
 func initSubCommand(ctx context.Context, info postgres.InitInfo) error {
 	contextLogger := log.FromContext(ctx)
-	err := info.CheckTargetDataDirectory(ctx)
+	err := info.EnsureTargetDirectoriesDoNotExist(ctx)
 	if err != nil {
 		return err
 	}
