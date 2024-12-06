afcfaf39bac93801242c097b9f558186752e425b
Abhishek Chanda <achanda@users.noreply.github.com>
2024-12-06 11:13:47 -0600

    test: make sure we test port correctness for services (#4934)
    Signed-off-by: Abhishek Chanda <abhishek.becs@gmail.com>
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    Co-authored-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    Co-authored-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    (cherry picked from commit fd62a1c01e7eb1a9effeb6a0773f36f305b10fa1)

--- pkg/specs/services_test.go
+++ pkg/specs/services_test.go
@@ -17,9 +17,12 @@ limitations under the License.
 package specs
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 
 	apiv1 "github.com/cloudnative-pg/cloudnative-pg/api/v1"
+	"github.com/cloudnative-pg/cloudnative-pg/pkg/postgres"
 	"github.com/cloudnative-pg/cloudnative-pg/pkg/utils"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -32,6 +35,12 @@ var _ = Describe("Services specification", func() {
 			Name: "clustername",
 		},
 	}
+	expectedPort := corev1.ServicePort{
+		Name:       PostgresContainerName,
+		Protocol:   corev1.ProtocolTCP,
+		TargetPort: intstr.FromInt32(postgres.ServerPort),
+		Port:       postgres.ServerPort,
+	}
 
 	It("create a configured -any service", func() {
 		service := CreateClusterAnyService(postgresql)
@@ -39,6 +48,8 @@ var _ = Describe("Services specification", func() {
 		Expect(service.Spec.PublishNotReadyAddresses).To(BeTrue())
 		Expect(service.Spec.Selector[utils.ClusterLabelName]).To(Equal("clustername"))
 		Expect(service.Spec.Selector[utils.PodRoleLabelName]).To(Equal(string(utils.PodRoleInstance)))
+		Expect(service.Spec.Ports).To(HaveLen(1))
+		Expect(service.Spec.Ports).To(ContainElement(expectedPort))
 	})
 
 	It("create a configured -r service", func() {
@@ -47,6 +58,8 @@ var _ = Describe("Services specification", func() {
 		Expect(service.Spec.PublishNotReadyAddresses).To(BeFalse())
 		Expect(service.Spec.Selector[utils.ClusterLabelName]).To(Equal("clustername"))
 		Expect(service.Spec.Selector[utils.PodRoleLabelName]).To(Equal(string(utils.PodRoleInstance)))
+		Expect(service.Spec.Ports).To(HaveLen(1))
+		Expect(service.Spec.Ports).To(ContainElement(expectedPort))
 	})
 
 	It("create a configured -ro service", func() {
@@ -55,6 +68,8 @@ var _ = Describe("Services specification", func() {
 		Expect(service.Spec.PublishNotReadyAddresses).To(BeFalse())
 		Expect(service.Spec.Selector[utils.ClusterLabelName]).To(Equal("clustername"))
 		Expect(service.Spec.Selector[utils.ClusterRoleLabelName]).To(Equal(ClusterRoleLabelReplica))
+		Expect(service.Spec.Ports).To(HaveLen(1))
+		Expect(service.Spec.Ports).To(ContainElement(expectedPort))
 	})
 
 	It("create a configured -rw service", func() {
@@ -63,5 +78,7 @@ var _ = Describe("Services specification", func() {
 		Expect(service.Spec.PublishNotReadyAddresses).To(BeFalse())
 		Expect(service.Spec.Selector[utils.ClusterLabelName]).To(Equal("clustername"))
 		Expect(service.Spec.Selector[utils.ClusterRoleLabelName]).To(Equal(ClusterRoleLabelPrimary))
+		Expect(service.Spec.Ports).To(HaveLen(1))
+		Expect(service.Spec.Ports).To(ContainElement(expectedPort))
 	})
 })
