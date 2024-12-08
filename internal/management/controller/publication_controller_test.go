cc5595c5369b06beab22b48449f11562ed052e21
Jaime Silvela <jaime.silvela@enterprisedb.com>
2024-12-08 18:10:14 +0100

    chore: complete the publication controller unit tests
    Signed-off-by: Jaime Silvela <jaime.silvela@enterprisedb.com>

--- internal/management/controller/publication_controller_test.go
+++ internal/management/controller/publication_controller_test.go
@@ -23,9 +23,9 @@ import (
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/jackc/pgx/v5"
 	corev1 "k8s.io/api/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
@@ -169,265 +169,170 @@ var _ = Describe("Managed publication controller tests", func() {
 		)
 	})
 
-	It("on deletion it removes finalizers and drops publication", func(ctx SpecContext) {
-		Expect(publication.Finalizers).To(BeEmpty())
+	When("retention policy is delete", func() {
+		It("on deletion it removes finalizers and drops the Publication", func(ctx SpecContext) {
+			assertObjectReconciledAfterDeletion(ctx, r, publication, &apiv1.Publication{}, fakeClient,
+				func() {
+					// Mocking DetectDB
+					expectedValue := sqlmock.NewRows([]string{""}).AddRow("0")
+					dbMock.ExpectQuery(publicationDetectionQuery).WithArgs(publication.Spec.Name).
+						WillReturnRows(expectedValue)
 
-		// Mocking Detect
-		noHits := sqlmock.NewRows([]string{""}).AddRow("0")
-		dbMock.ExpectQuery(publicationDetectionQuery).WithArgs(publication.Spec.Name).
-			WillReturnRows(noHits)
+					// Mocking CreateDB
+					expectedCreate := sqlmock.NewResult(0, 1)
+					expectedQuery := fmt.Sprintf(
+						"CREATE PUBLICATION %s FOR ALL TABLES",
+						pgx.Identifier{publication.Spec.Name}.Sanitize(),
+					)
+					dbMock.ExpectExec(expectedQuery).WillReturnResult(expectedCreate)
 
-		// Mocking CreatePublication
-		expectedCreate := sqlmock.NewResult(0, 1)
-		expectedQuery := fmt.Sprintf(
-			"CREATE PUBLICATION %s FOR ALL TABLES",
-			pgx.Identifier{publication.Spec.Name}.Sanitize(),
-		)
-		dbMock.ExpectExec(expectedQuery).WillReturnResult(expectedCreate)
-
-		// Reconcile and get the updated object
-		_, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: types.NamespacedName{
-			Namespace: publication.Namespace,
-			Name:      publication.Name,
-		}})
-		Expect(err).ToNot(HaveOccurred())
-
-		var updatedPublication apiv1.Publication
-		err = fakeClient.Get(ctx, client.ObjectKey{
-			Namespace: publication.Namespace,
-			Name:      publication.Name,
-		}, &updatedPublication)
-		Expect(err).ToNot(HaveOccurred())
-
-		Expect(updatedPublication.Status.Applied).Should(HaveValue(BeTrue()))
-		Expect(updatedPublication.Status.Message).Should(BeEmpty())
-		Expect(updatedPublication.Finalizers).NotTo(BeEmpty())
-
-		// the next 3 lines are a hacky bit to make sure the next reconciler
-		// call doesn't skip on account of Generation == ObservedGeneration.
-		// See fake.Client known issues with `Generation`
-		// https://pkg.go.dev/sigs.k8s.io/controller-runtime/pkg/client/fake@v0.19.0#NewClientBuilder
-		currentPublication := updatedPublication.DeepCopy()
-		updatedPublication.Status.ObservedGeneration = 2
-		Expect(fakeClient.Status().Patch(ctx, &updatedPublication, client.MergeFrom(currentPublication))).To(Succeed())
-
-		// We now look at the behavior when we delete the Publication object
-		Expect(fakeClient.Delete(ctx, publication)).To(Succeed())
-
-		// the Publication object is Deleted, but its finalizer prevents removal from
-		// the API
-		var fadingPublication apiv1.Publication
-		err = fakeClient.Get(ctx, client.ObjectKey{
-			Namespace: publication.Namespace,
-			Name:      publication.Name,
-		}, &fadingPublication)
-		Expect(err).ToNot(HaveOccurred())
-		Expect(fadingPublication.DeletionTimestamp).NotTo(BeZero())
-		Expect(fadingPublication.Finalizers).NotTo(BeEmpty())
-
-		// Mocking Drop Publication
-		expectedDrop := fmt.Sprintf("DROP PUBLICATION IF EXISTS %s",
-			pgx.Identifier{publication.Spec.Name}.Sanitize(),
-		)
-		dbMock.ExpectExec(expectedDrop).WillReturnResult(sqlmock.NewResult(0, 1))
-
-		// Reconcile and get the updated object
-		_, err = r.Reconcile(ctx, ctrl.Request{NamespacedName: types.NamespacedName{
-			Namespace: publication.Namespace,
-			Name:      publication.Name,
-		}})
-		Expect(err).ToNot(HaveOccurred())
-
-		var finalPublication apiv1.Publication
-		err = fakeClient.Get(ctx, client.ObjectKey{
-			Namespace: publication.Namespace,
-			Name:      publication.Name,
-		}, &finalPublication)
-		Expect(err).To(HaveOccurred())
-		Expect(apierrors.IsNotFound(err)).To(BeTrue())
+					// Mocking Drop Publication
+					expectedDrop := fmt.Sprintf("DROP PUBLICATION IF EXISTS %s",
+						pgx.Identifier{publication.Spec.Name}.Sanitize(),
+					)
+					dbMock.ExpectExec(expectedDrop).WillReturnResult(sqlmock.NewResult(0, 1))
+				},
+			)
+		})
 	})
 
-	// It("fails reconciliation if cluster isn't found (deleted cluster)", func(ctx SpecContext) {
-	// 	// since the fakeClient has the `cluster-example` cluster, let's reference
-	// 	// another cluster `cluster-other` that is not found by the fakeClient
-	// 	pgInstance := postgres.NewInstance().
-	// 		WithNamespace("default").
-	// 		WithPodName("cluster-other-1").
-	// 		WithClusterName("cluster-other")
+	When("retention policy is retain", func() {
+		It("on deletion it removes finalizers and does NOT drop the Publication", func(ctx SpecContext) {
+			publication.Spec.ReclaimPolicy = apiv1.PublicationReclaimRetain
+			Expect(fakeClient.Update(ctx, publication)).To(Succeed())
 
-	// 	f := fakeInstanceData{
-	// 		Instance: pgInstance,
-	// 		db:       db,
-	// 	}
+			assertObjectReconciledAfterDeletion(ctx, r, publication, &apiv1.Publication{}, fakeClient,
+				func() {
+					// Mocking DetectDB
+					expectedValue := sqlmock.NewRows([]string{""}).AddRow("0")
+					dbMock.ExpectQuery(publicationDetectionQuery).WithArgs(publication.Spec.Name).
+						WillReturnRows(expectedValue)
 
-	// 	r = &DatabaseReconciler{
-	// 		Client:   fakeClient,
-	// 		Scheme:   schemeBuilder.BuildWithAllKnownScheme(),
-	// 		instance: &f,
-	// 	}
+					// Mocking CreateDB
+					expectedCreate := sqlmock.NewResult(0, 1)
+					expectedQuery := fmt.Sprintf(
+						"CREATE PUBLICATION %s FOR ALL TABLES",
+						pgx.Identifier{publication.Spec.Name}.Sanitize(),
+					)
+					dbMock.ExpectExec(expectedQuery).WillReturnResult(expectedCreate)
+				},
+			)
+		})
+	})
 
-	// 	// patching the Database object to reference the newly created Cluster
-	// 	originalDatabase := publication.DeepCopy()
-	// 	publication.Spec.ClusterRef.Name = "cluster-other"
-	// 	Expect(fakeClient.Patch(ctx, publication, client.MergeFrom(originalDatabase))).To(Succeed())
+	It("fails reconciliation if cluster isn't found (deleted cluster)", func(ctx SpecContext) {
+		// since the fakeClient has the `cluster-example` cluster, let's reference
+		// another cluster `cluster-other` that is not found by the fakeClient
+		pgInstance := postgres.NewInstance().
+			WithNamespace("default").
+			WithPodName("cluster-other-1").
+			WithClusterName("cluster-other")
 
-	// 	// Reconcile and get the updated object
-	// 	_, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: types.NamespacedName{
-	// 		Namespace: publication.Namespace,
-	// 		Name:      publication.Name,
-	// 	}})
-	// 	Expect(err).ToNot(HaveOccurred())
+		f := fakeInstanceData{
+			Instance: pgInstance,
+			db:       db,
+		}
 
-	// 	var updatedDatabase apiv1.Database
-	// 	err = fakeClient.Get(ctx, client.ObjectKey{
-	// 		Namespace: publication.Namespace,
-	// 		Name:      publication.Name,
-	// 	}, &updatedDatabase)
-	// 	Expect(err).ToNot(HaveOccurred())
+		r = &PublicationReconciler{
+			Client:   fakeClient,
+			Scheme:   schemeBuilder.BuildWithAllKnownScheme(),
+			instance: &f,
+		}
 
-	// 	Expect(updatedDatabase.Status.Applied).Should(HaveValue(BeFalse()))
-	// 	Expect(updatedDatabase.Status.Message).Should(ContainSubstring(`"cluster-other" not found`))
-	// })
+		// updating the Database object to reference the newly created Cluster
+		publication.Spec.ClusterRef.Name = "cluster-other"
+		Expect(fakeClient.Update(ctx, publication)).To(Succeed())
 
-	// It("skips reconciliation if database object isn't found (deleted database)", func(ctx SpecContext) {
-	// 	// Initialize a new Database but without creating it in the K8S Cluster
-	// 	otherDatabase := &apiv1.Database{
-	// 		ObjectMeta: metav1.ObjectMeta{
-	// 			Name:       "db-other",
-	// 			Namespace:  "default",
-	// 			Generation: 1,
-	// 		},
-	// 		Spec: apiv1.DatabaseSpec{
-	// 			ClusterRef: corev1.LocalObjectReference{
-	// 				Name: cluster.Name,
-	// 			},
-	// 			Name:  "db-one",
-	// 			Owner: "app",
-	// 		},
-	// 	}
+		assertObjectWasReconciled(ctx, r, publication, &apiv1.Publication{}, fakeClient,
+			func() {
+				// no interactions expected with Postgres
+			},
+			func(updatedPublication *apiv1.Publication) {
+				Expect(updatedPublication.Status.Applied).Should(HaveValue(BeFalse()))
+				Expect(updatedPublication.Status.Message).Should(ContainSubstring(`"cluster-other" not found`))
+			},
+		)
+	})
 
-	// 	// Reconcile the database that hasn't been created in the K8S Cluster
-	// 	result, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: types.NamespacedName{
-	// 		Namespace: otherDatabase.Namespace,
-	// 		Name:      otherDatabase.Name,
-	// 	}})
+	It("skips reconciliation if publication object isn't found (deleted publication)", func(ctx SpecContext) {
+		// Initialize a new Publication but without creating it in the K8S Cluster
+		otherPublication := &apiv1.Publication{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:       "db-other",
+				Namespace:  "default",
+				Generation: 1,
+			},
+			Spec: apiv1.PublicationSpec{
+				ClusterRef: corev1.LocalObjectReference{
+					Name: cluster.Name,
+				},
+				Name: "db-one",
+			},
+		}
 
-	// 	// Expect the reconciler to exit silently since the object doesn't exist
-	// 	Expect(err).ToNot(HaveOccurred())
-	// 	Expect(result).Should(BeZero()) // nothing to do, since the DB is being deleted
-	// })
+		// Reconcile the publication that hasn't been created in the K8S Cluster
+		result, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: types.NamespacedName{
+			Namespace: otherPublication.Namespace,
+			Name:      otherPublication.Name,
+		}})
 
-	// It("drops database with ensure absent option", func(ctx SpecContext) {
-	// 	// Mocking dropDatabase
-	// 	expectedValue := sqlmock.NewResult(0, 1)
-	// 	expectedQuery := fmt.Sprintf(
-	// 		"DROP DATABASE IF EXISTS %s",
-	// 		pgx.Identifier{publication.Spec.Name}.Sanitize(),
-	// 	)
-	// 	dbMock.ExpectExec(expectedQuery).WillReturnResult(expectedValue)
+		// Expect the reconciler to exit silently since the object doesn't exist
+		Expect(err).ToNot(HaveOccurred())
+		Expect(result).Should(BeZero()) // nothing to do, since the DB is being deleted
+	})
 
-	// 	// Update the obj to set EnsureAbsent
-	// 	publication.Spec.Ensure = apiv1.EnsureAbsent
-	// 	Expect(fakeClient.Update(ctx, publication)).To(Succeed())
+	It("marks as failed if the target publication is already being managed", func(ctx SpecContext) {
+		// Let's force the publication to have a past reconciliation
+		publication.SetObservedGeneration(2)
+		Expect(fakeClient.Status().Update(ctx, publication)).To(Succeed())
 
-	// 	// Reconcile and get the updated object
-	// 	_, err = r.Reconcile(ctx, ctrl.Request{NamespacedName: types.NamespacedName{
-	// 		Namespace: publication.Namespace,
-	// 		Name:      publication.Name,
-	// 	}})
-	// 	Expect(err).ToNot(HaveOccurred())
+		// A new Publication Object targeting the same "pub-all"
+		pubDuplicate := &apiv1.Publication{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:       "pub-duplicate",
+				Namespace:  "default",
+				Generation: 1,
+			},
+			Spec: apiv1.PublicationSpec{
+				ClusterRef: corev1.LocalObjectReference{
+					Name: cluster.Name,
+				},
+				Name: "pub-all",
+			},
+		}
 
-	// 	err = fakeClient.Get(ctx, client.ObjectKey{
-	// 		Namespace: publication.Namespace,
-	// 		Name:      publication.Name,
-	// 	}, publication)
-	// 	Expect(err).ToNot(HaveOccurred())
+		// Expect(fakeClient.Create(ctx, currentManager)).To(Succeed())
+		Expect(fakeClient.Create(ctx, pubDuplicate)).To(Succeed())
 
-	// 	Expect(publication.Status.Applied).To(HaveValue(BeTrue()))
-	// 	Expect(publication.Status.Message).To(BeEmpty())
-	// 	Expect(publication.Status.ObservedGeneration).To(BeEquivalentTo(1))
-	// })
+		assertObjectWasReconciled(ctx, r, pubDuplicate, &apiv1.Publication{}, fakeClient,
+			func() {
+				// No interactions expected with Postgres
+			},
+			func(updatedPublication *apiv1.Publication) {
+				expectedError := fmt.Sprintf("%q is already managed by object %q",
+					pubDuplicate.Spec.Name, publication.Name)
+				Expect(updatedPublication.Status.Applied).To(HaveValue(BeFalse()))
+				Expect(updatedPublication.Status.Message).To(ContainSubstring(expectedError))
+				Expect(updatedPublication.Status.ObservedGeneration).To(BeZero())
+			},
+		)
+	})
 
-	// It("marks as failed if the target Database is already being managed", func(ctx SpecContext) {
-	// 	// The Database obj currently managing "test-database"
-	// 	currentManager := &apiv1.Database{
-	// 		ObjectMeta: metav1.ObjectMeta{
-	// 			Name:      "current-manager",
-	// 			Namespace: "default",
-	// 		},
-	// 		Spec: apiv1.DatabaseSpec{
-	// 			ClusterRef: corev1.LocalObjectReference{
-	// 				Name: cluster.Name,
-	// 			},
-	// 			Name:  "test-database",
-	// 			Owner: "app",
-	// 		},
-	// 		Status: apiv1.DatabaseStatus{
-	// 			Applied:            ptr.To(true),
-	// 			ObservedGeneration: 1,
-	// 		},
-	// 	}
+	It("properly signals a database is on a replica cluster", func(ctx SpecContext) {
+		initialCluster := cluster.DeepCopy()
+		cluster.Spec.ReplicaCluster = &apiv1.ReplicaClusterConfiguration{
+			Enabled: ptr.To(true),
+		}
+		Expect(fakeClient.Patch(ctx, cluster, client.MergeFrom(initialCluster))).To(Succeed())
 
-	// 	// A new Database Object targeting the same "test-database"
-	// 	dbDuplicate := &apiv1.Database{
-	// 		ObjectMeta: metav1.ObjectMeta{
-	// 			Name:       "db-duplicate",
-	// 			Namespace:  "default",
-	// 			Generation: 1,
-	// 		},
-	// 		Spec: apiv1.DatabaseSpec{
-	// 			ClusterRef: corev1.LocalObjectReference{
-	// 				Name: cluster.Name,
-	// 			},
-	// 			Name:  "test-database",
-	// 			Owner: "app",
-	// 		},
-	// 	}
-
-	// 	Expect(fakeClient.Create(ctx, currentManager)).To(Succeed())
-	// 	Expect(fakeClient.Create(ctx, dbDuplicate)).To(Succeed())
-
-	// 	// Reconcile and get the updated object
-	// 	_, err = r.Reconcile(ctx, ctrl.Request{NamespacedName: types.NamespacedName{
-	// 		Namespace: dbDuplicate.Namespace,
-	// 		Name:      dbDuplicate.Name,
-	// 	}})
-	// 	Expect(err).ToNot(HaveOccurred())
-
-	// 	err = fakeClient.Get(ctx, client.ObjectKey{
-	// 		Namespace: dbDuplicate.Namespace,
-	// 		Name:      dbDuplicate.Name,
-	// 	}, dbDuplicate)
-	// 	Expect(err).ToNot(HaveOccurred())
-
-	// 	expectedError := fmt.Sprintf("database %q is already managed by Database object %q",
-	// 		dbDuplicate.Spec.Name, currentManager.Name)
-	// 	Expect(dbDuplicate.Status.Applied).To(HaveValue(BeFalse()))
-	// 	Expect(dbDuplicate.Status.Message).To(ContainSubstring(expectedError))
-	// 	Expect(dbDuplicate.Status.ObservedGeneration).To(BeZero())
-	// })
-
-	// It("properly signals a database is on a replica cluster", func(ctx SpecContext) {
-	// 	initialCluster := cluster.DeepCopy()
-	// 	cluster.Spec.ReplicaCluster = &apiv1.ReplicaClusterConfiguration{
-	// 		Enabled: ptr.To(true),
-	// 	}
-	// 	Expect(fakeClient.Patch(ctx, cluster, client.MergeFrom(initialCluster))).To(Succeed())
-
-	// 	_, err := r.Reconcile(ctx, ctrl.Request{NamespacedName: types.NamespacedName{
-	// 		Namespace: publication.Namespace,
-	// 		Name:      publication.Spec.Name,
-	// 	}})
-	// 	Expect(err).ToNot(HaveOccurred())
-
-	// 	var updatedDatabase apiv1.Database
-	// 	err = fakeClient.Get(ctx, client.ObjectKey{
-	// 		Namespace: publication.Namespace,
-	// 		Name:      publication.Name,
-	// 	}, &updatedDatabase)
-	// 	Expect(err).ToNot(HaveOccurred())
-
-	// 	Expect(updatedDatabase.Status.Applied).Should(BeNil())
-	// 	Expect(updatedDatabase.Status.Message).Should(ContainSubstring("waiting for the cluster to become primary"))
-	// })
+		assertObjectWasReconciled(ctx, r, publication, &apiv1.Publication{}, fakeClient,
+			func() {
+				// No interactions expected with Postgres
+			},
+			func(updatedPublication *apiv1.Publication) {
+				Expect(updatedPublication.Status.Applied).Should(BeNil())
+				Expect(updatedPublication.Status.Message).Should(ContainSubstring("waiting for the cluster to become primary"))
+			},
+		)
+	})
 })
