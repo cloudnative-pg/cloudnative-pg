f10dea32485e7b6ecb2f873a37dc99730842f9a8
Jaime Silvela <jaime.silvela@enterprisedb.com>
2024-12-18 15:27:19 +0100

    fix: key collision in structured logs (#6324)
    Inside the logs we found the name `name` as a key for a couple of values, now we
    assign the proper keys like `jobName` and `podName`
    
    Closes #6321
    
    Signed-off-by: Jaime Silvela <jaime.silvela@enterprisedb.com>
    (cherry picked from commit e22e7e9fdf7ee3e63d425a428b538fe76b8bbe22)

--- pkg/resources/instance/client.go
+++ pkg/resources/instance/client.go
@@ -175,7 +175,7 @@ func (r *statusClient) GetStatusFromInstances(
 	for idx := range status.Items {
 		if status.Items[idx].Error != nil {
 			log.FromContext(ctx).Info("Cannot extract Pod status",
-				"name", status.Items[idx].Pod.Name,
+				"podName", status.Items[idx].Pod.Name,
 				"error", status.Items[idx].Error.Error())
 		}
 	}
