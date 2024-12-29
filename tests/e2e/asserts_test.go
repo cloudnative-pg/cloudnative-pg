4c9f5adcf779535d3f88ff8564dd6e69702b4bc2
Marco Nenciarini <marco.nenciarini@enterprisedb.com>
2024-12-29 15:33:52 +0100

    fix: removed e2e weird logic
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>

--- tests/e2e/asserts_test.go
+++ tests/e2e/asserts_test.go
@@ -297,7 +297,6 @@ func AssertClusterIsReady(namespace string, clusterName string, timeout int, env
 func AssertClusterDefault(
 	namespace string,
 	clusterName string,
-	isExpectedToDefault bool,
 	env *testsUtils.TestingEnvironment,
 ) {
 	By("having a Cluster object populated with default values", func() {
@@ -315,11 +314,7 @@ func AssertClusterDefault(
 		validator := webhookv1.ClusterCustomValidator{}
 		validationWarn, validationErr := validator.ValidateCreate(env.Ctx, cluster)
 		Expect(validationWarn).To(BeEmpty())
-		if isExpectedToDefault {
-			Expect(validationErr).Should(BeEmpty(), validationErr)
-		} else {
-			Expect(validationErr).ShouldNot(BeEmpty(), validationErr)
-		}
+		Expect(validationErr).ToNot(HaveOccurred())
 	})
 }
 
