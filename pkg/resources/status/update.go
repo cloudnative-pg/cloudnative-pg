e6dc522dc5d56ed0cd7c84bba906da2e0f32da16
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2025-01-08 14:18:53 +0100

    chore: typo in pkg/resources/status/update.go
    Co-authored-by: Gabriele Quaresima <gabriele.quaresima@enterprisedb.com>
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>

--- pkg/resources/status/update.go
+++ pkg/resources/status/update.go
@@ -29,7 +29,7 @@ import (
 )
 
 // PatchWithOptimisticLock updates the status of the cluster using the passed
-// transaction functions (in the order passed).
+// transaction functions (in the given order).
 // Important: after successfully updating the status, this
 // function refreshes it into the passed cluster
 func PatchWithOptimisticLock(
