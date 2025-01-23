f8a7324d9b9f224e9fdea56277e2e7cf5be0554f
Leonardo Cecchi <leonardo.cecchi@gmail.com>
2025-01-23 16:15:06 +0100

    wip: fix e2e tests
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@gmail.com>

--- api/v1/cluster_funcs.go
+++ api/v1/cluster_funcs.go
@@ -1476,3 +1476,13 @@ func (p *Probe) ApplyInto(k8sProbe *corev1.Probe) {
 		k8sProbe.TerminationGracePeriodSeconds = p.TerminationGracePeriodSeconds
 	}
 }
+
+// ApplyInto applies the content of the probe configuration in a Kubernetes
+// probe
+func (p *StartupProbe) ApplyInto(k8sProbe *corev1.Probe) {
+	if p == nil {
+		return
+	}
+
+	p.Probe.ApplyInto(k8sProbe)
+}
