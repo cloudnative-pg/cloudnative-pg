afd1f386b48a402d747297c4fca147395888b1f7
Armando Ruocco <armando.ruocco@enterprisedb.com>
2025-02-19 17:39:31 +0100

    refactor: simplify configparser by removing redundant logic (#6739)
    This patch refactors the configparser to remove redundant logic and
    improve code clarity. It eliminates unnecessary indirections. No
    functional changes are introduced, ensuring compatibility with existing
    behavior.
    
    Closes #6739
    
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    Co-authored-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    (cherry picked from commit 36cb5188264d14509e14c6d583d111606b8bebdc)

--- internal/configuration/configuration.go
+++ internal/configuration/configuration.go
@@ -127,7 +127,7 @@ func NewConfiguration() *Data {
 
 // ReadConfigMap reads the configuration from the environment and the passed in data map
 func (config *Data) ReadConfigMap(data map[string]string) {
-	configparser.ReadConfigMap(config, newDefaultConfig(), data, configparser.OsEnvironment{})
+	configparser.ReadConfigMap(config, newDefaultConfig(), data)
 }
 
 // IsAnnotationInherited checks if an annotation with a certain name should
