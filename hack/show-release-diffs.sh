b14d634604bd58f92d3c20fc5cd95e662ed91253
Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>
2025-03-26 07:36:12 +0100

    feat(ip): assign copyright to the Linux Foundation (#7203)
    Adopt the new attribution information for contributions to
    CloudNativePG:
    
        Copyright © contributors to CloudNativePG, established as
        CloudNativePG a Series of LF Projects, LLC.
    
    Adopt the SPDX format for Apache License 2.0
    
    Closes #7202
    
    Signed-off-by: Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    Co-authored-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    (cherry picked from commit 5c51b9aa3942db2cae133884dcd17b56de56f65d)

--- hack/show-release-diffs.sh
+++ hack/show-release-diffs.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 ##
+## Copyright © contributors to CloudNativePG, established as
+## CloudNativePG a Series of LF Projects, LLC.
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+##
+## SPDX-License-Identifier: Apache-2.0
+##
+
 ## CloudNativePG - Show diffs from main for a release branch
 ##
 ## This is a helper script that prints the GitHub pull requests
@@ -19,21 +37,6 @@
 ## This example compares the current branch with 1.15:
 ##
 ##     ./hack/show-release-diffs.sh release-1.15
-##
-## Copyright The CloudNativePG Contributors
-##
-## Licensed under the Apache License, Version 2.0 (the "License");
-## you may not use this file except in compliance with the License.
-## You may obtain a copy of the License at
-##
-##     http://www.apache.org/licenses/LICENSE-2.0
-##
-## Unless required by applicable law or agreed to in writing, software
-## distributed under the License is distributed on an "AS IS" BASIS,
-## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-## See the License for the specific language governing permissions and
-## limitations under the License.
-##
 
 set -o errexit -o nounset -o pipefail
 
