90650e4931fb616d3bda86aebe3b3a8aa20d1b2c
Niccolò Fei <niccolo.fei@enterprisedb.com>
2025-04-08 18:05:02 +0200

    ci: use standard images
    Signed-off-by: Niccolò Fei <niccolo.fei@enterprisedb.com>

--- tests/e2e/cluster_major_upgrade_test.go
+++ tests/e2e/cluster_major_upgrade_test.go
@@ -111,14 +111,10 @@ var _ = Describe("Postgres Major Upgrade", Label(tests.LabelPostgresMajorUpgrade
 
 	generatePostgreSQLCluster := func(namespace string, storageClass string, majorVersion int) *v1.Cluster {
 		cluster := generateBaseCluster(namespace, storageClass)
-		cluster.Spec.ImageName = "ghcr.io/cloudnative-pg/postgresql:" + strconv.Itoa(majorVersion)
-		cluster.Spec.Bootstrap = &v1.BootstrapConfiguration{
-			InitDB: &v1.BootstrapInitDB{
-				PostInitSQL: []string{
-					"CREATE EXTENSION IF NOT EXISTS pg_stat_statements;",
-					"CREATE EXTENSION IF NOT EXISTS pg_trgm;",
-				},
-			},
+		cluster.Spec.ImageName = fmt.Sprintf("ghcr.io/cloudnative-pg/postgresql:%d-standard-bookworm", majorVersion)
+		cluster.Spec.Bootstrap.InitDB.PostInitSQL = []string{
+			"CREATE EXTENSION IF NOT EXISTS pg_stat_statements;",
+			"CREATE EXTENSION IF NOT EXISTS pg_trgm;",
 		}
 		cluster.Spec.PostgresConfiguration.Parameters["pg_stat_statements.track"] = "top"
 		return cluster
@@ -132,26 +128,22 @@ var _ = Describe("Postgres Major Upgrade", Label(tests.LabelPostgresMajorUpgrade
 	generatePostGISCluster := func(namespace string, storageClass string, majorVersion int) *v1.Cluster {
 		cluster := generateBaseCluster(namespace, storageClass)
 		cluster.Spec.ImageName = "ghcr.io/cloudnative-pg/postgis:" + strconv.Itoa(majorVersion)
-		cluster.Spec.Bootstrap = &v1.BootstrapConfiguration{
-			InitDB: &v1.BootstrapInitDB{
-				PostInitApplicationSQL: []string{
-					"CREATE EXTENSION postgis",
-					"CREATE EXTENSION postgis_raster",
-					"CREATE EXTENSION postgis_sfcgal",
-					"CREATE EXTENSION fuzzystrmatch",
-					"CREATE EXTENSION address_standardizer",
-					"CREATE EXTENSION address_standardizer_data_us",
-					"CREATE EXTENSION postgis_tiger_geocoder",
-					"CREATE EXTENSION postgis_topology",
-					"CREATE TABLE geometries (name varchar, geom geometry)",
-					"INSERT INTO geometries VALUES" +
-						" ('Point', 'POINT(0 0)')," +
-						" ('Linestring', 'LINESTRING(0 0, 1 1, 2 1, 2 2)')," +
-						" ('Polygon', 'POLYGON((0 0, 1 0, 1 1, 0 1, 0 0))')," +
-						" ('PolygonWithHole', 'POLYGON((0 0, 10 0, 10 10, 0 10, 0 0),(1 1, 1 2, 2 2, 2 1, 1 1))')," +
-						" ('Collection', 'GEOMETRYCOLLECTION(POINT(2 0),POLYGON((0 0, 1 0, 1 1, 0 1, 0 0)))');",
-				},
-			},
+		cluster.Spec.Bootstrap.InitDB.PostInitApplicationSQL = []string{
+			"CREATE EXTENSION postgis",
+			"CREATE EXTENSION postgis_raster",
+			"CREATE EXTENSION postgis_sfcgal",
+			"CREATE EXTENSION fuzzystrmatch",
+			"CREATE EXTENSION address_standardizer",
+			"CREATE EXTENSION address_standardizer_data_us",
+			"CREATE EXTENSION postgis_tiger_geocoder",
+			"CREATE EXTENSION postgis_topology",
+			"CREATE TABLE geometries (name varchar, geom geometry)",
+			"INSERT INTO geometries VALUES" +
+				" ('Point', 'POINT(0 0)')," +
+				" ('Linestring', 'LINESTRING(0 0, 1 1, 2 1, 2 2)')," +
+				" ('Polygon', 'POLYGON((0 0, 1 0, 1 1, 0 1, 0 0))')," +
+				" ('PolygonWithHole', 'POLYGON((0 0, 10 0, 10 10, 0 10, 0 0),(1 1, 1 2, 2 2, 2 1, 1 1))')," +
+				" ('Collection', 'GEOMETRYCOLLECTION(POINT(2 0),POLYGON((0 0, 1 0, 1 1, 0 1, 0 0)))');",
 		}
 		return cluster
 	}
@@ -183,7 +175,7 @@ var _ = Describe("Postgres Major Upgrade", Label(tests.LabelPostgresMajorUpgrade
 		// Default target Images
 		targetImages := map[string]string{
 			postgisEntry:           fmt.Sprintf("%v:%v", postgres.PostgisImageRepository, targetMajor),
-			postgresqlEntry:        fmt.Sprintf("%v:%v", postgres.ImageRepository, targetMajor),
+			postgresqlEntry:        fmt.Sprintf("%v:%v-standard-bookworm", postgres.ImageRepository, targetMajor),
 			postgresqlMinimalEntry: fmt.Sprintf("%v:%v-minimal-bookworm", postgres.ImageRepository, targetMajor),
 		}
 		// Set custom targets when detecting a given env variable
