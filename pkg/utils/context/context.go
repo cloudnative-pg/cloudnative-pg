7030f2ead8e7ff2a3e32373b39f0efb3645b89c3
Armando Ruocco <armando.ruocco@enterprisedb.com>
2025-04-09 18:05:58 +0200

    refactor(cnpgi): remove code duplication and code dependencies (#7242)
    This commit refactors the cnpg-i implementation by removing duplicate code
    and minimizing dependencies between modules.
    
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- pkg/utils/context/context.go
+++ pkg/utils/context/context.go
@@ -17,7 +17,7 @@ limitations under the License.
 SPDX-License-Identifier: Apache-2.0
 */
 
-package utils
+package context
 
 // contextKey a type used to assign values inside the context
 type contextKey string
