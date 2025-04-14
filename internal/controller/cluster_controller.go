8495ce3583ba5e5073a9fcb12efa8565b7909478
Armando Ruocco <armando.ruocco@enterprisedb.com>
2025-04-14 15:59:21 +0200

    chore: reduce diff
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- internal/controller/cluster_controller.go
+++ internal/controller/cluster_controller.go
@@ -1307,14 +1307,6 @@ func (r *ClusterReconciler) createFieldIndexes(ctx context.Context, mgr ctrl.Man
 		return err
 	}
 
-	// Create a new indexed field on Jobs.
-	if err := mgr.GetFieldIndexer().IndexField(
-		ctx,
-		&batchv1.Job{},
-		jobOwnerKey, jobOwnerIndexFunc); err != nil {
-		return err
-	}
-
 	// Create a new indexed field on Backup.
 	if err := mgr.GetFieldIndexer().IndexField(
 		ctx,
@@ -1326,7 +1318,11 @@ func (r *ClusterReconciler) createFieldIndexes(ctx context.Context, mgr ctrl.Man
 		return err
 	}
 
-	return nil
+	// Create a new indexed field on Jobs.
+	return mgr.GetFieldIndexer().IndexField(
+		ctx,
+		&batchv1.Job{},
+		jobOwnerKey, jobOwnerIndexFunc)
 }
 
 // IsOwnedByCluster checks that an object is owned by a Cluster and returns
