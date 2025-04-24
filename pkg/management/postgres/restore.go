a611e298867aaf62eca73612aef22c4bf96235c0
Armando Ruocco <armando.ruocco@enterprisedb.com>
2025-04-24 16:07:13 +0200

    fix: always close the client
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- pkg/management/postgres/restore.go
+++ pkg/management/postgres/restore.go
@@ -823,6 +823,7 @@ func (info InitInfo) WriteInitialPostgresqlConf(ctx context.Context, cluster *ap
 	if err != nil {
 		return fmt.Errorf("error while creating the plugin client: %w", err)
 	}
+	defer pluginCli.Close(ctx)
 	ctx = pluginClient.SetPluginClientInContext(ctx, pluginCli)
 	ctx = cluster.SetInContext(ctx)
 
