7b67a1c20cc266b66ee969bf3db2f95252227d85
Armando Ruocco <armando.ruocco@enterprisedb.com>
2025-05-06 10:07:25 +0200

    refactor: centralize `pgControlData` access logic (#7377)
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>
    (cherry picked from commit 1c9cec9538066d30d7c0466e05ddc43c184f8e19)

--- pkg/utils/parser.go
+++ pkg/utils/parser.go
@@ -19,18 +19,152 @@ SPDX-License-Identifier: Apache-2.0
 
 package utils
 
-import "strings"
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/cloudnative-pg/machinery/pkg/log"
+)
+
+type pgControlDataKey = string
+
+const (
+	// pgControlDataKeyLatestCheckpointTimelineID is the
+	// latest checkpoint's TimeLineID pg_controldata entry
+	pgControlDataKeyLatestCheckpointTimelineID pgControlDataKey = "Latest checkpoint's TimeLineID"
+
+	// pgControlDataKeyREDOWALFile is the latest checkpoint's
+	// REDO WAL file pg_controldata entry
+	pgControlDataKeyREDOWALFile pgControlDataKey = "Latest checkpoint's REDO WAL file"
+
+	// pgControlDataKeyDatabaseSystemIdentifier is the database
+	// system identifier pg_controldata entry
+	pgControlDataKeyDatabaseSystemIdentifier pgControlDataKey = "Database system identifier"
+
+	// pgControlDataKeyLatestCheckpointREDOLocation is the latest
+	// checkpoint's REDO location pg_controldata entry
+	pgControlDataKeyLatestCheckpointREDOLocation pgControlDataKey = "Latest checkpoint's REDO location"
+
+	// pgControlDataKeyTimeOfLatestCheckpoint is the time
+	// of latest checkpoint pg_controldata entry
+	pgControlDataKeyTimeOfLatestCheckpoint pgControlDataKey = "Time of latest checkpoint"
+
+	// pgControlDataDatabaseClusterStateKey is the status
+	// of the latest primary that run on this data directory.
+	pgControlDataDatabaseClusterStateKey pgControlDataKey = "Database cluster state"
+
+	// pgControlDataDataPageChecksumVersion reports whether the checksums are enabled in the cluster
+	pgControlDataDataPageChecksumVersion pgControlDataKey = "Data page checksum version"
+
+	// pgControlDataBytesPerWALSegment reports the size of the WAL segments
+	pgControlDataBytesPerWALSegment pgControlDataKey = "Bytes per WAL segment"
+)
+
+// PgControlData represents the parsed output of pg_controldata
+type PgControlData map[pgControlDataKey]string
+
+// GetLatestCheckpointTimelineID returns the latest checkpoint's TimeLineID
+func (p PgControlData) GetLatestCheckpointTimelineID() string {
+	return p[pgControlDataKeyLatestCheckpointTimelineID]
+}
+
+// TryGetLatestCheckpointTimelineID returns the latest checkpoint's TimeLineID
+func (p PgControlData) TryGetLatestCheckpointTimelineID() (string, bool) {
+	v, ok := p[pgControlDataKeyLatestCheckpointTimelineID]
+	return v, ok
+}
+
+// GetREDOWALFile returns the latest checkpoint's REDO WAL file
+func (p PgControlData) GetREDOWALFile() string {
+	return p[pgControlDataKeyREDOWALFile]
+}
+
+// TryGetREDOWALFile returns the latest checkpoint's REDO WAL file
+func (p PgControlData) TryGetREDOWALFile() (string, bool) {
+	v, ok := p[pgControlDataKeyREDOWALFile]
+	return v, ok
+}
+
+// GetDatabaseSystemIdentifier returns the database system identifier
+func (p PgControlData) GetDatabaseSystemIdentifier() string {
+	return p[pgControlDataKeyDatabaseSystemIdentifier]
+}
+
+// GetLatestCheckpointREDOLocation returns the latest checkpoint's REDO location
+func (p PgControlData) GetLatestCheckpointREDOLocation() string {
+	return p[pgControlDataKeyLatestCheckpointREDOLocation]
+}
+
+// GetTimeOfLatestCheckpoint returns the time of latest checkpoint
+func (p PgControlData) GetTimeOfLatestCheckpoint() string {
+	return p[pgControlDataKeyTimeOfLatestCheckpoint]
+}
+
+// GetDatabaseClusterState returns the status of the latest primary that ran on this data directory
+func (p PgControlData) GetDatabaseClusterState() string {
+	return p[pgControlDataDatabaseClusterStateKey]
+}
+
+// GetDataPageChecksumVersion returns whether the checksums are enabled in the cluster
+func (p PgControlData) GetDataPageChecksumVersion() (string, error) {
+	value, ok := p[pgControlDataDataPageChecksumVersion]
+	if !ok {
+		return "", fmt.Errorf("no '%s' section in pg_controldata output", pgControlDataDataPageChecksumVersion)
+	}
+	return value, nil
+}
+
+// GetBytesPerWALSegment returns the size of the WAL segments
+func (p PgControlData) GetBytesPerWALSegment() (int, error) {
+	value, ok := p[pgControlDataBytesPerWALSegment]
+	if !ok {
+		return 0, fmt.Errorf("no '%s' section in pg_controldata output", pgControlDataBytesPerWALSegment)
+	}
+
+	walSegmentSize, err := strconv.Atoi(value)
+	if err != nil {
+		return 0, fmt.Errorf(
+			"wrong '%s' pg_controldata value (not an integer): '%s' %w",
+			pgControlDataBytesPerWALSegment, value, err)
+	}
+
+	return walSegmentSize, nil
+}
+
+// PgDataState represents the "Database cluster state" field of pg_controldata
+type PgDataState string
+
+// IsShutdown checks if the PGDATA status represents
+// a shut down instance
+func (state PgDataState) IsShutdown(ctx context.Context) bool {
+	contextLogger := log.FromContext(ctx)
+
+	switch state {
+	case "shut down", "shut down in recovery":
+		return true
+
+	case "starting up", "shutting down", "in crash recovery", "in archive recovery", "in production":
+		return false
+	}
+
+	err := fmt.Errorf("unknown pg_controldata cluster state")
+	contextLogger.Error(err, "Unknown pg_controldata cluster state, defaulting to running cluster",
+		"state", state)
+	return false
+}
 
 // ParsePgControldataOutput parses a pg_controldata output into a map of key-value pairs
-func ParsePgControldataOutput(data string) map[string]string {
+func ParsePgControldataOutput(data string) PgControlData {
 	pairs := make(map[string]string)
 	lines := strings.Split(data, "\n")
 	for _, line := range lines {
-		frags := strings.Split(line, ":")
-		if len(frags) != 2 {
+		key, value, done := strings.Cut(line, ":")
+		if !done {
 			continue
 		}
-		pairs[strings.TrimSpace(frags[0])] = strings.TrimSpace(frags[1])
+		pairs[strings.TrimSpace(key)] = strings.TrimSpace(value)
 	}
 	return pairs
 }
