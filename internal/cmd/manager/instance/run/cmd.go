196f54f17c7483e839b32ab1bf55428f2e46d44b
Jaime Silvela <jaime.silvela@mailfence.com>
2025-07-10 15:47:43 +0200

    refactor: implement runnable to perform updates on a ticker
    Signed-off-by: Jaime Silvela <jaime.silvela@mailfence.com>

--- internal/cmd/manager/instance/run/cmd.go
+++ internal/cmd/manager/instance/run/cmd.go
@@ -342,6 +342,11 @@ func runSubCommand(ctx context.Context, instance *postgres.Instance) error {
 		return err
 	}
 
+	if err = mgr.Add(metricsExporter); err != nil {
+		contextLogger.Error(err, "unable to add metricsExporter updater runnable")
+		return err
+	}
+
 	contextLogger.Info("starting tablespace manager")
 	if err := tablespaces.NewTablespaceReconciler(instance, mgr.GetClient()).
 		SetupWithManager(mgr); err != nil {
