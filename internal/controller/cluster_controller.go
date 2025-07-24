95fdc447e41a1c3964bd7f973244a0bb0a375e6b
Francesco Canovai <francesco.canovai@enterprisedb.com>
2025-07-24 17:20:09 +0200

    fix: give cluster reconciler the permission to delete SyncQuorums
    Signed-off-by: Francesco Canovai <francesco.canovai@enterprisedb.com>

--- internal/controller/cluster_controller.go
+++ internal/controller/cluster_controller.go
@@ -145,7 +145,7 @@ var ErrNextLoop = utils.ErrNextLoop
 // +kubebuilder:rbac:groups=snapshot.storage.k8s.io,resources=volumesnapshots,verbs=get;create;watch;list;patch
 // +kubebuilder:rbac:groups=postgresql.cnpg.io,resources=imagecatalogs,verbs=get;watch;list
 // +kubebuilder:rbac:groups=postgresql.cnpg.io,resources=clusterimagecatalogs,verbs=get;watch;list
-// +kubebuilder:rbac:groups=postgresql.cnpg.io,resources=syncquorums,verbs=create;get;watch;list
+// +kubebuilder:rbac:groups=postgresql.cnpg.io,resources=syncquorums,verbs=create;get;watch;delete;list
 // +kubebuilder:rbac:groups=postgresql.cnpg.io,resources=syncquorums/status,verbs=get;patch;update;watch
 
 // Reconcile is the operator reconcile loop
