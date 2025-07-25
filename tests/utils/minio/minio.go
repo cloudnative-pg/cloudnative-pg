92aa4025ef36c8d3a19db0deca964308b58e021a
Marco Nenciarini <marco.nenciarini@enterprisedb.com>
2025-07-26 01:09:57 +0200

    chore: fix some common typos in the code (#8141)
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>
    (cherry picked from commit e877f81f252f23cd639d765619334fc249c4aec0)

--- tests/utils/minio/minio.go
+++ tests/utils/minio/minio.go
@@ -519,7 +519,7 @@ func (m *Env) getCaSecret(env *environment.TestingEnvironment, namespace string)
 	}, nil
 }
 
-// CreateCaSecret creates the certificates required to authenticate against the the MinIO service
+// CreateCaSecret creates the certificates required to authenticate against the MinIO service
 func (m *Env) CreateCaSecret(env *environment.TestingEnvironment, namespace string) error {
 	caSecret, err := m.getCaSecret(env, namespace)
 	if err != nil {
@@ -529,7 +529,7 @@ func (m *Env) CreateCaSecret(env *environment.TestingEnvironment, namespace stri
 	return err
 }
 
-// CountFiles uses the minioClient in the given `namespace` to count  the
+// CountFiles uses the minioClient in the given `namespace` to count the
 // amount of files matching the given `path`
 func CountFiles(minioEnv *Env, path string) (value int, err error) {
 	var stdout string
