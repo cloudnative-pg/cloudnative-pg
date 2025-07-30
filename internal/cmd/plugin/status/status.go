730a2f8b9d6f435389297a9ddda8be4d03ef90db
Infinoid <mark-github@glines.org>
2025-07-30 03:15:55 -0400

    fix(plugin): cosmetic error in `cnpg status` output (#8146)
    The table columns don't line up in the "cnpg status" output when a pod
    status lookup failed.
    
    Closes: #8145
    
    Signed-off-by: Mark Glines <mark@glines.org>
    (cherry picked from commit b7e9f07cf6fa2181bc5c9b8e82d4b37b27ee92ba)

--- internal/cmd/plugin/status/status.go
+++ internal/cmd/plugin/status/status.go
@@ -643,7 +643,6 @@ func (fullStatus *PostgresqlStatus) printInstancesStatus() {
 				instance.Pod.Name,
 				"-",
 				"-",
-				"-",
 				apierrs.ReasonForError(instance.Error),
 				instance.Pod.Status.QOSClass,
 				"-",
