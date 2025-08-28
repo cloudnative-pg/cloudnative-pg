003f8e8044601e0a5aaa5ec369a82b66a7864958
Armando Ruocco <armando.ruocco@enterprisedb.com>
2025-08-28 12:30:33 +0200

    chore: organize labels
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- pkg/specs/pgbouncer/services_test.go
+++ pkg/specs/pgbouncer/services_test.go
@@ -78,13 +78,13 @@ var _ = Describe("Pooler Service", func() {
 			Expect(service.Name).To(Equal(pooler.Name))
 			Expect(service.Namespace).To(Equal(pooler.Namespace))
 			Expect(service.Labels).To(BeEquivalentTo(map[string]string{
-				utils.ClusterLabelName:   cluster.Name,
-				utils.PgbouncerNameLabel: pooler.Name,
-				utils.PodRoleLabelName:   string(utils.PodRolePooler),
-				utils.AppLabelName:       utils.AppName,
-				utils.InstanceLabelName:  cluster.Name,
-				utils.ComponentLabelName: utils.PoolerComponentName,
-				utils.ManagedByLabelName: utils.ManagerName,
+				utils.ClusterLabelName:                cluster.Name,
+				utils.PgbouncerNameLabel:              pooler.Name,
+				utils.PodRoleLabelName:                string(utils.PodRolePooler),
+				utils.KubernetesAppLabelName:          utils.AppName,
+				utils.KubernetesAppInstanceLabelName:  cluster.Name,
+				utils.KubernetesAppComponentLabelName: utils.PoolerComponentName,
+				utils.KubernetesAppManagedByLabelName: utils.ManagerName,
 			}))
 			Expect(service.Spec.Type).To(Equal(corev1.ServiceTypeClusterIP))
 			Expect(service.Spec.Ports).To(ConsistOf(corev1.ServicePort{
