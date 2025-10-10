0909d2bc7513c139e52d137feaddac28b4600c06
Pascal Bourdier <PascalBourdier@users.noreply.github.com>
2025-10-10 09:15:23 +0200

    fix: prevent potential nil pointer dereference (#8756)
    Prevent possible nil pointer dereference when an error occurs inside the
    hibernation reconciler.
    
    Signed-off-by: Pascal Bourdier <pascal.bourdier@gmail.com>
    (cherry picked from commit 5788e026a2453e51b0c47b95337538e710270fb8)

--- internal/controller/cluster_controller.go
+++ internal/controller/cluster_controller.go
@@ -482,13 +482,17 @@ func (r *ClusterReconciler) reconcile(ctx context.Context, cluster *apiv1.Cluste
 	// ensuring the primary to be healthy. The hibernation starts from the
 	// primary Pod to ensure the replicas are in sync and doing it here avoids
 	// any unwanted switchover.
-	if result, err := hibernation.Reconcile(
+	hibernationResult, err := hibernation.Reconcile(
 		ctx,
 		r.Client,
 		cluster,
 		resources.instances.Items,
-	); result != nil || err != nil {
-		return *result, err
+	)
+	if hibernationResult != nil {
+		return *hibernationResult, err
+	}
+	if err != nil {
+		return ctrl.Result{}, err
 	}
 
 	// We have already updated the status in updateResourceStatus call,
