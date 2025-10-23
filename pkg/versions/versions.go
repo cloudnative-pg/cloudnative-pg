2262aeccced7ee2546dd9e2d00afa88c56fc2323
Marco Nenciarini <marco.nenciarini@enterprisedb.com>
2025-10-23 14:32:41 +0200

    Version tag to 1.25.4
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>

--- pkg/versions/versions.go
+++ pkg/versions/versions.go
@@ -23,13 +23,13 @@ package versions
 
 const (
 	// Version is the version of the operator
-	Version = "1.25.3"
+	Version = "1.25.4"
 
 	// DefaultImageName is the default image used by the operator to create pods
 	DefaultImageName = "ghcr.io/cloudnative-pg/postgresql:18.0-system-trixie"
 
 	// DefaultOperatorImageName is the default operator image used by the controller in the pods running PostgreSQL
-	DefaultOperatorImageName = "ghcr.io/cloudnative-pg/cloudnative-pg:1.25.3"
+	DefaultOperatorImageName = "ghcr.io/cloudnative-pg/cloudnative-pg:1.25.4"
 )
 
 // BuildInfo is a struct containing all the info about the build
@@ -39,7 +39,7 @@ type BuildInfo struct {
 
 var (
 	// buildVersion injected during the build
-	buildVersion = "1.25.3"
+	buildVersion = "1.25.4"
 
 	// buildCommit injected during the build
 	buildCommit = "none"
