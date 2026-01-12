dd3f99fcb121b87c67fbe44a139bff7273d9d5fe
Marco Nenciarini <marco.nenciarini@enterprisedb.com>
2026-01-12 17:58:29 +0100

    fix(testing): remove non-breaking spaces from all shell scripts
    Signed-off-by: Marco Nenciarini <marco.nenciarini@enterprisedb.com>

--- hack/testing-tools/common/00-paths.sh
+++ hack/testing-tools/common/00-paths.sh
@@ -7,7 +7,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-20.0
+#      http://www.apache.org/licenses/LICENSE-20.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -55,8 +55,8 @@ reset='\033[0m'
 
 # Check if stdout is a terminal; if not, clear the codes to prevent output pollution.
 if [ ! -t 1 ]; then
-  bright=''
-  reset=''
+  bright=''
+  reset=''
 fi
 export bright reset
 # -----------------------------------------------
