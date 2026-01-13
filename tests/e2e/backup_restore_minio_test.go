1bd0bd4960b9006d0fc7e49bdd4f1e930bf6c111
Armando Ruocco <armando.ruocco@enterprisedb.com>
2026-01-13 15:09:28 +0100

    test(e2e): explicit main cluster deletion
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- tests/e2e/backup_restore_minio_test.go
+++ tests/e2e/backup_restore_minio_test.go
@@ -112,6 +112,13 @@ var _ = Describe("MinIO - Backup and restore", Label(tests.LabelBackupRestore), 
 			})
 		})
 
+		AfterAll(func() {
+			// While namespace deletion would handle this implicitly, explicit deletion helps:
+			// - Identify any deletion issues early and in a more clear way rather than waiting for namespace cleanup
+			err := DeleteResourcesFromFile(namespace, clusterWithMinioSampleFile)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
 		// We back up and restore a cluster, and verify some expected data to
 		// be there
 		It("backs up and restores a cluster using minio", func() {
