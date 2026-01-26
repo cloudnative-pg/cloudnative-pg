8033275c179bd882caf493076dc86acd9432a1f3
Armando Ruocco <armando.ruocco@enterprisedb.com>
2026-01-26 17:47:04 +0100

    fix: properly resolve extensions and set phase error on major upgrade failures
    When a cluster performs a PostgreSQL major version upgrade, extension
    images must be resolved for the new major version since they are
    version-specific (e.g., postgis:3.4-pg16 vs postgis:3.4-pg17).
    
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- internal/controller/cluster_image.go
+++ internal/controller/cluster_image.go
@@ -38,6 +38,7 @@ import (
 
 	apiv1 "github.com/cloudnative-pg/cloudnative-pg/api/v1"
 	"github.com/cloudnative-pg/cloudnative-pg/pkg/resources/status"
+	"github.com/cloudnative-pg/cloudnative-pg/pkg/utils/extensions"
 )
 
 // reconcileImage processes the image request, executes it, and stores
@@ -193,7 +194,7 @@ func (r *ClusterReconciler) getRequestedImageInfo(
 		return apiv1.ImageInfo{}, fmt.Errorf("selected major version is not available in the catalog")
 	}
 
-	extensions, err := getExtensionsFromCatalog(cluster, catalog, requestedMajorVersion)
+	exts, err := extensions.ResolveFromCatalog(cluster, catalog, requestedMajorVersion)
 	if err != nil {
 		return apiv1.ImageInfo{}, fmt.Errorf("cannot retrieve extensions for image %s: %w", catalogImage, err)
 	}
@@ -201,7 +202,7 @@ func (r *ClusterReconciler) getRequestedImageInfo(
 	return apiv1.ImageInfo{
 		Image:        catalogImage,
 		MajorVersion: requestedMajorVersion,
-		Extensions:   extensions,
+		Extensions:   exts,
 	}, nil
 }
 
@@ -295,87 +296,4 @@ func (r *ClusterReconciler) mapImageCatalogsToClusters() handler.MapFunc {
 		}
 		return requests
 	}
-}
-
-// getExtensionsFromCatalog returns a list of requested Extensions from a Catalog for a given
-// Postgres major version.
-// If present, extension entries present in the catalog are always used as a starting base.
-// If additional configuration is passed via `cluster.Spec.PostgresConfiguration.Extensions`
-// for an extension that's already defined in the catalog, the values defined in the Cluster
-// take precedence.
-func getExtensionsFromCatalog(
-	cluster *apiv1.Cluster,
-	catalog apiv1.GenericImageCatalog,
-	requestedMajorVersion int,
-) ([]apiv1.ExtensionConfiguration, error) {
-	requestedExtensions := cluster.Spec.PostgresConfiguration.Extensions
-	resolvedExtensions := make([]apiv1.ExtensionConfiguration, 0, len(requestedExtensions))
-
-	// Build a map of extensions coming from the catalog
-	catalogExtensionsMap := make(map[string]apiv1.ExtensionConfiguration)
-	if extensions, ok := catalog.GetSpec().FindExtensionsForMajor(requestedMajorVersion); ok {
-		for _, extension := range extensions {
-			catalogExtensionsMap[extension.Name] = extension
-		}
-	}
-
-	// Resolve extensions
-	for _, extension := range requestedExtensions {
-		catalogExtension, found := catalogExtensionsMap[extension.Name]
-
-		// Validate that the ImageVolumeSource.Reference is properly defined.
-		// We want to allow overriding each field of an extension defined in a catalog,
-		// meaning that even the ImageVolumeSource.Reference is defined as an optional field,
-		// although it must be defined either in the catalog or in the Cluster Spec.
-
-		// Case 1. This case is also covered by the validateExtensions cluster webhook, but it doesn't
-		// hurt to have it here as well.
-		if !found && extension.ImageVolumeSource.Reference == "" {
-			return []apiv1.ExtensionConfiguration{}, fmt.Errorf(
-				"extension `%q` defined exclusively in the Cluster Spec but no `ImageVolumeSource.Reference` is present",
-				extension.Name)
-		}
-
-		// Case 2. This case must be handled here because we don't have a validation webhook for the catalog,
-		// but it could be moved there if we decide to add one.
-		if found && catalogExtension.ImageVolumeSource.Reference == "" && extension.ImageVolumeSource.Reference == "" {
-			return []apiv1.ExtensionConfiguration{}, fmt.Errorf(
-				"extension `%q` defined in image catalog %s/%s but no `ImageVolumeSource.Reference` is present "+
-					"in both the image catalog and the Cluster Spec",
-				extension.Name, catalog.GetNamespace(), catalog.GetName(),
-			)
-		}
-
-		var resultExtension apiv1.ExtensionConfiguration
-		if found {
-			// Found the extension in the catalog, so let's use the catalog entry as a base
-			// and eventually override it with Cluster Spec values
-			resultExtension = catalogExtension
-		} else {
-			// No catalog entry, rely fully on the Cluster Spec
-			resolvedExtensions = append(resolvedExtensions, extension)
-			continue
-		}
-
-		// Apply the Cluster Spec overrides
-		if extension.ImageVolumeSource.Reference != "" {
-			resultExtension.ImageVolumeSource.Reference = extension.ImageVolumeSource.Reference
-		}
-		if extension.ImageVolumeSource.PullPolicy != "" {
-			resultExtension.ImageVolumeSource.PullPolicy = extension.ImageVolumeSource.PullPolicy
-		}
-		if len(extension.ExtensionControlPath) > 0 {
-			resultExtension.ExtensionControlPath = extension.ExtensionControlPath
-		}
-		if len(extension.DynamicLibraryPath) > 0 {
-			resultExtension.DynamicLibraryPath = extension.DynamicLibraryPath
-		}
-		if len(extension.LdLibraryPath) > 0 {
-			resultExtension.LdLibraryPath = extension.LdLibraryPath
-		}
-
-		resolvedExtensions = append(resolvedExtensions, resultExtension)
-	}
-
-	return resolvedExtensions, nil
 }
