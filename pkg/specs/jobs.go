7c49c2b15f5a7405927d0d4fc2d14479533f679f
Armando Ruocco <armando.ruocco@enterprisedb.com>
2026-02-19 11:29:43 +0100

    feat(specs): set TTLSecondsAfterFinished on jobs
    Set a 2-day (172800s) TTL on all jobs created by the operator
    (init, join, snapshot-recovery, pg_basebackup).
    
    Signed-off-by: Armando Ruocco <armando.ruocco@enterprisedb.com>

--- pkg/specs/jobs.go
+++ pkg/specs/jobs.go
@@ -332,6 +332,7 @@ func CreatePrimaryJob(cluster apiv1.Cluster, nodeSerial int, role jobRole, initC
 
 	envConfig := CreatePodEnvConfig(cluster, jobName)
 
+	ttl := int32(172800) // 2 days
 	job := &batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      jobName,
@@ -348,6 +349,7 @@ func CreatePrimaryJob(cluster apiv1.Cluster, nodeSerial int, role jobRole, initC
 			},
 		},
 		Spec: batchv1.JobSpec{
+			TTLSecondsAfterFinished: &ttl,
 			Template: corev1.PodTemplateSpec{
 				ObjectMeta: metav1.ObjectMeta{
 					Labels: map[string]string{
