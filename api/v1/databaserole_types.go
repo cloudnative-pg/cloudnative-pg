2ecaf318015839ca72272ef9c9314b3f3f6638c3
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2026-05-07 13:57:54 +0200

    chore: avoid using SchemeBuilder
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>

--- api/v1/databaserole_types.go
+++ api/v1/databaserole_types.go
@@ -121,7 +121,3 @@ type DatabaseRoleList struct {
 	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []DatabaseRole `json:"items"`
 }
-
-func init() {
-	SchemeBuilder.Register(&DatabaseRole{}, &DatabaseRoleList{})
-}
