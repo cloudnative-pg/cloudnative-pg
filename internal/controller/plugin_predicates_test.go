df794801455cc5d836bb3be2b94ed77ba040dfc3
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2026-06-04 14:00:36 +0200

    feat: reload CNPG-i plugins automatically when their pods are rolled
    Previously, after upgrading a CNPG-i plugin (re-applying its manifest with
    a new image), the operator kept talking to the plugin through the
    connection it had opened during the most recent reconciliation. Clusters
    using that plugin would not see the new version until something else
    triggered a reconcile (a spec change, the periodic resync, or an operator
    restart), which could leave clusters running against an old plugin
    implementation for a long time.
    
    With this change, the operator watches the EndpointSlices that back
    CNPG-i plugin Services in the operator namespace. When the plugin
    Deployment finishes rolling out and the new Pods become Ready, the
    EndpointSlice changes and every cluster using that plugin is enqueued
    for reconciliation, which reopens the connection against the new Pods.
    
    A new field index on Cluster (".spec.usedPlugins") is used to look up
    all clusters affected by a given plugin without scanning the full
    cluster list on every EndpointSlice event.
    
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>

---
+++ internal/controller/plugin_predicates_test.go
@@ -0,0 +1,71 @@
+/*
+Copyright © contributors to CloudNativePG, established as
+CloudNativePG a Series of LF Projects, LLC.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package controller
+
+import (
+	discoveryv1 "k8s.io/api/discovery/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("operatorNamespaceServiceEndpointSlicePredicate", func() {
+	const operatorNamespace = "cnpg-system"
+
+	predicate := operatorNamespaceServiceEndpointSlicePredicate(operatorNamespace)
+
+	newSlice := func(namespace, serviceName string) *discoveryv1.EndpointSlice {
+		slice := &discoveryv1.EndpointSlice{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "example-abcde",
+				Namespace: namespace,
+			},
+		}
+		if serviceName != "" {
+			slice.Labels = map[string]string{
+				discoveryv1.LabelServiceName: serviceName,
+			}
+		}
+		return slice
+	}
+
+	It("accepts slices in the operator namespace that back a Service", func() {
+		slice := newSlice(operatorNamespace, "some-plugin")
+		Expect(predicate.Create(event.CreateEvent{Object: slice})).To(BeTrue())
+	})
+
+	It("rejects slices outside the operator namespace", func() {
+		slice := newSlice("other-namespace", "some-plugin")
+		Expect(predicate.Create(event.CreateEvent{Object: slice})).To(BeFalse())
+	})
+
+	It("rejects slices without the kubernetes.io/service-name label", func() {
+		slice := newSlice(operatorNamespace, "")
+		Expect(predicate.Create(event.CreateEvent{Object: slice})).To(BeFalse())
+	})
+
+	It("rejects slices whose kubernetes.io/service-name label is empty", func() {
+		slice := newSlice(operatorNamespace, "")
+		slice.Labels = map[string]string{discoveryv1.LabelServiceName: ""}
+		Expect(predicate.Create(event.CreateEvent{Object: slice})).To(BeFalse())
+	})
+})
