9a724ce33451a59becca522e09d5a7ee143b64b2
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2026-08-14 05:52:13 +0200

    test(e2e): use env.DefaultStorageClass in e2e tests (#11317)
    Avoids reading E2E_DEFAULT_STORAGE_CLASS from the environment directly
    and reuses the value already exposed on the test environment (env.DefaultStorageClass).
    
    ---------
    
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
    Signed-off-by: Tao Li <tao.li@enterprisedb.com>
    Co-authored-by: Tao Li <tao.li@enterprisedb.com>

--- tests/e2e/pod_selector_refs_test.go
+++ tests/e2e/pod_selector_refs_test.go
@@ -21,11 +21,11 @@ package e2e
 
 import (
 	"fmt"
-	"os"
 
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/util/retry"
+	"k8s.io/utils/ptr"
 
 	apiv1 "github.com/cloudnative-pg/cloudnative-pg/api/v1"
 	"github.com/cloudnative-pg/cloudnative-pg/tests"
@@ -62,9 +62,6 @@ var _ = Describe("Pod selector refs for pg_hba", Label(tests.LabelPostgresConfig
 			namespace, err = env.CreateUniqueTestNamespace(env.Ctx, env.Client, "pod-selector-refs-e2e")
 			Expect(err).ToNot(HaveOccurred())
 
-			storageClass := os.Getenv("E2E_DEFAULT_STORAGE_CLASS")
-			Expect(storageClass).ToNot(BeEmpty())
-
 			cluster := &apiv1.Cluster{
 				ObjectMeta: metav1.ObjectMeta{
 					Name:      clusterName,
@@ -73,7 +70,7 @@ var _ = Describe("Pod selector refs for pg_hba", Label(tests.LabelPostgresConfig
 				Spec: apiv1.ClusterSpec{
 					Instances: 1,
 					StorageConfiguration: apiv1.StorageConfiguration{
-						StorageClass: &storageClass,
+						StorageClass: ptr.To(env.DefaultStorageClass),
 						Size:         "1Gi",
 					},
 					PodSelectorRefs: []apiv1.PodSelectorRef{
